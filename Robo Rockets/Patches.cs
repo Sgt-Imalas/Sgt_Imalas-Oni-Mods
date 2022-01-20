9fa8af81118feceb6fec7092ee9ddf9ea7e436bd
Knastoron <bene.schlai@gmx.de>
2022-01-20 12:59:05 +0100

    Add project files.

---
+++ Robo Rockets/Patches.cs
@@ -0,0 +1,22 @@
+using HarmonyLib;
+
+namespace Robo_Rockets
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(Db))]
+        [HarmonyPatch("Initialize")]
+        public class Db_Initialize_Patch
+        {
+            public static void Prefix()
+            {
+                Debug.Log("I execute before Db.Initialize!");
+            }
+
+            public static void Postfix()
+            {
+                Debug.Log("I execute after Db.Initialize!");
+            }
+        }
+    }
+}
