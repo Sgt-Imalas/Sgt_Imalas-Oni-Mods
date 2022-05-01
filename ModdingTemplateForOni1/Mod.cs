0bebd4064914e60296566bb11e2ec1d4ffbf05bf
Knastoron <bene.schlai@gmx.de>
2022-05-01 04:29:11 +0200

    ExportedTemplate

---
+++ ModdingTemplateForOni1/Mod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModdingTemplateForOni1
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
