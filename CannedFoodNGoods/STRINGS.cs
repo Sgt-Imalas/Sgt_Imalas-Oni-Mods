bedd6d8a1268fb1f83e32156913be9634749df1d
Knastoron <bene.schlai@gmx.de>
2022-06-09 15:24:03 +0200

    string linking fix

--- CannedFoodNGoods/STRINGS.cs
+++ CannedFoodNGoods/STRINGS.cs
@@ -11,12 +11,12 @@ namespace CannedFoods
                 public class CF_CANNEDBBQ
                 {
                     public static LocString NAME = global::STRINGS.UI.FormatAsLink("Canned Barbecue", CannedBBQConfig.ID);
-                    public static LocString DESC = "Barbecue preserved for the ages.";
+                    public static LocString DESC = global::STRINGS.UI.FormatAsLink(global::STRINGS.ITEMS.FOOD.COOKEDMEAT.NAME, CookedMeatConfig.ID) + " preserved for the ages.";
                 }
                 public class CF_CANNEDTUNA
                 {
                     public static LocString NAME = global::STRINGS.UI.FormatAsLink("Canned Tuna", CannedTunaConfig.ID);
-                    public static LocString DESC = "Pacu fry preserved for the ages.\nIncreases radiation resistance on consumption.";
+                    public static LocString DESC = global::STRINGS.UI.FormatAsLink(global::STRINGS.ITEMS.FOOD.COOKEDFISH.NAME, CookedFishConfig.ID)+" preserved for the ages.\nIncreases radiation resistance on consumption.";
                 }
             }
         }
