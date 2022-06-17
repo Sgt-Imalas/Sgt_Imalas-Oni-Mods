4b28c9136c2951423d4cf808755c5a01af6e1894
Knastoron <bene.schlai@gmx.de>
2022-06-17 15:22:45 +0200

    DebugFunctionV1 in RR, Adjusted habitat radiator template

--- Robo Rockets/RocketSubstitutionClasses/RocketControlStationNoChorePrecondition.cs
+++ Robo Rockets/RocketSubstitutionClasses/RocketControlStationNoChorePrecondition.cs
@@ -15,10 +15,15 @@ namespace KnastoronOniMods
         
         private SchedulerHandle newSweepyHandle;
         private GameObject brainController;
-        public void MakeNewPilotBot()
+        public void MakeNewPilotBot(bool isDebug = false)
         {
-            if (this.newSweepyHandle.IsValid || brainController!=null)
+            if ((this.newSweepyHandle.IsValid || brainController!=null)&&!isDebug)
                 return;
+
+            if (brainController != null)
+            {
+                KillRobo();
+            }
             this.newSweepyHandle = GameScheduler.Instance.Schedule("Make brain", 1f, (System.Action<object>)(obj =>
             {
                 GameObject go = GameUtil.KInstantiate(Assets.GetPrefab((Tag)"AiBrain"), Grid.CellToPos(Grid.CellRight(Grid.PosToCell(this.gameObject))), Grid.SceneLayer.Creatures);
@@ -39,6 +44,10 @@ namespace KnastoronOniMods
         protected override void OnCleanUp()
         {
             base.OnCleanUp();
+            KillRobo();
+        }
+        protected void KillRobo()
+        {
             brainController.GetComponent<SelfDestructInWrongEnvironmentComponent>().SelfDestruct();
             brainController = null;
         }
