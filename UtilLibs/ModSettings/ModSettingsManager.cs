f0d6736496d3c395f0a7210f282c023c8cb343df
Knastoron <bene.schlai@gmx.de>
2022-07-12 01:36:39 +0200

    Some Cleanup, first ideas on own mod settings (but then; plib..)

---
+++ UtilLibs/ModSettings/ModSettingsManager.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilLibs.ModSettings
+{
+    public class ModSettingsManager<T> where T : IModSettingsInstance, new()
+    {
+        public T Settings { get; set; }
+        public Action<T> OnRead;
+        private FileSystemWatcher watcher;
+        private string ConfigPath;
+
+        public void InstantiateSettings()
+        {
+            ConfigPath = Path.Combine(UtilMethods.ModPath, "ModConfig.json");
+            Settings = DeserializeJson();
+        }
+
+        public void ResetSettings()
+        {
+            Settings = new T();
+            SerializeJson();
+        }
+
+        public T DeserializeJson()
+        {
+            T returnSettings;
+            if (!File.Exists(ConfigPath))
+            {
+                returnSettings = new T(); 
+            }
+            using (StreamReader r = new StreamReader(ConfigPath))
+            {
+                string json = r.ReadToEnd();
+                returnSettings = JsonConvert.DeserializeObject<T>(json);
+            }
+            return returnSettings;
+        }
+
+        public void SerializeJson()
+        {
+            var data = JsonConvert.SerializeObject(Settings);
+            File.WriteAllText(ConfigPath, data);
+        }
+    }
+}
