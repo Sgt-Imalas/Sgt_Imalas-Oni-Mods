5c115b9b77d0754a8680b956a3036c4e597de6fa
Knastoron <bene.schlai@gmx.de>
2022-07-19 19:04:18 +0200

    Templates, Graphic for extended medium, stargazer wip, glass patch

--- Rockets-TinyYetBig/Buildings/HabitatModuleStargazerConfig.cs
+++ Rockets-TinyYetBig/Buildings/HabitatModuleStargazerConfig.cs
@@ -8,34 +8,31 @@ using UnityEngine;
 
 namespace Rockets_TinyYetBig
 {
-    class HabitatModuleMediumExpandedConfig : IBuildingConfig
+    class HabitatModuleStargazerConfig : IBuildingConfig
     {
-        public const string ID = "RTB_HabitatModuleMediumExpanded";
-        private ConduitPortInfo gasInputPort = new ConduitPortInfo(ConduitType.Gas, new CellOffset(-2, 0));
-        private ConduitPortInfo gasOutputPort = new ConduitPortInfo(ConduitType.Gas, new CellOffset(2, 0));
-        private ConduitPortInfo liquidInputPort = new ConduitPortInfo(ConduitType.Liquid, new CellOffset(-2, 3));
-        private ConduitPortInfo liquidOutputPort = new ConduitPortInfo(ConduitType.Liquid, new CellOffset(2, 3));
-
+        public const string ID = "RTB_HabitatModuleStargazer";
+        private ConduitPortInfo gasInputPort = new ConduitPortInfo(ConduitType.Gas, new CellOffset(-1, 0));
+        private ConduitPortInfo gasOutputPort = new ConduitPortInfo(ConduitType.Gas, new CellOffset(1, 0));
+        private ConduitPortInfo liquidInputPort = new ConduitPortInfo(ConduitType.Liquid, new CellOffset(-1, 1));
+        private ConduitPortInfo liquidOutputPort = new ConduitPortInfo(ConduitType.Liquid, new CellOffset(1, 1));
         public override string[] GetDlcIds() => DlcManager.AVAILABLE_EXPANSION1_ONLY;
-
         public override BuildingDef CreateBuildingDef()
         {
-            float[] denseTieR1 = new float[] { 800f };
+            float[] denseTieR0 = new float[] { 350f }; ;
             string[] rawMetals = MATERIALS.RAW_METALS;
             EffectorValues noiseLevel = NOISE_POLLUTION.NOISY.TIER2;
             EffectorValues none = BUILDINGS.DECOR.NONE;
-
             BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(
-                id: ID,
+                id : ID,
                 width: 5,
-                height: 6,
-                anim: "rocket_habitat_medium_module_extended_kanim",
+                height: 3,
+                anim: "rocket_nosecone_small_extended_kanim", 
                 hitpoints: 1000,
-                construction_time: 60f,
-                construction_mass: denseTieR1,
+                construction_time: 60f, 
+                construction_mass: denseTieR0, 
                 construction_materials: rawMetals,
-                melting_point: 9999f,
-                BuildLocationRule.Anywhere,
+                melting_point: 9999f, 
+                BuildLocationRule.Anywhere, 
                 decor: none,
                 noise: noiseLevel);
             BuildingTemplates.CreateRocketBuildingDef(buildingDef);
@@ -58,10 +55,11 @@ namespace Rockets_TinyYetBig
             BuildingConfigManager.Instance.IgnoreDefaultKComponent(typeof(RequiresFoundation), prefab_tag);
             go.AddOrGet<LoopingSounds>();
             go.GetComponent<KPrefabID>().AddTag(RoomConstraints.ConstraintTags.IndustrialMachinery);
+            go.GetComponent<KPrefabID>().AddTag(GameTags.NoseRocketModule);
             go.GetComponent<KPrefabID>().AddTag(GameTags.LaunchButtonRocketModule);
             go.AddOrGet<AssignmentGroupController>().generateGroupOnStart = true;
-            go.AddOrGet<PassengerRocketModule>().interiorReverbSnapshot = AudioMixerSnapshots.Get().MediumRocketInteriorReverbSnapshot;
-            go.AddOrGet<ClustercraftExteriorDoor>().interiorTemplateName = "interiors/habitat_medium_expanded";
+            go.AddOrGet<PassengerRocketModule>().interiorReverbSnapshot = AudioMixerSnapshots.Get().SmallRocketInteriorReverbSnapshot;
+            go.AddOrGet<ClustercraftExteriorDoor>().interiorTemplateName = "interiors/habitat_stargazer";
             go.AddOrGetDef<SimpleDoorController.Def>();
             go.AddOrGet<NavTeleporter>();
             go.AddOrGet<AccessControl>();
@@ -69,10 +67,6 @@ namespace Rockets_TinyYetBig
             go.AddOrGet<RocketCommandConditions>();
             go.AddOrGet<RocketProcessConditionDisplayTarget>();
             go.AddOrGet<CharacterOverlay>().shouldShowName = true;
-            go.AddOrGet<BuildingAttachPoint>().points = new BuildingAttachPoint.HardPoint[1]
-            {
-      new BuildingAttachPoint.HardPoint(new CellOffset(0, 6), GameTags.Rocket, (AttachableBuilding) null)
-            };
             Storage storage1 = go.AddComponent<Storage>();
             storage1.showInUI = false;
             storage1.capacityKg = 10f;
@@ -99,22 +93,21 @@ namespace Rockets_TinyYetBig
 
         public override void DoPostConfigureComplete(GameObject go)
         {
-            BuildingTemplates.ExtendBuildingToRocketModuleCluster(go, (string)null, ROCKETRY.BURDEN.MAJOR);
+            BuildingTemplates.ExtendBuildingToRocketModuleCluster(go, (string)null, ROCKETRY.BURDEN.MINOR_PLUS);
             Ownable ownable = go.AddOrGet<Ownable>();
             ownable.slotID = Db.Get().AssignableSlots.HabitatModule.Id;
             ownable.canBePublic = false;
             FakeFloorAdder fakeFloorAdder = go.AddOrGet<FakeFloorAdder>();
-            fakeFloorAdder.floorOffsets = new CellOffset[5]
+            fakeFloorAdder.floorOffsets = new CellOffset[3]
             {
-      new CellOffset(-2, -1),
       new CellOffset(-1, -1),
       new CellOffset(0, -1),
-      new CellOffset(1, -1),
-      new CellOffset(2, -1)
+      new CellOffset(1, -1)
             };
             fakeFloorAdder.initiallyActive = false;
             go.AddOrGet<BuildingCellVisualizer>();
             go.GetComponent<ReorderableBuilding>().buildConditions.Add((SelectModuleCondition)new LimitOneCommandModule());
+            go.GetComponent<ReorderableBuilding>().buildConditions.Add((SelectModuleCondition)new TopOnly());
         }
 
         public override void DoPostConfigurePreview(BuildingDef def, GameObject go)
