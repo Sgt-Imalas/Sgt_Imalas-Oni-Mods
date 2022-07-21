e684b1bdd1151542dfa25a7a157eb2bafab6f5a0
Knastoron <bene.schlai@gmx.de>
2022-07-21 02:06:41 +0200

    Cryopod Init

---
+++ Cryopod/Mod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cryopod
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
---
+++ Cryopod/Patches.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryopod
+{
+    class Patches
+    {
+    }
+}
