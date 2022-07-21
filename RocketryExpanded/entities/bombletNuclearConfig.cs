3a1fd17bd477baca2b5da66d05515005c37a3209
Knastoron <bene.schlai@gmx.de>
2022-07-21 14:16:39 +0200

    addition to last commit; namespace update

--- RocketryExpanded/entities/bombletNuclearConfig.cs
+++ RocketryExpanded/entities/bombletNuclearConfig.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace RocketryExpanded.entities
+namespace ExplosiveMaterials.entities
 {
     public class BombletNuclearConfig : IEntityConfig
     {
