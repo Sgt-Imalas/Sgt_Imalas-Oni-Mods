e0cdbdda53879e2884f9cc361542c2fa54e0d40a
Knastoron <bene.schlai@gmx.de>
2022-09-14 21:23:53 +0200

    New Mod: Daily Routine

---
+++ DailyRoutine/Mod.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+
+namespace DailyRoutine
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
---
+++ DailyRoutine/ModAssets.cs
@@ -0,0 +1,17 @@
+﻿
+using Klei.AI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DailyRoutine
+{
+    internal class ModAssets
+    {
+
+    }
+}
