ebd11f8a1e71a0831f448823adda83138c1e3f9f
Knastoron <bene.schlai@gmx.de>
2022-09-26 03:21:05 +0200

    Back2theDrawingboard

--- Rockets-TinyYetBig/Docking/DockingPatches.cs
+++ Rockets-TinyYetBig/Docking/DockingPatches.cs
@@ -12,31 +12,12 @@ namespace Rockets_TinyYetBig.Patches
 {
     class DockingPatches
     {
-        [HarmonyPatch(typeof(Clustercraft))]
-        [HarmonyPatch("OnSpawn")]
-        public class AddDockingManager_Patch
-        { 
-            public static void Postfix(Clustercraft __instance)
-            {
-                //__instance.FindOrAdd<DockingManager>();
-            }
-        }
-        [HarmonyPatch(typeof(Clustercraft))]
-        [HarmonyPatch("OnCleanUp")]
-        public class RemoveDockingManager_Patch
-        {
-            public static void Postfix(Clustercraft __instance)
-            {
-                //__instance.FindOrAdd<DockingManager>().DeleteObject();
-            }
-        }
-
         [HarmonyPatch(typeof(DetailsScreen), "OnPrefabInit")]
         public static class CustomSideScreenPatch_DailyReset
         {
             public static void Postfix(List<DetailsScreen.SideScreenRef> ___sideScreens)
             {
-                UIUtils.AddClonedSideScreen<DockingSidescreen>("DockingSidescreen", "LogicBroadcastChannelSideScreen", typeof(LogicBroadcastChannelSideScreen));
+               // UIUtils.AddClonedSideScreen<DockingSidescreen>("DockingSidescreen", "LogicBroadcastChannelSideScreen", typeof(LogicBroadcastChannelSideScreen));
             }
         }
     }
