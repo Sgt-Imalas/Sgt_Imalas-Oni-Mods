756e104f71eabc85fb2a60517b1452243cce4d7b
Knastoron <bene.schlai@gmx.de>
2022-10-17 17:05:03 +0200

    Petrol Rockets now allow for ethanol fuel

---
+++ Rockets-TinyYetBig/Patches/PetrolRocketUseAllCombustiblesPatch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Rockets_TinyYetBig.Patches
+{
+    class PetrolRocketUseAllCombustiblesPatch
+    {
+        [HarmonyPatch(typeof(KeroseneEngineClusterConfig), "DoPostConfigureComplete")]
+        public static class ReplaceFuelTagInPetrolRocket
+        {
+            public static void Postfix(GameObject go)
+            {
+                RocketEngineCluster rocketEngineCluster = go.GetComponent<RocketEngineCluster>();
+                rocketEngineCluster.fuelTag = GameTags.CombustibleLiquid;
+            }
+
+        }
+        [HarmonyPatch(typeof(KeroseneEngineClusterSmallConfig), "DoPostConfigureComplete")]
+        public static class ReplaceFuelTagInSmallPetrolRocket
+        {
+            public static void Postfix(GameObject go)
+            {
+                RocketEngineCluster rocketEngineCluster = go.GetComponent<RocketEngineCluster>();
+                rocketEngineCluster.fuelTag = GameTags.CombustibleLiquid;
+            }
+
+        }
+    }
+}
+//
