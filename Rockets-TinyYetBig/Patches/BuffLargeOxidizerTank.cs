a0a4d051399fbbbf538da02d73314b202654e912
Knastoron <bene.schlai@gmx.de>
2022-10-19 03:12:10 +0200

    oxidizer module buff

---
+++ Rockets-TinyYetBig/Patches/BuffLargeOxidizerTank.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Rockets_TinyYetBig.Patches
+{
+    class BuffLargeOxidizerTank
+    {
+        [HarmonyPatch(typeof(OxidizerTankClusterConfig), "DoPostConfigureComplete")]
+        public static class IncreaseCapacityto1350
+        {
+            public static void Postfix(GameObject go)
+            {
+                OxidizerTank oxidizerTank = go.AddOrGet<OxidizerTank>();
+                oxidizerTank.targetFillMass = 1350f;
+                oxidizerTank.maxFillMass = 1350f;
+            }
+
+        }
+        
+    }
+}
+//
