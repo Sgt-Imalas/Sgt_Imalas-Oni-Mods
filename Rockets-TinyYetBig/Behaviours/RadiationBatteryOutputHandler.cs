b976d6096fd7b47958a55a0c017113308430d62f
Knastoron <bene.schlai@gmx.de>
2022-11-08 01:43:34 +0100

    shifted battery logic to behavs.

--- Rockets-TinyYetBig/Behaviours/RadiationBatteryOutputHandler.cs
+++ Rockets-TinyYetBig/Behaviours/RadiationBatteryOutputHandler.cs
@@ -7,9 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Rockets_TinyYetBig
+namespace Rockets_TinyYetBig.Behaviours
 {
-    class RadiationBatteryOutputHandler : KMonoBehaviour, 
+    class RadiationBatteryOutputHandler : KMonoBehaviour,
         IHighEnergyParticleDirection,
         ISim200ms, ISaveLoadable,
         //IUserControlledCapacity, 
@@ -22,7 +22,7 @@ namespace Rockets_TinyYetBig
         private MeterController directionController;
 
         private static readonly EventSystem.IntraObjectHandler<RadiationBatteryOutputHandler> OnStorageChangedDelegate
-            = new EventSystem.IntraObjectHandler<RadiationBatteryOutputHandler>((System.Action<RadiationBatteryOutputHandler, object>)((component, data) => component.OnStorageChange(data)));
+            = new EventSystem.IntraObjectHandler<RadiationBatteryOutputHandler>((component, data) => component.OnStorageChange(data));
 
         [MyCmpReq]
         public HighEnergyParticleStorage hepStorage;
@@ -30,7 +30,7 @@ namespace Rockets_TinyYetBig
 
 
 
-        public bool AllowSpawnParticles => this.hasLogicWire && this.isLogicActive;
+        public bool AllowSpawnParticles => hasLogicWire && isLogicActive;
         private bool hasLogicWire;
         private bool isLogicActive;
         private float launchTimer = 0;
@@ -38,10 +38,10 @@ namespace Rockets_TinyYetBig
         public void Sim200ms(float dt)
         {
             launchTimer += dt;
-            if ((double)launchTimer < (double)minLaunchInterval || !AllowSpawnParticles || (double)hepStorage.Particles < (double)particleThreshold)
+            if (launchTimer < (double)minLaunchInterval || !AllowSpawnParticles || (double)hepStorage.Particles < particleThreshold)
                 return;
             launchTimer = 0.0f;
-            this.Fire();
+            Fire();
         }
 
         private void OnLogicValueChanged(object data)
@@ -49,38 +49,38 @@ namespace Rockets_TinyYetBig
             LogicValueChanged logicValueChanged = (LogicValueChanged)data;
             if (!(logicValueChanged.portID == HEPBattery.FIRE_PORT_ID))
                 return;
-            this.isLogicActive = logicValueChanged.newValue > 0;
-            this.hasLogicWire = this.GetNetwork() != null;
+            isLogicActive = logicValueChanged.newValue > 0;
+            hasLogicWire = GetNetwork() != null;
         }
-        private LogicCircuitNetwork GetNetwork() => Game.Instance.logicCircuitManager.GetNetworkForCell(this.GetComponent<LogicPorts>().GetPortCell(HEPBattery.FIRE_PORT_ID));
+        private LogicCircuitNetwork GetNetwork() => Game.Instance.logicCircuitManager.GetNetworkForCell(GetComponent<LogicPorts>().GetPortCell(HEPBattery.FIRE_PORT_ID));
 
         public int GetCircularHEPOutputCell()
         {
             int x = 0, y = 0;
-            if (this.Direction.ToString().Contains("Down"))
+            if (Direction.ToString().Contains("Down"))
                 y -= 1;
-            else if (this.Direction.ToString().Contains("Up"))
+            else if (Direction.ToString().Contains("Up"))
                 y += 1;
-            if (this.Direction.ToString().Contains("Right"))
+            if (Direction.ToString().Contains("Right"))
                 x += 1;
-            else if (this.Direction.ToString().Contains("Left"))
+            else if (Direction.ToString().Contains("Left"))
                 x -= 1;
-            var build = this.GetComponent<Building>();
+            var build = GetComponent<Building>();
 
             var offset = build.GetHighEnergyParticleOutputOffset();
             offset.x += x;
             offset.y += y;
 
-            int cell = Grid.OffsetCell(this.GetComponent<Building>().GetCell(), offset);
+            int cell = Grid.OffsetCell(GetComponent<Building>().GetCell(), offset);
             return cell;
         }
 
         public void Fire()
         {
-            int particleOutputCell = this.GetCircularHEPOutputCell();
+            int particleOutputCell = GetCircularHEPOutputCell();
             GameObject gameObject = GameUtil.KInstantiate(Assets.GetPrefab((Tag)"HighEnergyParticle"), Grid.CellToPosCCC(particleOutputCell, Grid.SceneLayer.FXFront2), Grid.SceneLayer.FXFront2);
             gameObject.SetActive(true);
-            if (!((UnityEngine.Object)gameObject != (UnityEngine.Object)null))
+            if (!(gameObject != null))
                 return;
             HighEnergyParticle component = gameObject.GetComponent<HighEnergyParticle>();
             component.payload = hepStorage.ConsumeAndGet(particleThreshold);
@@ -90,16 +90,16 @@ namespace Rockets_TinyYetBig
 
         //public LocString CapacityUnits => UI.UNITSUFFIXES.HIGHENERGYPARTICLES.PARTRICLES;
 
-        public IStorage Storage => (IStorage)this.hepStorage;
+        public IStorage Storage => hepStorage;
         public EightDirection Direction
         {
-            get => this._direction;
+            get => _direction;
             set
             {
-                this._direction = value;
-                if (this.directionController == null)
+                _direction = value;
+                if (directionController == null)
                     return;
-                this.directionController.SetPositionPercent( (45f*EightDirectionUtil.GetDirectionIndex(this._direction))/360f);
+                directionController.SetPositionPercent(45f * EightDirectionUtil.GetDirectionIndex(_direction) / 360f);
             }
         }
 
@@ -108,17 +108,17 @@ namespace Rockets_TinyYetBig
         public float physicalFuelCapacity;
         public float UserMaxCapacity
         {
-            get => this.hepStorage.capacity;
+            get => hepStorage.capacity;
             set
             {
-                this.hepStorage.capacity = value;
-                this.Trigger((int)GameHashes.ParticleStorageCapacityChanged, (object)this);
+                hepStorage.capacity = value;
+                Trigger((int)GameHashes.ParticleStorageCapacityChanged, this);
             }
         }
         public float MinCapacity => 0.0f;
-        public float MaxCapacity => this.physicalFuelCapacity;
+        public float MaxCapacity => physicalFuelCapacity;
 
-        public float AmountStored => this.hepStorage.Particles;
+        public float AmountStored => hepStorage.Particles;
 
         public bool WholeValues => false;
         #endregion
@@ -136,21 +136,21 @@ namespace Rockets_TinyYetBig
 
 
 
-            this.m_meter = new MeterController((KAnimControllerBase)this.GetComponent<KBatchedAnimController>(), "meter_target", "meter", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, Array.Empty<string>());
-            this.m_meter.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
-            this.directionController = new MeterController((KAnimControllerBase)this.GetComponent<KBatchedAnimController>(), "redirector_target", "redirector", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, Array.Empty<string>());
-            this.directionController.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
+            m_meter = new MeterController(GetComponent<KBatchedAnimController>(), "meter_target", "meter", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, Array.Empty<string>());
+            m_meter.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
+            directionController = new MeterController(GetComponent<KBatchedAnimController>(), "redirector_target", "redirector", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, Array.Empty<string>());
+            directionController.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
 
             Direction = Direction;
-            this.OnStorageChange((object)null);
+            OnStorageChange(null);
             //this.Subscribe<RadiationBatteryOutputHandler>((int)GameHashes.ParticleStorageCapacityChanged, OnStorageChangedDelegate);
-            this.Subscribe<RadiationBatteryOutputHandler>((int)GameHashes.OnParticleStorageChanged, OnStorageChangedDelegate);
-            this.Subscribe((int)GameHashes.LogicEvent, new System.Action<object>(this.OnLogicValueChanged));
+            Subscribe((int)GameHashes.OnParticleStorageChanged, OnStorageChangedDelegate);
+            Subscribe((int)GameHashes.LogicEvent, new Action<object>(OnLogicValueChanged));
 
         }
 
 
-        private void OnStorageChange(object data) => this.m_meter.SetPositionPercent(this.hepStorage.Particles / Mathf.Max(1f, this.hepStorage.capacity));
+        private void OnStorageChange(object data) => m_meter.SetPositionPercent(hepStorage.Particles / Mathf.Max(1f, hepStorage.capacity));
         protected override void OnCleanUp()
         {
             base.OnCleanUp();
@@ -170,17 +170,17 @@ namespace Rockets_TinyYetBig
 
         public int SliderDecimalPlaces(int index) => 0;
 
-        public float GetSliderMin(int index) => (float)25;
+        public float GetSliderMin(int index) => 25;
 
-        public float GetSliderMax(int index) => (float)250;
+        public float GetSliderMax(int index) => 250;
 
-        public float GetSliderValue(int index) => this.particleThreshold;
+        public float GetSliderValue(int index) => particleThreshold;
 
-        public void SetSliderValue(float value, int index) => this.particleThreshold = value;
+        public void SetSliderValue(float value, int index) => particleThreshold = value;
 
         public string GetSliderTooltipKey(int index) => "STRINGS.UI.UISIDESCREENS.RADBOLTTHRESHOLDSIDESCREEN.TOOLTIP";
 
-        string ISliderControl.GetSliderTooltip() => string.Format((string)Strings.Get("STRINGS.UI.UISIDESCREENS.RADBOLTTHRESHOLDSIDESCREEN.TOOLTIP"), (object)this.particleThreshold);
+        string ISliderControl.GetSliderTooltip() => string.Format((string)Strings.Get("STRINGS.UI.UISIDESCREENS.RADBOLTTHRESHOLDSIDESCREEN.TOOLTIP"), particleThreshold);
 
         #endregion
     }
