afdcf35ffd7d8ea73f997aca6b15fcb512f71166
Knastoron <bene.schlai@gmx.de>
2022-11-08 03:42:19 +0100

    Refactoring into component bc sidescreen to smi is stoopid, mod DONE

---
+++ RotatableRadboltStorage/BatteryDirectionAddon.cs
@@ -0,0 +1,78 @@
+﻿using KSerialization;
+using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RotatableRadboltStorage
+{
+    class BatteryDirectionAddon : KMonoBehaviour,
+        IHighEnergyParticleDirection,
+        ISaveLoadable
+    {
+        [MyCmpReq]
+        private KSelectable selectable;
+        [Serialize]
+        private EightDirection _direction;
+        private MeterController directionController;
+        
+        public int GetCircularHEPOutputCell()
+        {
+            int x = 0, y = 0;
+            if (Direction.ToString().Contains("Down"))
+                y -= 1;
+            else if (Direction.ToString().Contains("Up"))
+                y += 1;
+            if (Direction.ToString().Contains("Right"))
+                x += 1;
+            else if (Direction.ToString().Contains("Left"))
+                x -= 1;
+            var build = GetComponent<Building>();
+
+            var offset = build.GetHighEnergyParticleOutputOffset();
+            offset.x += x;
+            offset.y += y;
+
+            int cell = Grid.OffsetCell(GetComponent<Building>().GetCell(), offset);
+            return cell;
+        }
+
+        //public void Fire()
+        //{
+        //    int particleOutputCell = GetCircularHEPOutputCell();
+        //    GameObject gameObject = GameUtil.KInstantiate(Assets.GetPrefab((Tag)"HighEnergyParticle"), Grid.CellToPosCCC(particleOutputCell, Grid.SceneLayer.FXFront2), Grid.SceneLayer.FXFront2);
+        //    gameObject.SetActive(true);
+        //    if (!(gameObject != null))
+        //        return;
+        //    HighEnergyParticle component = gameObject.GetComponent<HighEnergyParticle>();
+        //    component.payload = hepStorage.ConsumeAndGet(particleThreshold);
+        //    component.SetDirection(Direction);
+        //}
+
+
+        //public LocString CapacityUnits => UI.UNITSUFFIXES.HIGHENERGYPARTICLES.PARTRICLES;
+
+        public EightDirection Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                if (directionController == null)
+                    return;
+                directionController.SetPositionPercent(45f * EightDirectionUtil.GetDirectionIndex(_direction) / 360f);
+            }
+        }
+
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            directionController = new MeterController(GetComponent<KBatchedAnimController>(), "redirector_target", "redirector", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, Array.Empty<string>());
+            directionController.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
+            Direction = Direction;
+        }
+    }
+}
--- RotatableRadboltStorage/Patches.cs
+++ RotatableRadboltStorage/Patches.cs
@@ -8,33 +8,77 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UtilLibs;
+using static KAnim;
 
 namespace RotatableRadboltStorage
 {
     internal class Patches
     {
-        /// <summary>
-        /// add buildings to plan screen
-        /// </summary>
-        [HarmonyPatch(typeof(GeneratedBuildings))]
-        [HarmonyPatch(nameof(GeneratedBuildings.LoadGeneratedBuildings))]
-        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
-        {
 
-            public static void Prefix()
+        /// <summary>
+        /// add destination selection sidescreen to rad battery
+        /// </summary>
+        [HarmonyPatch(typeof(HighEnergyParticleDirectionSideScreen))]
+        [HarmonyPatch(nameof(HighEnergyParticleDirectionSideScreen.IsValidForTarget))]
+        public static class AddTargetValidityForRadBattery
+        {
+            public static bool Prefix(GameObject target, ref bool __result)
             {
-                //ModUtil.AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.XXXX, XXXX.ID);
+                if (target.TryGetComponent<BatteryDirectionAddon>(out var targetComponent))
+                {
+                    __result = true;
+                    return false;
+                }
+                return true;
             }
         }
-        /// <summary>
-        /// Init. auto translation
-        /// </summary>
-        [HarmonyPatch(typeof(Localization), "Initialize")]
-        public static class Localization_Initialize_Patch
+
+        [HarmonyPatch(typeof(HEPBattery))]
+        [HarmonyPatch(nameof(HEPBattery.Fire))]
+        public static class TargetCellForRadBattery
         {
-            public static void Postfix()
+            public static bool Prefix(HEPBattery.Instance smi)
             {
-                LocalisationUtil.Translate(typeof(STRINGS), true);
+                var DirectionAddon = smi.GetComponent<BatteryDirectionAddon>();
+                if (DirectionAddon != null)
+                {
+                    int particleOutputCell = DirectionAddon.GetCircularHEPOutputCell();
+                    GameObject gameObject = GameUtil.KInstantiate(Assets.GetPrefab((Tag)"HighEnergyParticle"), Grid.CellToPosCCC(particleOutputCell, Grid.SceneLayer.FXFront2), Grid.SceneLayer.FXFront2);
+                    gameObject.SetActive(true);
+                    if (!(gameObject != null))
+                        return false;
+                    HighEnergyParticle component = gameObject.GetComponent<HighEnergyParticle>();
+                    component.payload = smi.particleStorage.ConsumeAndGet(smi.particleThreshold);
+                    component.SetDirection(DirectionAddon.Direction);
+
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(HEPBatteryConfig))]
+        [HarmonyPatch(nameof(HEPBatteryConfig.CreateBuildingDef))]
+        public static class AdjustNormalHEPBattery
+        {
+            public static void Postfix(ref BuildingDef __result)
+            {
+                __result
+                    .HighEnergyParticleOutputOffset = new CellOffset(0, 1);
+                __result.AnimFiles = new KAnimFile[1]
+                {
+                    Assets.GetAnim((HashedString) "radbolt_battery_rotatable_kanim")
+                };
+
+            }
+        }
+        [HarmonyPatch(typeof(HEPBatteryConfig))]
+        [HarmonyPatch(nameof(HEPBatteryConfig.ConfigureBuildingTemplate))]
+        public static class AdjustNormalHEPBatteryTwo
+        {
+            public static void Postfix(GameObject go)
+            {
+                go.AddOrGet<BatteryDirectionAddon>();
             }
         }
     }
