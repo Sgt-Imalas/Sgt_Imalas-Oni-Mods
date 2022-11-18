ac8e2f512f47eabd9821426f6b8fdacaeedc6ce6
Knastoron <bene.schlai@gmx.de>
2022-11-18 21:18:09 +0100

    solid loader now blocks foods that have no business to be in a cargo bay if no food module is installed

---
+++ Rockets-TinyYetBig/Patches/FixRocketConduitStorageAccess.cs
@@ -0,0 +1,110 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Operational;
+using static STRINGS.BUILDINGS.PREFABS;
+
+namespace Rockets_TinyYetBig.Patches
+{
+    internal class FixRocketConduitStorageAccess
+    {
+        /// <summary>
+        /// This throws out food that is not allowed in cargo bays
+        /// </summary>
+        [HarmonyPatch(typeof(RocketConduitStorageAccess), "Sim200ms")]
+        public static class OnlyAllowStorageFilterItemsInStorage
+        {
+
+            public static bool CouldStorageAllowThisTag(Tag tagAllowed, Storage storage)
+            {
+                bool flag = false;
+                foreach (Tag storageFilter in storage.storageFilters)
+                {
+                    if (DiscoveredResources.Instance.GetDiscoveredResourcesFromTag(storageFilter).Contains(tagAllowed))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                return flag;
+            }
+            public static bool Prefix(float dt, 
+                RocketConduitStorageAccess __instance, 
+                Operational ___operational, 
+                CraftModuleInterface ___craftModuleInterface,
+                Filterable ___filterable)
+            {
+                if (___operational != null && !___operational.IsOperational)
+                    return false;
+                float currentMassStored = __instance.storage.MassStored();
+                if ((double)currentMassStored >= (double)__instance.targetLevel - 0.01f && (double)currentMassStored <= (double)__instance.targetLevel + 0.01f)
+                    return false;
+                if (___operational != null)
+                    ___operational.SetActive(true);
+                float amount = __instance.targetLevel - currentMassStored;
+
+                bool NoFittingStorageForTags = false;
+
+                foreach (Ref<RocketModuleCluster> clusterModule in (IEnumerable<Ref<RocketModuleCluster>>)___craftModuleInterface.ClusterModules)
+                {
+                    CargoBayCluster cargoBay = clusterModule.Get().GetComponent<CargoBayCluster>();
+
+                    if (cargoBay != null && cargoBay.storageType == __instance.cargoType)
+                    {
+                        if ((double)amount > 0.0 && (double)cargoBay.storage.MassStored() > 0.0 )
+                        {
+                            for (int index = cargoBay.storage.items.Count - 1; index >= 0; --index)
+                            {
+                                GameObject go = cargoBay.storage.items[index];
+                                if ((!(___filterable != null) || !(___filterable.SelectedTag != GameTags.Void) || !(go.PrefabID() != ___filterable.SelectedTag))
+                                    )
+                                {
+                                    Pickupable pickupable = go.GetComponent<Pickupable>().Take(amount);
+                                    if (pickupable != null)
+                                    {
+                                        amount -= pickupable.PrimaryElement.Mass;
+                                        __instance.storage.Store(pickupable.gameObject, true);
+                                    }
+                                    if ((double)amount <= 0.0)
+                                        break;
+                                }
+                            }
+                            if ((double)amount <= 0.0)
+                                break;
+                        }
+                        if ((double)amount < 0.0 && (double)cargoBay.storage.RemainingCapacity() > 0.0)
+                        {
+                            double num2 = (double)Mathf.Min(-amount, cargoBay.storage.RemainingCapacity());
+                            for (int index = __instance.storage.items.Count - 1; index >= 0; --index)
+                            {
+                                if (__instance.storage.items[index] != null &&
+                                    __instance.cargoType == CargoBay.CargoType.Solids &&
+                                    !CouldStorageAllowThisTag(__instance.storage.items[index].PrefabID(), cargoBay.storage))
+                                {
+                                    break;
+                                }
+                                Pickupable pickupable = __instance.storage.items[index].GetComponent<Pickupable>().Take(-amount);
+
+                                if (pickupable != null)
+                                {
+                                        amount += pickupable.PrimaryElement.Mass;
+                                        cargoBay.storage.Store(pickupable.gameObject, true);
+                                }
+                                if ((double)amount >= 0.0)
+                                    break;
+                            }
+                            if ((double)amount >= 0.0)
+                                break;
+                        }
+                    }
+                }
+                return false;
+            }
+
+        }
+    }
+}
