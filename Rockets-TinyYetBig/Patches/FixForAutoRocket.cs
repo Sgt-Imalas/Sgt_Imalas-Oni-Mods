9c2061302f4f35a9492a66f3e41a85c2133d7bba
Knastoron <bene.schlai@gmx.de>
2022-11-25 01:01:20 +0100

    Fix for autorocket crash

---
+++ Rockets-TinyYetBig/Patches/FixForAutoRocket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Rockets_TinyYetBig.Patches
+{
+    class FixForAutoRocket
+    {
+        [HarmonyPatch(typeof(ElementLoader), "GetElement")]
+        public static class FixAutoRocket
+        {
+            public static void Postfix(Tag tag, ref Element __result)
+            {
+                if(tag== GameTags.CombustibleLiquid && __result == default(Element))
+                {
+                    ElementLoader.elementTagTable.TryGetValue(SimHashes.Petroleum.CreateTag(), out __result);
+                }
+            }
+
+        }
+    }
+}
