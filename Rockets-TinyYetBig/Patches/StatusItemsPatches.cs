82b039dabffb3881c44ca65b4dd0fcfc9ccb2965
Knastoron <bene.schlai@gmx.de>
2022-12-08 15:24:23 +0100

    Status items on rocket fixed

--- Rockets-TinyYetBig/Patches/StatusItemsPatches.cs
+++ Rockets-TinyYetBig/Patches/StatusItemsPatches.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using static STRINGS.BUILDINGS.PREFABS;
 
 namespace Rockets_TinyYetBig.Patches
@@ -30,6 +31,8 @@ namespace Rockets_TinyYetBig.Patches
         [HarmonyPatch(nameof(Clustercraft.UpdateStatusItem))]
         public static class GeneratorModuleStatusItems
         {
+            static Dictionary<KSelectable, Guid> batteryStatusItemGuids = new Dictionary<KSelectable, Guid>();
+            static Dictionary<KSelectable, Guid> generatorStatusItemGuids = new Dictionary<KSelectable, Guid>();
             public static void Postfix(Clustercraft __instance)
             {
                 KSelectable selectable = __instance.GetComponent<KSelectable>();
@@ -40,7 +43,7 @@ namespace Rockets_TinyYetBig.Patches
                 Tuple<float, float> data = new Tuple<float, float>(0, 0);
                 foreach (var module in __instance.ModuleInterface.ClusterModules)
                 {
-                    if(module.Get().gameObject.TryGetComponent<RTB_ModuleGenerator>(out var generator))
+                    if (module.Get().gameObject.TryGetComponent<RTB_ModuleGenerator>(out var generator))
                     {
                         var genStats = generator.GetConsumptionStatusStats();
                         data.first += genStats.first;
@@ -48,10 +51,31 @@ namespace Rockets_TinyYetBig.Patches
                         //generator.FuelStatusHandle =
                     }
                 }
-                if(data.first > 0 || data.second >0)
-                    selectable.AddStatusItem( ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus, (object)data);
+
+                if (data.first > 0 || data.second > 0)
+                    selectable.SetStatusItem(Db.Get().StatusItemCategories.Power, ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus, (object)data);
                 else
                     selectable.RemoveStatusItem(ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus);
+
+                //if (data.first > 0 || data.second > 0)
+                //{
+                //    if (!generatorStatusItemGuids.ContainsKey(selectable))
+                //    {
+                //        generatorStatusItemGuids.Add(selectable, selectable.AddStatusItem(ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus, (object)data));
+                //    }
+                //    else
+                //    {
+                //        generatorStatusItemGuids[selectable] = selectable.ReplaceStatusItem(generatorStatusItemGuids[selectable], ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus, (object)data);
+                //    }
+                //}
+                //else
+                //{
+                //    selectable.RemoveStatusItem(ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus);
+                //    if (generatorStatusItemGuids.ContainsKey(selectable))
+                //    {
+                //        generatorStatusItemGuids.Remove(selectable);
+                //    }
+                //}
 
 
                 Tuple<float, float> dataBattery = new Tuple<float, float>(0, 0);
@@ -65,9 +89,31 @@ namespace Rockets_TinyYetBig.Patches
                     }
                 }
                 if (dataBattery.first > 0 || dataBattery.second > 0)
-                    selectable.AddStatusItem(ModAssets.StatusItems.RTB_RocketBatteryStatus, (object)dataBattery);
+                    selectable.SetStatusItem(Db.Get().StatusItemCategories.OperatingEnergy, ModAssets.StatusItems.RTB_RocketBatteryStatus, (object)dataBattery);
                 else
                     selectable.RemoveStatusItem(ModAssets.StatusItems.RTB_RocketBatteryStatus);
+
+                //if (dataBattery.first > 0 || dataBattery.second > 0)
+                //{
+                //    if (!batteryStatusItemGuids.ContainsKey(selectable))
+                //    {
+                //        batteryStatusItemGuids.Add(selectable, selectable.AddStatusItem(ModAssets.StatusItems.RTB_RocketBatteryStatus, (object)dataBattery));
+                //    }
+                //    else
+                //    {
+                //        batteryStatusItemGuids[selectable] = selectable.ReplaceStatusItem(batteryStatusItemGuids[selectable], ModAssets.StatusItems.RTB_RocketBatteryStatus, (object)dataBattery);
+                //    }
+                //}
+                //else
+                //{
+                //    selectable.RemoveStatusItem(ModAssets.StatusItems.RTB_ModuleGeneratorFuelStatus);
+                //    if (batteryStatusItemGuids.ContainsKey(selectable))
+                //    {
+                //        batteryStatusItemGuids.Remove(selectable);
+                //    }
+                // }
+
+
             }
 
         }
@@ -82,12 +128,12 @@ namespace Rockets_TinyYetBig.Patches
                 {
                     __instance.ModuleSolarPanelWattage.resolveStringCallback = (Func<string, object, string>)((str, data) =>
                     {
-                        if(data is ModuleSolarPanel) 
-                        { 
+                        if (data is ModuleSolarPanel)
+                        {
                             ModuleSolarPanel moduleSolarPanel = (ModuleSolarPanel)data;
                             str = str.Replace("{Wattage}", GameUtil.GetFormattedWattage(moduleSolarPanel.CurrentWattage));
                         }
-                        else if(data is ModuleSolarPanelAdjustable)
+                        else if (data is ModuleSolarPanelAdjustable)
                         {
                             ModuleSolarPanelAdjustable moduleSolarPanel = (ModuleSolarPanelAdjustable)data;
                             str = str.Replace("{Wattage}", GameUtil.GetFormattedWattage(moduleSolarPanel.CurrentWattage));
@@ -100,6 +146,6 @@ namespace Rockets_TinyYetBig.Patches
             }
 
         }
-        
+
     }
 }
