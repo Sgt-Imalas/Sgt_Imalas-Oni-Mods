6b206d3bbb1357f7c762c3d0f8694d5f10c529bc
Knastoron <bene.schlai@gmx.de>
2022-12-17 20:28:12 +0100

    Upkeep

--- Rockets-TinyYetBig/SpaceStations/OrbitUpkeep/OrbitalUpkeepEngine.cs
+++ Rockets-TinyYetBig/SpaceStations/OrbitUpkeep/OrbitalUpkeepEngine.cs
@@ -7,10 +7,55 @@ using System.Threading.Tasks;
 
 namespace Rockets_TinyYetBig.SpaceStations.OrbitUpkeep
 {
-    internal class OrbitalUpkeepEngine : KMonoBehaviour
+    internal class OrbitalUpkeepEngine : KMonoBehaviour, ISim1000ms
     {
         [Serialize]
         Ref<OrbitalUpkeepObject> worldUpkeepMng;
+        [MyCmpGet]
+        Operational operational;
+
+        [MyCmpGet]
+        Storage storage;
+
+        public readonly CellOffset[] tier1Engine =
+        {
+            new CellOffset(0,-1),
+        };
+
+
+
+        public Tag fuelElement = SimHashes.Void.CreateTag();
+        public float consumptionRate = 1f;
+
+        public SimHashes exhaustElement = SimHashes.CarbonDioxide;
+        public float exhaustProductionRate = 10f;
+        public float exhaustProductionTemperature = 293.15f;
+
+
+        public void Sim1000ms(float dt)
+        {
+            if (operational.IsOperational && this.HasFuelForBurn())
+            {
+                this.ConsumeFuel();
+                this.ProvideUpkeep();
+            }
+        }
+
+        private bool HasFuelForBurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConsumeFuel()
+        {
+
+        }
+
+        private void ProvideUpkeep()
+        {
+            throw new NotImplementedException();
+        }
+
         protected override void OnCleanUp()
         {
             base.OnCleanUp();
