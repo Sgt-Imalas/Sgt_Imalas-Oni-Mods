8568bc756bed9afcc611a727d46e11b378234165
Knastoron <bene.schlai@gmx.de>
2022-12-24 19:45:54 +0100

    shock wörm idea

---
+++ ShockWormMob/Patches.cs
@@ -0,0 +1,151 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using PeterHan.PLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUNING;
+using UnityEngine;
+using UtilLibs;
+using static ShockWormMob.ModAssets;
+
+namespace ShockWormMob
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// add buildings to plan screen
+        /// </summary>
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch(nameof(GeneratedBuildings.LoadGeneratedBuildings))]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+
+            public static void Prefix()
+            {
+                //ModUtil.AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.XXXX, XXXX.ID);
+            }
+        }
+        /// <summary>
+        /// Init. auto translation
+        /// </summary>
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+        public static class Localization_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                LocalisationUtil.Translate(typeof(STRINGS), true);
+            }
+        }
+
+        [HarmonyPatch(typeof(ShockwormConfig), "CreatePrefab")]
+        public static class Make_Creature
+        {
+            public static void Postfix(GameObject __result)
+            {
+
+                __result.AddOrGetDef<OvercrowdingMonitor.Def>().spaceRequiredPerCreature = CREATURES.SPACE_REQUIREMENTS.TIER1;
+                ChoreTable.Builder chore_table =
+                    new ChoreTable.Builder()
+                    .Add((StateMachine.BaseDef)new DeathStates.Def())
+                    .Add((StateMachine.BaseDef)new AnimInterruptStates.Def())
+                    .Add((StateMachine.BaseDef)new TrappedStates.Def())
+                    .Add((StateMachine.BaseDef)new BaggedStates.Def())
+                    .Add((StateMachine.BaseDef)new StunnedStates.Def())
+                    .Add((StateMachine.BaseDef)new FallStates.Def())
+                    .Add((StateMachine.BaseDef)new DebugGoToStates.Def())
+                    .Add((StateMachine.BaseDef)new DrowningStates.Def())
+                    .Add((StateMachine.BaseDef)new AttackStates.Def("attack_loop", "attack_pst"))
+                    .Add((StateMachine.BaseDef)new IdleStates.Def());
+                EntityTemplates.AddCreatureBrain(__result, chore_table, ShockwormConfigSpeciesID, "");
+                KPrefabID component = __result.GetComponent<KPrefabID>(); 
+                component.AddTag(GameTags.Creatures.Flyer);
+
+                __result.AddOrGetDef<ThreatMonitor.Def>();
+                __result.AddOrGetDef<AgeMonitor.Def>();
+
+                Trait trait = Db.Get().CreateTrait("ShockWormBaseBaseTrait", "Shock Worm", "Shock Worm", (string)null, false, (ChoreGroup[])null, true, true);
+                trait.Add(new AttributeModifier(Db.Get().Amounts.HitPoints.maxAttribute.Id, 15f, "Shock Worm"));
+                trait.Add(new AttributeModifier(Db.Get().Amounts.Age.maxAttribute.Id, 25f, "Shock Worm"));
+
+                Modifiers modifiers = __result.AddOrGet<Modifiers>();
+                modifiers.initialTraits.Add("ShockWormBaseBaseTrait");
+                SoundEventVolumeCache.instance.AddVolume("shockworm_kanim", "attack_loop", NOISE_POLLUTION.CREATURES.TIER6);
+
+            }
+        }
+        [HarmonyPatch(typeof(CodexEntryGenerator), "GenerateCreatureEntries")]
+        public class CodexEntryGenerator_GenerateCreatureEntries_Patch
+        {
+            internal delegate void GenerateImageContainers(Sprite[] sprites,
+            List<ContentContainer> containers, ContentContainer.ContentLayout layout);
+
+            internal delegate void GenerateCreatureDescriptionContainers(GameObject creature,
+            List<ContentContainer> containers);
+
+            internal static readonly GenerateImageContainers GENERATE_IMAGE_CONTAINERS =
+            typeof(CodexEntryGenerator).CreateStaticDelegate<GenerateImageContainers>(
+            nameof(GenerateImageContainers), typeof(Sprite[]), typeof(List<ContentContainer>),
+            typeof(ContentContainer.ContentLayout));
+
+
+            internal static readonly GenerateCreatureDescriptionContainers GENERATE_DESC =
+                typeof(CodexEntryGenerator).CreateStaticDelegate<GenerateCreatureDescriptionContainers>(
+                nameof(GenerateCreatureDescriptionContainers), typeof(GameObject),
+                typeof(List<ContentContainer>));
+
+            private static void AddToCodex(Tag speciesTag, string name,
+                IDictionary<string, CodexEntry> results)
+            {
+                string tagStr = speciesTag.ToString();
+                var brains = Assets.GetPrefabsWithComponent<CreatureBrain>();
+                var entry = new CodexEntry(nameof(global::STRINGS.CREATURES), new List<ContentContainer> {
+                new ContentContainer(new List<ICodexWidget> {
+                    new CodexSpacer(),
+                    new CodexSpacer()
+                }, ContentContainer.ContentLayout.Vertical)
+            }, name)
+                {
+                    parentId = nameof(global::STRINGS.CREATURES)
+                };
+                CodexCache.AddEntry(tagStr, entry);
+                results.Add(tagStr, entry);
+                // Find all critters with this tag
+                foreach (var prefab in brains)
+                    if (prefab.GetDef<BabyMonitor.Def>() == null && prefab.TryGetComponent(
+                            out CreatureBrain brain) && brain.species == speciesTag)
+                    {
+                        Sprite babySprite = null;
+                        string prefabID = prefab.PrefabID().Name;
+                        var baby = Assets.TryGetPrefab(prefabID + "Baby");
+                        var contentContainerList = new List<ContentContainer>(4);
+                        var first = Def.GetUISprite(prefab, brain.symbolPrefix + "ui").first;
+                        if (baby != null)
+                            babySprite = Def.GetUISprite(baby).first;
+                        if (babySprite != null)
+                            GENERATE_IMAGE_CONTAINERS.Invoke(new[] { first, babySprite },
+                                contentContainerList, ContentContainer.ContentLayout.Horizontal);
+                        else
+                            contentContainerList.Add(new ContentContainer(new List<ICodexWidget> {
+                            new CodexImage(128, 128, first)
+                        }, ContentContainer.ContentLayout.Vertical));
+                        GENERATE_DESC.Invoke(prefab, contentContainerList);
+                        entry.subEntries.Add(new SubEntry(prefabID, tagStr, contentContainerList,
+                                prefab.GetProperName())
+                        {
+                            icon = first,
+                            iconColor = Color.white
+                        });
+                    }
+            }
+            public static void Postfix(Dictionary<string, CodexEntry> __result)
+            {
+                AddToCodex(ShockwormConfigSpeciesID, "Shock worms", __result);
+            }
+        }
+
+    }
+}
---
+++ ShockWormMob/STRINGS.cs
@@ -0,0 +1,30 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShockWormMob
+{
+    internal class STRINGS
+    {
+
+        public class CREATURES
+        {
+            public class FAMILY_PLURAL
+            {
+                public static LocString SHOCKWORMSPECIES = "Shock Worms";
+
+            }
+        }
+
+        public class BUILDINGS
+        {
+            public class PREFABS
+            {
+
+            }
+        }
+    }
+}
