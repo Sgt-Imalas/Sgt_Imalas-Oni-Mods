029eace8bcaa6ba4a81094e9ee404254ad4f151f
Knastoron <bene.schlai@gmx.de>
2023-01-08 21:26:03 +0100

    Reactive Change on BuffOxidizerModule

--- Rockets-TinyYetBig/Patches/BuffLargeOxidizerTank.cs
+++ Rockets-TinyYetBig/Patches/BuffLargeOxidizerTank.cs
@@ -10,21 +10,39 @@ namespace Rockets_TinyYetBig.Patches
 {
     class BuffLargeOxidizerTank
     {
-        [HarmonyPatch(typeof(OxidizerTankClusterConfig), "DoPostConfigureComplete")]
+        //[HarmonyPatch(typeof(OxidizerTankClusterConfig), "DoPostConfigureComplete")]
+        //public static class IncreaseCapacityto1350
+        //{
+        //    public static void Postfix(GameObject go)
+        //    {
+        //        if (Config.Instance.BuffLargeOxidizer)
+        //        {
+        //            OxidizerTank oxidizerTank = go.AddOrGet<OxidizerTank>();
+        //            oxidizerTank.targetFillMass = 1350f;
+        //            oxidizerTank.maxFillMass = 1350f;
+        //        }
+        //    }
+
+        //}
+        [HarmonyPatch(typeof(OxidizerTank), "OnSpawn")]
         public static class IncreaseCapacityto1350
         {
-            public static void Postfix(GameObject go)
+            public static void Postfix(OxidizerTank __instance)
             {
-                if (Config.Instance.BuffLargeOxidizer)
+                if (Config.Instance.BuffLargeOxidizer && __instance.maxFillMass == 900f)
                 {
-                    OxidizerTank oxidizerTank = go.AddOrGet<OxidizerTank>();
-                    oxidizerTank.targetFillMass = 1350f;
-                    oxidizerTank.maxFillMass = 1350f;
+                    __instance.maxFillMass = 1350f;
+                }
+                else if (!Config.Instance.BuffLargeOxidizer && __instance.maxFillMass == 1350f)
+                {
+                    if (__instance.targetFillMass > 900f)
+                        __instance.targetFillMass = 900f;
+                    __instance.maxFillMass = 900f;
                 }
             }
 
         }
-        
+
     }
 }
 //
