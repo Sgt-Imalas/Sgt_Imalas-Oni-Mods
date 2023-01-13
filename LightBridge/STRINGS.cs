8565e0f7a5eaed74eb57a51826054501eb877d07
Knastoron <bene.schlai@gmx.de>
2023-01-13 19:26:42 +0100

    light bridge strings

--- LightBridge/STRINGS.cs
+++ LightBridge/STRINGS.cs
@@ -16,6 +16,13 @@ namespace LightBridge
             public class PREFABS
             {
 
+                public class LB_LIGHTBRIDGEEMITTER
+                {
+
+                    public static LocString NAME = (LocString)"Light Bridge Emitter";
+                    public static LocString DESC = (LocString)"These bridges are made from light. If you rubbed your cheek on one, it would be like standing outside with the sun shining on your face. It would also set your hair on fire, so don't actually do it.";
+                    public static LocString EFFECT = (LocString)"Emitts a semi-permeable light bridge of configurable length.\n\nLonger bridges require higher amounts of power";
+                }
             }
         }
         public class UI
