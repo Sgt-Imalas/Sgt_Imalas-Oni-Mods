e237580f71c72d08b000cb5b9736d091c690e0c4
Knastoron <bene.schlai@gmx.de>
2023-01-19 00:09:28 +0100

    further event data

---
+++ Rockets-TinyYetBig/TwitchEvents/Events/RocketBoostEvent.cs
@@ -0,0 +1,43 @@
+﻿using ONITwitchLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Rockets_TinyYetBig.TwitchEvents.Events
+{
+    internal class RocketBoostEvent : TwitchEventBase
+    {
+        public override string ID => "RTB_TwitchEvent_RocketBoost";
+        public override string EventName => "Rocket Boost";
+
+        public override Danger EventDanger => Danger.None;
+        public override string EventDescription => "A rocket has recieved a boost!";
+        public override EventWeight EventProbability => EventWeight.WEIGHT_NORMAL;
+        public override Func<object, bool> Condition 
+            =>
+                data =>
+                {
+                    foreach (Clustercraft craft in Components.Clustercrafts)
+                        if (craft.Status == Clustercraft.CraftStatus.InFlight)
+                            return true;
+
+                    return false;
+                };
+        public override Action<object> EventAction => (
+            data =>
+            {
+                List<Clustercraft> rockets = Components.Clustercrafts.Items;
+                rockets.ShuffleList();
+                foreach (Clustercraft craft in rockets)
+                    if (craft.Status == Clustercraft.CraftStatus.InFlight)
+                    {
+                        craft.controlStationBuffTimeRemaining = Math.Max(600f, (float)new System.Random().Next(300,900)) ;
+                        break;
+                    }
+                ToastManager.InstantiateToast(EventName, EventDescription);
+            });
+    }
+}
---
+++ Rockets-TinyYetBig/TwitchEvents/TwitchEventsInit.cs
@@ -0,0 +1,67 @@
+﻿using ONITwitchLib.Core;
+using ONITwitchLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DebugHandler;
+
+namespace Rockets_TinyYetBig.TwitchEvents
+{
+    internal class TwitchEventsInit
+    {
+        static bool initalized = false;
+        static EventManager eventInst;
+        static DataManager dataInst;
+        static TwitchDeckManager deckInst;
+
+        public const int WEIGHT_NEVER = 0;
+        public const int WEIGHT_ALMOST_NEVER = 1;
+        public const int WEIGHT_RARE = 3;
+        public const int WEIGHT_NORMAL = 10;
+        public const int WEIGHT_COMMON = 30;
+
+        public static void Init()
+        {
+            if (initalized)
+                return;
+
+            eventInst = EventManager.Instance;
+            dataInst = DataManager.Instance;
+            deckInst = TwitchDeckManager.Instance;
+
+            initalized = true;
+            Debug.Log("Rocketry Expanded: Initalized Twitch integration");
+        }
+        public static void RegisterAll()
+        {
+            if (!TwitchModInfo.TwitchIsPresent)
+            {
+                Debug.LogWarning($"Rocketry Expanded: Twitch not enabled!");
+                return;
+            }
+
+            Init();
+
+            RegisterEvent(new MandatoryTesting(WEIGHT_COMMON));
+        }
+
+
+        public static void RegisterEvent(TwitchEventBase twitchEvent)
+        {
+            if (twitchEvent.GetWeight == WEIGHT_NEVER)
+                return;
+
+            var deckInst = TwitchDeckManager.Instance;
+
+            var (_event, _eventGroup) = EventGroup.DefaultSingleEventGroup(twitchEvent.ID, twitchEvent.GetWeight, twitchEvent.EventName);
+            _event.AddListener(twitchEvent.EventAction);
+            _event.AddCondition(twitchEvent.Condition);
+            _event.Danger = twitchEvent.EventDanger;
+            deckInst.AddGroup(_eventGroup);
+        }
+
+
+    }
+}
