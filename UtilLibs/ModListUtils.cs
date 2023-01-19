b075b605e41cf7eeea1732011983fe263ade1c29
Admin <Admin@DESKTOP-I46AJNG>
2023-01-19 13:36:36 +0100

    Added ModIsActive helper

---
+++ UtilLibs/ModListUtils.cs
@@ -0,0 +1,25 @@
+﻿using KMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilLibs
+{
+    internal class ModListUtils
+    {
+        public static bool ModIsActive(string modId)
+        {
+            Manager modManager = Global.Instance.modManager;
+            foreach (var mod in modManager.mods)
+            {
+                if (!mod.IsEnabledForActiveDlc())
+                    continue;
+                if(mod.staticID== modId)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
