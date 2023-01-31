2a13780ede22d05add5f387264fe9eb40673ed63
Knastoron <bene.schlai@gmx.de>
2023-02-01 00:01:55 +0100

    blue eye spice

---
+++ Rockets-TinyYetBig/TwitchEvents/SpaceSpice/RocketeerSpicePatches.cs
@@ -0,0 +1,77 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Rockets_TinyYetBig.TwitchEvents.SpaceSpice
+{
+    internal class RocketeerSpicePatches
+    {
+        [HarmonyPatch(typeof(Edible), "StopConsuming")]
+        public static class PatchDroppingOfTincans
+        {
+            public static void Prefix(Worker worker, Edible __instance)
+            {
+                foreach (var spice in __instance.spices)
+                {
+                    Debug.Log(spice);
+                }
+                var rocketSpice = __instance.spices.Find((s) => s.Id == "PILOTING_SPICE");
+                if (!rocketSpice.Equals(default(SpiceInstance)))
+                {
+                    if(worker.TryGetComponent<SpiceEyes>(out var eyes))
+                    {
+                        eyes.AddEyeDuration(rocketSpice.StatBonus.duration);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AccessorySlots), MethodType.Constructor, typeof(ResourceSet))]
+        public class AccessorySlots_TargetMethod_Patch
+        {
+            public static void Postfix(AccessorySlots __instance, ResourceSet parent)
+            {
+                GlowyEyes.Register(__instance, parent);
+            }
+        }
+        public class GlowyEyes
+        {
+
+            public static void Register(AccessorySlots instance, ResourceSet parent)
+            {
+                AddAccessories(Assets.GetAnim("eye_spice_effect_kanim"), instance.Eyes, parent);
+            }
+
+            public static void AddAccessories(KAnimFile file, AccessorySlot slot, ResourceSet parent)
+            {
+                var build = file.GetData().build;
+                var id = slot.Id.ToLower();
+
+                for (var i = 0; i < build.symbols.Length; i++)
+                {
+                    var symbolName = HashCache.Get().Get(build.symbols[i].hash);
+                    if (symbolName.StartsWith(id))
+                    {
+                        var accessory = new Accessory(symbolName, parent, slot, file.batchTag, build.symbols[i]);
+                        slot.accessories.Add(accessory);
+                        HashCache.Get().Add(accessory.IdHash.HashValue, accessory.Id);
+                    }
+                }
+            }
+        }
+        [HarmonyPatch(typeof(MinionConfig), "CreatePrefab")]
+        public class MinionConfig_CreatePrefab_Patch
+        {
+            public static void Postfix(ref GameObject __result)
+            {
+                __result.AddComponent<SpiceEyes>();
+            }
+        }
+    }
+}
---
+++ Rockets-TinyYetBig/TwitchEvents/SpaceSpice/SpiceEyes.cs
@@ -0,0 +1,161 @@
+﻿using Klei.AI;
+using KSerialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockets_TinyYetBig.TwitchEvents.SpaceSpice
+{
+    internal class SpiceEyes : KMonoBehaviour, ISim1000ms
+    {
+        [MyCmpReq]
+        private MinionIdentity identity;
+        [MyCmpGet]
+        private Accessorizer accessorizer;
+        [MyCmpGet]
+        Effects effects;
+        [MyCmpReq]
+        private KBatchedAnimController kbac;
+
+        [Serialize]
+        public string currentEyes;
+
+        [Serialize]
+        public string originalEyes;
+
+        [Serialize]
+        public float SpiceEyesDuration = -100;
+        public override void OnSpawn()
+        {
+            base.OnSpawn();
+
+            originalEyes = Db.Get().Accessories.Get(accessorizer.bodyData.eyes).Id;
+            Debug.Log("ORIGINAL EYE SYMBOL IS " + originalEyes);
+
+            OnLoadGame();
+
+            if (SpiceEyesDuration>0)
+            {
+                Apply(currentEyes);
+            }
+        }
+        public void Remove()
+        {
+            if (accessorizer != null)
+            {
+                ReplaceAccessory(originalEyes);
+                currentEyes = null;
+            }
+
+            SpiceEyesDuration = -100f;
+        }
+
+        public void OnLoadGame()
+        {
+            if (SpiceEyesDuration>0)
+            {
+                ChangeAccessorySlot(currentEyes);
+            }
+        }
+        public void Apply(string accessory)
+        {
+            if (accessorizer != null)
+            {
+                ReplaceAccessory(accessory);
+                currentEyes = accessory;
+            }
+        }
+
+        public void OnSaveGame()
+        {
+            if (SpiceEyesDuration>0)
+            {
+                ChangeAccessorySlot(originalEyes);
+            }
+        }
+
+        private void ReplaceAccessory(string accessory)
+        {
+            var eyeSlot = Db.Get().AccessorySlots.Eyes;
+            var newAccessory = eyeSlot.Lookup(accessory);
+            var currentAccessory = accessorizer.GetAccessory(eyeSlot);
+            Debug.Log($"replacing accessory from {currentAccessory.Id} to {accessory}");
+
+            if (newAccessory == null)
+            {
+                Debug.LogWarning($"Could not add accessory {accessory}, it was not found in the database.");
+                return;
+            }
+
+            accessorizer.RemoveAccessory(currentAccessory);
+            accessorizer.AddAccessory(newAccessory);
+            accessorizer.ApplyAccessories();
+        }
+
+        private void ChangeAccessorySlot(HashedString value)
+        {
+            if (!value.IsValid)
+            {
+                Debug.Log("not valid value");
+                return;
+            }
+
+            Debug.Log("Changing accessory slot to " + HashCache.Get().Get(value));
+
+            var bodyData = accessorizer.bodyData;
+            bodyData.mouth = value;
+
+            var items = accessorizer.accessories;
+            var slot = Db.Get().AccessorySlots.Mouth;
+            var accessories = Db.Get().Accessories;
+
+            for (var i = 0; i < items.Count; i++)
+            {
+                var item = items[i];
+                var accessory = item.Get();
+                if (accessory.slot == slot)
+                {
+                    Debug.Log("changing slot");
+                    items[i] = new ResourceRef<Accessory>(accessories.Get(value));
+
+                    // force refresh the symbol
+                    var newAccessory = items[i].Get();
+                    kbac.GetComponent<SymbolOverrideController>().AddSymbolOverride(newAccessory.slot.targetSymbolId, newAccessory.symbol, 0);
+
+                    return;
+                }
+            }
+        }
+
+        public void Sim1000ms(float dt)
+        {
+            if(SpiceEyesDuration>0)
+            {
+                SpiceEyesDuration-= dt;
+            }
+            if(SpiceEyesDuration <= 0 && SpiceEyesDuration > -50) 
+            {
+                Remove();
+            }
+
+            //if(effects.effects.First(ef => ef.effect.Id == "PILOTING_SPICE") != null)
+            //{
+
+            //}
+            //foreach (var effect in effects.effects)
+            //{
+
+            //    Debug.Log("E: " + effect.effect.Id + "; " + effect.statusItem.Id + "; " + effect.modifier.Id + "; " + effect.timeRemaining);
+            //}
+                
+        }
+
+        internal void AddEyeDuration(float duration)
+        {
+            SpiceEyesDuration = duration;
+            Apply(originalEyes+"_glow");
+        }
+    }
+}
