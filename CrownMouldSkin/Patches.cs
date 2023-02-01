83e455dbea1a62d3004d5a4f2dae4757a665f5e9
Knastoron <bene.schlai@gmx.de>
2023-02-01 23:30:23 +0100

    crown mould skins

---
+++ CrownMouldSkin/Patches.cs
@@ -0,0 +1,41 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static CrownMouldSkin.ModAssets;
+
+namespace CrownMouldSkin
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// add buildings to plan screen
+        /// </summary>
+        [HarmonyPatch(typeof(BuildingFacades),"Load")]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+
+            public static void Postfix(BuildingFacades __instance)
+            {
+                __instance.Add("CMS_cornerTileAnkled", (string)global::STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.PASTELPOLKA.NAME, (string)global::STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.PASTELPOLKA.DESC, PermitRarity.Decent, CornerMouldingConfig.ID, "corner_tile_ankled_kanim");
+            }
+        }
+        /// <summary>
+        /// Init. auto translation
+        /// </summary>
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+        public static class Localization_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                LocalisationUtil.Translate(typeof(STRINGS), true);
+            }
+        }
+    }
+}
