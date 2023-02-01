b7e5769a914f73fd86c619e04b94346c7a035322
Knastoron <bene.schlai@gmx.de>
2023-02-02 00:53:35 +0100

    fridge found

--- Rockets-TinyYetBig/Buildings/CargoBays/FridgeModuleConfig.cs
+++ Rockets-TinyYetBig/Buildings/CargoBays/FridgeModuleConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rockets_TinyYetBig.TODO
+namespace Rockets_TinyYetBig.Buildings.CargoBays
 {
     class FridgeModuleConfig
     {
