c3052683290ef61f2162736543c0186bdfe73d96
Knastoron <bene.schlai@gmx.de>
2023-02-06 22:13:04 +0100

    corner mould skins

--- CrownMouldSkin/STRINGS.cs
+++ CrownMouldSkin/STRINGS.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static STRINGS.BUILDINGS.PREFABS;
 
 namespace CrownMouldSkin
 {
@@ -13,6 +14,42 @@ namespace CrownMouldSkin
         {
             public class PREFABS
             {
+                public class CORNERMOULDING
+                {
+                    public class FACADES
+                    {
+                        public class CORNER_TILE_B
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile B", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                        public class CORNER_TILE_C
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile C", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                        public class CORNER_TILE_D
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile D", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                        public class CORNER_TILE_E
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile E", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                        public class CORNER_TILE_F
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile F", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                        public class CORNER_TILE_G
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Corner Tile G", nameof(CORNERMOULDING));
+                            public static LocString DESC = (LocString)"A new look for the Corner Moulding";
+                        }
+                    }
+                }
 
             }
         }
