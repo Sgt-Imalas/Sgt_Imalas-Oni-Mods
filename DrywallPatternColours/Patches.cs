5805aac1a2071ba575a1f1ebdfaaeda85dcb8473
Knastoron <bene.schlai@gmx.de>
2023-03-04 21:19:52 +0100

    Drywall skins init

---
+++ DrywallPatternColours/Patches.cs
@@ -0,0 +1,49 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static DrywallPatternColours.ModAssets;
+
+namespace DrywallPatternColours
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// add buildings to plan screen
+        /// </summary>
+        /// 
+
+        [HarmonyPatch(typeof(BuildingFacades), MethodType.Constructor, typeof(ResourceSet))]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+
+            public static void Postfix(BuildingFacades __instance)
+            {
+                __instance.Add("ExteriorWall_red_deep", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.RED_DEEP.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.RED_DEEP.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_red_deep_kanim");
+                __instance.Add("ExteriorWall_orange_satsuma", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.ORANGE_SATSUMA.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.ORANGE_SATSUMA.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_orange_satsuma_kanim");
+                __instance.Add("ExteriorWall_yellow_lemon", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.YELLOW_LEMON.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.YELLOW_LEMON.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_yellow_lemon_kanim");
+                __instance.Add("ExteriorWall_green_kelly", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.GREEN_KELLY.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.GREEN_KELLY.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_green_kelly_kanim");
+                __instance.Add("ExteriorWall_blue_cobalt", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.BLUE_COBALT.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.BLUE_COBALT.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_blue_cobalt_kanim");
+                __instance.Add("ExteriorWall_pink_flamingo", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.PINK_FLAMINGO.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.PINK_FLAMINGO.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_pink_flamingo_kanim");
+                __instance.Add("ExteriorWall_grey_charcoal", (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.GREY_CHARCOAL.NAME, (string)STRINGS.BUILDINGS.PREFABS.EXTERIORWALL.FACADES.GREY_CHARCOAL.DESC, PermitRarity.Universal, ExteriorWallConfig.ID, "walls_grey_charcoal_kanim");
+            }
+        }
+        /// <summary>
+        /// Init. auto translation
+        /// </summary>
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+        public static class Localization_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                LocalisationUtil.Translate(typeof(STRINGS), true);
+            }
+        }
+    }
+}
---
+++ DrywallPatternColours/STRINGS.cs
@@ -0,0 +1,67 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrywallPatternColours
+{
+    internal class STRINGS
+    {
+        public class BUILDINGS
+        {
+            public class PREFABS
+            {
+                public class EXTERIORWALL
+                {
+                    public class FACADES
+                    {
+                        public class RED_DEEP
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Magma", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A red wallpaper.";
+                        }
+
+                        public class ORANGE_SATSUMA
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Bright", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"An orange wallpaper.";
+                        }
+
+                        public class YELLOW_LEMON
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Yellowcake", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A radiation-free wallpaper.";
+                        }
+
+                        public class GREEN_KELLY
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Algae", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A slippery wallpaper.";
+                        }
+
+                        public class BLUE_COBALT
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("H2O", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A damp wallpaper.";
+                        }
+
+                        public class PINK_FLAMINGO
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Petal", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A pink wallpaper.";
+                        }
+
+                        public class GREY_CHARCOAL
+                        {
+                            public static LocString NAME = (LocString)UI.FormatAsLink("Charcoal", nameof(EXTERIORWALL));
+                            public static LocString DESC = (LocString)"A sleek wallpaper.";
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
