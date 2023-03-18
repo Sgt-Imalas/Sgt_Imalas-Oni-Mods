b9b60b6d91253a9cc62af2f3c0be3c79c6450cf8
Knastoron <bene.schlai@gmx.de>
2023-03-18 16:52:45 +0100

    dream journal obsolete

--- StoreDreamJournals/Patches.cs
+++ StoreDreamJournals/Patches.cs
@@ -1,6 +1,7 @@
 ﻿using Database;
 using HarmonyLib;
 using Klei.AI;
+using Steamworks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +26,7 @@ namespace StoreDreamJournals
             public static void Postfix(GameObject __result)
             {
 
-                __result.AddOrGet<KBoxCollider2D>().size = (Vector2)new Vector2f(0.67f, 0.75f);
+                //__result.AddOrGet<KBoxCollider2D>().size = (Vector2)new Vector2f(0.67f, 0.75f);
 
                 //KPrefabID kprefabId = __result.AddOrGet<KPrefabID>();                
                 //kprefabId.RemoveTag(GameTags.StoryTraitResource);
@@ -44,7 +45,32 @@ namespace StoreDreamJournals
                 kprefabId.AddTag(GameTags.StoryTraitResource);
             }
         }
-        
+
+        [HarmonyPatch(typeof(MainMenu))]
+        [HarmonyPatch(nameof(MainMenu.OnSpawn))]
+        public static class ObsoleteMessage
+        {
+            public static void Postfix()
+            {
+                System.Action UnsubscribeAction = new System.Action(
+                () => {
+                    SteamUGC.UnsubscribeItem(new PublishedFileId_t(ulong.Parse("2882203528")));
+                });
+
+                KMod.Manager.Dialog(Global.Instance.globalCanvas,
+                "Obsolete Mod!",
+                "The Mod \"Store Dream Journals/Fix Dream Journal Hitbox\" has become obsolete and will soon be removed from the Workshop.\nDo you want to unsubscribe now?",
+                "Yes (Unsubscribe)",
+                UnsubscribeAction,
+                "No",
+                () =>
+                {
+
+                });
+            }
+        }
+
+
 
         [HarmonyPatch(typeof(Db))]
         [HarmonyPatch("Initialize")]
