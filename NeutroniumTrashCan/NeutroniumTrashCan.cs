1b273feb4f2ebbfe318b1c033ab5bf3e132f4807
Knastoron <bene.schlai@gmx.de>
2023-04-02 00:38:57 +0200

    neutronium trash can

---
+++ NeutroniumTrashCan/NeutroniumTrashCan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutroniumTrashCan
+{
+    internal class NeutroniumTrashCan : KMonoBehaviour, ISim1000ms
+    {
+        [MyCmpGet]
+        Storage storage;
+        public void Sim1000ms(float dt)
+        {
+            if (storage.MassStored() > 0)
+            {
+                foreach(var item in storage.items)
+                {
+                    UnityEngine.Object.Destroy(item.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ NeutroniumTrashCan/STRINGS.cs
@@ -0,0 +1,26 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeutroniumTrashCan
+{
+    internal class STRINGS
+    {
+        public class BUILDINGS
+        {
+            public class PREFABS
+            {
+                public class NTC_NEUTRONIUMTRASHCAN
+                {
+                        public static LocString NAME = (LocString)global::STRINGS.UI.FormatAsLink("Neutronium Trash Can", nameof(NTC_NEUTRONIUMTRASHCAN));
+                        public static LocString DESC = "Its bigger on the inside!";
+                        public static LocString EFFECT = "Eats and deletes Neutronium - for those cleanly shaved walls.";
+                    
+                }
+            }
+        }
+    }
+}
