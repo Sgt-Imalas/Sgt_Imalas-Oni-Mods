d8ff4bb791ff6c9ba2b6c50b7a734f4716060f94
Knastoron <bene.schlai@gmx.de>
2023-04-15 22:30:41 +0200

    dirty brine

---
+++ DirtyBrine/Patches.cs
@@ -0,0 +1,39 @@
+﻿using Database;
+using ElementUtilNamespace;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static DirtyBrine.ModAssets;
+
+namespace DirtyBrine
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// akis beached 
+        /// </summary>
+        [HarmonyPatch(typeof(ElementLoader))]
+        [HarmonyPatch(nameof(ElementLoader.Load))]
+        public class ElementLoader_Load_Patch
+        {
+            public static void Prefix(Dictionary<string, SubstanceTable> substanceTablesByDlc)
+            {
+                var list = substanceTablesByDlc[DlcManager.VANILLA_ID].GetList();
+
+                var brine = list.Find(e => e.elementID == SimHashes.Brine);
+
+                var pWater = list.Find(e => e.elementID == SimHashes.DirtyWater);
+                var saltWater = list.Find(e => e.elementID == SimHashes.SaltWater);
+
+                brine.colour = Color.Lerp(saltWater.colour, pWater.colour, 0.5f);
+                brine.uiColour = Color.Lerp(saltWater.uiColour, pWater.uiColour, 0.5f);
+            }
+        }
+    }
+}
---
+++ DirtyBrine/STRINGS.cs
@@ -0,0 +1,20 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirtyBrine
+{
+    internal class STRINGS
+    {
+        public class BUILDINGS
+        {
+            public class PREFABS
+            {
+
+            }
+        }
+    }
+}
