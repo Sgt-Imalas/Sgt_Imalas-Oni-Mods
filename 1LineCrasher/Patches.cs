1279ebde7539b3b319709568bc6fa7eadf5ad950
Knastoron <bene.schlai@gmx.de>
2023-05-05 22:09:42 +0200

    comma cycle mod

--- 1LineCrasher/Patches.cs
+++ 1LineCrasher/Patches.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using HarmonyLib;
 namespace _1LineCrasher
 {
     internal class Patches
@@ -6,17 +6,17 @@ namespace _1LineCrasher
         /// <summary>
         /// add buildings to plan screen
         /// </summary>
-        //[HarmonyPatch(typeof(GeneratedBuildings))]
-        //[HarmonyPatch(nameof(GeneratedBuildings.LoadGeneratedBuildings))]
-        //public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
-        //{
-        //    /// <summary>
-        //    /// "CLEAN" this project once to generate publicizer files
-        //    /// </summary>
-        //    public static void Postfix()
-        //    {
-        //        Debug.Log("weeeeeeeeeeeee");
-        //    }
-        //}
+        [HarmonyPatch(typeof(DateTime))]
+        [HarmonyPatch(nameof(DateTime.Update))]
+        public static class CommaToNumber
+        {
+            /// <summary>
+            /// "CLEAN" this project once to generate publicizer files
+            /// </summary>
+            public static void Postfix(DateTime __instance)
+            {
+                __instance.text.text = (GameClock.Instance.GetTimeInCycles() +1).ToString("n3");
+            }
+        }
     }
 }
