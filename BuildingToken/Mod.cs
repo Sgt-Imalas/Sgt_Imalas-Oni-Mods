25c934a0b8ea2891beb460ad7d5b6b97e0a1a6b6
Knastoron <bene.schlai@gmx.de>
2023-05-28 02:47:59 +0200

    token list in file

--- BuildingToken/Mod.cs
+++ BuildingToken/Mod.cs
@@ -1,7 +1,13 @@
 ﻿using HarmonyLib;
+using Klei;
 using KMod;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using TemplateClasses;
 using UtilLibs;
 using static STRINGS.BUILDINGS.PREFABS;
 
@@ -9,18 +15,85 @@ namespace BuildingToken
 {
     public class Mod : UserMod2
     {
+        public static readonly string Header = "All the following buildings will get tokens added to their recipes:\n";
+        public static readonly string Info = "<add your building IDs here, separated by a comma (,)>";
         public override void OnLoad(Harmony harmony)
         {
-            
+
+            SgtLogger.LogVersion(this);
+            ModAssets.RuleFilePath = FileSystem.Normalize(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TokenBuildings.txt"));
+            SgtLogger.l(ModAssets.RuleFilePath,"Building Token File Location");
+
+            FileInfo info = new FileInfo(ModAssets.RuleFilePath);
+            if (!info.Exists)
+            {
+                SgtLogger.l("file not found, creating a new one");
+                WriteToFile(ModAssets.RuleFilePath, Header + Info);
+                SgtLogger.l("File created");
+
+                info = new FileInfo(ModAssets.RuleFilePath);
+            }
+            string tokenBuildings = ReadFromFile(info);
+            tokenBuildings = tokenBuildings
+                .Replace(Header,string.Empty)
+                .Replace(Info,string.Empty)
+                .Replace(" ",string.Empty)
+                .Replace("\n", string.Empty);
+
+            ModAssets.TokenBuildings = new List<string>();
+            ModAssets.TokenBuildings = tokenBuildings.Split(',').ToList();
+
+
+
             foreach (string tokenBuilding in ModAssets.TokenBuildings)
             {
+                var building = Assets.TryGetPrefab(tokenBuilding);
+                
+                SgtLogger.l("added token entry for a building with the id: \"" + tokenBuilding + "\".");
+
                 var tag = ModAssets.AddTokenForBuilding(tokenBuilding);
                 GameTags.MaterialBuildingElements.Add(tag);
             }
 
             //GameTags.Other.Add("x");
             base.OnLoad(harmony);
-            SgtLogger.LogVersion(this);
+        }
+
+        public static string ReadFromFile(FileInfo filePath)
+        {
+            if (!filePath.Exists)
+            {
+                SgtLogger.logwarning("Not a valid file");
+                return null;
+            }
+            else
+            {
+                FileStream filestream = filePath.OpenRead();
+                using (var sr = new StreamReader(filestream))
+                {
+                    string list = sr.ReadToEnd();
+                    return list;
+                }
+            }
+        }
+        public void WriteToFile(string path, string toWrite)
+        {
+            try
+            {
+                //var path = Path.Combine(ModAssets.ScheduleTemplatePath, FileName + ".json");
+
+                var fileInfo = new FileInfo(path);
+                FileStream fcreate = fileInfo.Open(FileMode.Create);
+
+                using (var streamWriter = new StreamWriter(fcreate))
+                {
+                   streamWriter.Write(toWrite);
+                }
+            }
+            catch (Exception e)
+            {
+                SgtLogger.logError("Could not write file, Exception: " + e);
+            }
         }
     }
 }
--- BuildingToken/ModAssets.cs
+++ BuildingToken/ModAssets.cs
@@ -13,6 +13,7 @@ namespace BuildingToken
 {
     internal class ModAssets
     {
+        public static string RuleFilePath;
         public static Dictionary<string,Tag> BuildingTokenTags = new Dictionary<string,Tag>();
         public static Dictionary<string,string> BuildingTokens = new Dictionary<string, string>();
         public static Tag AddTokenForBuilding(string id)
@@ -22,13 +23,6 @@ namespace BuildingToken
             return ToAddTag;
         }
 
-        public static List<string> TokenBuildings = new List<string>()
-            {
-                DoorConfig.ID,
-                BunkerDoorConfig.ID,
-                ManualPressureDoorConfig.ID,
-                PressureDoorConfig.ID
-            };
-
+        public static List<string> TokenBuildings = new List<string>();
     }
 }
