c9ac081cea10e999f13b3ad0e63cef1d8f8baca7
Knastoron <bene.schlai@gmx.de>
2023-06-23 15:35:36 +0200

    biome trim fix

---
+++ BiomeTrimsFixed/Patches.cs
@@ -0,0 +1,30 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static BiomeTrimsFixed.ModAssets;
+
+namespace BiomeTrimsFixed
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(GroundRenderer.WorldChunk))]
+        [HarmonyPatch(nameof(GroundRenderer.WorldChunk.GetBiomeIdx))]
+        public class DB_Init
+        {
+            public static void Postfix(GroundRenderer __instance, int cell, ref int __result)
+            {
+                if (World.Instance != null && World.Instance.zoneRenderData != null && __result == 3)
+                {
+                    __result = (int)World.Instance.zoneRenderData.GetSubWorldZoneType(cell);
+                }
+            }
+        }
+    }
+}
