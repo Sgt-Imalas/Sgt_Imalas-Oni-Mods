0297ab788d7f2de2cdfe51d908eaf638d70e8a12
Admin <Admin@DESKTOP-I46AJNG>
2023-06-29 18:56:20 +0200

    adjusted amogus events, chaos event init

---
+++ Imalas_TwitchChaosEvents/Events/InverseElementEvent.cs
@@ -0,0 +1,39 @@
+﻿using ONITwitchLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using Util_TwitchIntegrationLib;
+
+namespace Imalas_TwitchChaosEvents.Events
+{
+    internal class InverseElementEvent : ITwitchEventBase
+    {
+        public string ID => "ChaosTwitch_InverseElement";
+
+        public string EventGroupID => null;
+
+        public string EventName => throw new NotImplementedException();
+
+        public string EventDescription => throw new NotImplementedException();
+
+        public EventWeight EventWeight => EventWeight.WEIGHT_NORMAL;
+
+        public Action<object> EventAction => throw new NotImplementedException();
+
+        public Func<object, bool> Condition =>
+            (data) =>
+            {
+                return true;
+            };
+
+        public Danger EventDanger => Danger.High;
+
+        public void SpawnBuzzSaw()
+        {
+            ToastManager.InstantiateToastWithPosTarget(EventName, EventDescription, targetCoords);
+        }
+    }
+}
---
+++ Imalas_TwitchChaosEvents/TwitchIntegrationPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imalas_TwitchChaosEvents
+{
+    internal class TwitchIntegrationPatch
+    {
+        [HarmonyPatch(typeof(Db), "Initialize")]
+        public static class Db_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                Util_TwitchIntegrationLib.EventRegistration.InitializeTwitchEventsInNameSpace("Imalas_TwitchChaosEvents.Events");
+            }
+        }
+    }
+}
