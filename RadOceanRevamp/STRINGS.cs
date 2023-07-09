9b1faf92ba4142faa8f6a70c3f79a4f2359d0d09
Knastoron <bene.schlai@gmx.de>
2023-07-09 12:29:48 +0200

    mini worldgen experiment

---
+++ RadOceanRevamp/STRINGS.cs
@@ -0,0 +1,29 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadOceanRevamp
+{
+    internal class STRINGS
+    {
+        public class CLUSTER_NAMES
+        {
+            public class REVAMPED_RADOCEAN_CLUSTER
+            {
+                public static LocString NAME = "Revamped Radocean";
+                public static LocString DESCRIPTION = "The starting asteroid features the tundra, sandstone, radioactive and jungle biomes.";
+            }
+        }
+        public class WORLDS
+        {
+            public class REVAMPED_RADOCEAN_MOONLET
+            {
+                public static LocString NAME = "Rad Ocean Revamped";
+                public static LocString DESCRIPTION = "This world is rad ocean, but better!";
+            }
+        }
+    }
+}
