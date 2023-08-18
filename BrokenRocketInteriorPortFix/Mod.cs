d1f98a6acfe061fb90b0dbac674a129a5b98ed8f
Knastoron <bene.schlai@gmx.de>
2023-08-18 03:07:28 +0200

    rocket interior port fix mod

---
+++ BrokenRocketInteriorPortFix/Mod.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+
+namespace BrokenRocketInteriorPortFix
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
