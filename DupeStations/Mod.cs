2483ae6177fdb32c87cef6053d470bf172d05243
Admin <Admin@DESKTOP-I46AJNG>
2023-10-11 22:56:44 +0200

    reactable test

---
+++ DupeStations/Mod.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Options;
+using System;
+using System.Collections.Generic;
+using UtilLibs;
+
+namespace DupeStations
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            PUtil.InitLibrary(false);
+           // new POptions().RegisterOptions(this, typeof(Config));
+            base.OnLoad(harmony);
+            SgtLogger.LogVersion(this);
+        }
+        public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<KMod.Mod> mods)
+        {
+            base.OnAllModsLoaded(harmony, mods);
+            CompatibilityNotifications.FlagLoggingPrevention(mods);
+
+        }
+    }
+}
---
+++ DupeStations/PillDispenserWorkable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DupeStations
+{
+    public class PillDispenserWorkable : WorkableReactable
+    {
+        [MyCmpGet]
+        Storage pillStorage;
+
+
+        public PillDispenserWorkable(Workable workable, ChoreType chore_type, AllowedDirection allowed_direction = AllowedDirection.Any) : base(workable, "TakeMedicine", chore_type, allowed_direction)
+        {
+
+        }
+
+        public bool CanBegin()
+        {
+            return pillStorage.FindFirstWithMass(BasicRadPillConfig.ID, 1);
+        }
+
+        public override bool InternalCanBegin(GameObject new_reactor, Navigator.ActiveTransition transition)
+        {
+            if (base.InternalCanBegin(new_reactor, transition))
+            {//replace with current filterable
+                if (CanBegin())
+                {
+
+                }
+            }
+            return false;
+        }
+
+        public class Work : Workable
+        {
+
+        }
+    }
+}
