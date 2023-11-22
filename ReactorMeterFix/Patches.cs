c38bc1c23daa6e9ee4ade71b9a134794b8f2582e
Knastoron <bene.schlai@gmx.de>
2023-11-23 00:27:39 +0100

    reactor meter fix mini mod

---
+++ ReactorMeterFix/Patches.cs
@@ -0,0 +1,30 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+
+namespace ReactorMeterFix
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// add buildings to plan screen
+        /// </summary>
+        [HarmonyPatch(typeof(Reactor))]
+        [HarmonyPatch(nameof(Reactor.OnSpawn))]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+
+            public static void Prefix()
+            {
+                Reactor.meterFrameScaleHack = 1;
+            }
+        }
+    }
+}
