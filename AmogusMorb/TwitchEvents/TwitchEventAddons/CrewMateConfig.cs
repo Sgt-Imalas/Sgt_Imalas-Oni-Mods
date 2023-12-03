63510b5315bb0e91023f4bbc96fd3d4ee31cb5d2
Knastoron <bene.schlai@gmx.de>
2023-12-03 14:43:03 +0100

    amogus broken anim fix

--- AmogusMorb/TwitchEvents/TwitchEventAddons/CrewMateConfig.cs
+++ AmogusMorb/TwitchEvents/TwitchEventAddons/CrewMateConfig.cs
@@ -12,41 +12,38 @@ namespace AmogusMorb.TwitchEvents.TwitchEventAddons
     internal class CrewMateConfig : IEntityConfig
     {
         public const string ID = "AMOGUS_Twitch_Crewmate";
-        public static string imposta_trait = "AMOGUS_Twitch_Crewmate";
-        public const float MASS = 100f;
-        private const float WIDTH = 1f;
-        private const float HEIGHT = 1f;
+        public static string BASE_TRAIT_ID = "AMOGUS_Twitch_Crewmate";
 
 
-        public string[] GetDlcIds() => DlcManager.AVAILABLE_EXPANSION1_ONLY;
+        public string[] GetDlcIds() => DlcManager.AVAILABLE_ALL_VERSIONS;
 
         public GameObject CreatePrefab()
         {
-            string name1 = (string)"Crewmate";
-            string name2 = name1;
+            string name = (string)"Crewmate";
             string desc = (string)"best fwiend";
-            EffectorValues tieR0 = TUNING.DECOR.BONUS.TIER2;
             string anim = "twitch_imposter_kanim";
-            EffectorValues decor = tieR0;
+            EffectorValues decor = TUNING.DECOR.BONUS.TIER2;
             EffectorValues noise = new EffectorValues();
-            GameObject placedEntity = EntityTemplates.CreatePlacedEntity(ID, name2, desc, 25f, Assets.GetAnim((HashedString)anim), "idle_loop", Grid.SceneLayer.Creatures, 1, 1, decor, noise);
-            Db.Get().CreateTrait(imposta_trait, name1, name1, (string)null, false, (ChoreGroup[])null, true, true).Add(new AttributeModifier(Db.Get().Amounts.HitPoints.maxAttribute.Id, 10f, name1));
+            GameObject placedEntity = EntityTemplates.CreatePlacedEntity(ID, name, desc, 25f, Assets.GetAnim((HashedString)anim), "idle_loop", Grid.SceneLayer.Creatures, 1, 1, decor, noise);
+            Db.Get().CreateTrait(BASE_TRAIT_ID, name, name, (string)null, false, (ChoreGroup[])null, true, true).Add(new AttributeModifier(Db.Get().Amounts.HitPoints.maxAttribute.Id, 10f, name));
             KPrefabID component = placedEntity.GetComponent<KPrefabID>();
             component.AddTag(GameTags.Creatures.Walker);
             component.AddTag(GameTags.OriginalCreature);
             component.AddTag(GameTags.Amphibious);
             component.prefabInitFn += (KPrefabID.PrefabFn)(inst => inst.GetAttributes().Add(Db.Get().Attributes.MaxUnderwaterTravelCost));
-            EntityTemplates.ExtendEntityToBasicCreature(placedEntity, FactionManager.FactionID.Friendly, imposta_trait, "RobotNavGrid", onDeathDropCount: 0, warningLowTemperature: 250.15f, warningHighTemperature: 2393.15f, lethalLowTemperature: 200.15f, lethalHighTemperature: 2623.15f);
+            EntityTemplates.ExtendEntityToBasicCreature(placedEntity, FactionManager.FactionID.Friendly, BASE_TRAIT_ID, "RobotNavGrid", onDeathDropCount: 0, warningLowTemperature: 250.15f, warningHighTemperature: 2393.15f, lethalLowTemperature: 200.15f, lethalHighTemperature: 2623.15f);
             
+
             placedEntity.AddOrGetDef<ThreatMonitor.Def>();
+            placedEntity.AddOrGet<Trappable>();
             placedEntity.AddOrGetDef<CreatureFallMonitor.Def>();
 
             placedEntity.AddOrGetDef<OvercrowdingMonitor.Def>().spaceRequiredPerCreature = 0;
             placedEntity.AddOrGet<LoopingSounds>().updatePosition = true;
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_movement_short", NOISE_POLLUTION.CREATURES.TIER2);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_jump", NOISE_POLLUTION.CREATURES.TIER3);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_land", NOISE_POLLUTION.CREATURES.TIER3);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_expel", NOISE_POLLUTION.CREATURES.TIER4);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_movement_short", NOISE_POLLUTION.CREATURES.TIER2);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_jump", NOISE_POLLUTION.CREATURES.TIER3);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_land", NOISE_POLLUTION.CREATURES.TIER3);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_expel", NOISE_POLLUTION.CREATURES.TIER4);
             EntityTemplates.CreateAndRegisterBaggedCreature(placedEntity, true, false);
             EntityTemplates.AddCreatureBrain(placedEntity,
                 new ChoreTable.Builder()
@@ -77,8 +74,7 @@ namespace AmogusMorb.TwitchEvents.TwitchEventAddons
 
         public void OnSpawn(GameObject inst)
         {
-
-            inst.AddOrGet<CrewColoursSetter>();
+            //inst.AddOrGet<CrewColoursSetter>();
             Navigator component2 = inst.AddOrGet<Navigator>();
             component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new BipedTransitionLayer(component2, 3.325f, 2.5f));
             component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new DoorTransitionLayer(component2));
--- AmogusMorb/TwitchEvents/TwitchEventAddons/ImposterConfig.cs
+++ AmogusMorb/TwitchEvents/TwitchEventAddons/ImposterConfig.cs
@@ -13,25 +13,20 @@ namespace AmogusMorb.TwitchEvents.TwitchEventAddons
     {
         public const string ID = "AMOGUS_Twitch_Imposter";
         public static string imposta_trait = "AMOGUS_Twitch_ImposterBaseTrait";
-        public const int MAXIMUM_TECH_CONSTRUCTION_TIER = 2;
-        public const float MASS = 100f;
-        private const float WIDTH = 1f;
-        private const float HEIGHT = 1f;
 
 
-        public string[] GetDlcIds() => DlcManager.AVAILABLE_EXPANSION1_ONLY;
+        public string[] GetDlcIds() => DlcManager.AVAILABLE_ALL_VERSIONS;
 
         public GameObject CreatePrefab()
         {
-            string name1 = (string)"Imposter";
-            string name2 = name1;
+            string name = (string)"Imposter";
             string desc = (string)"sussy baka";
             EffectorValues tieR0 = TUNING.DECOR.PENALTY.TIER2;
             string anim = "twitch_imposter_kanim";
             EffectorValues decor = tieR0;
             EffectorValues noise = new EffectorValues();
-            GameObject placedEntity = EntityTemplates.CreatePlacedEntity(ID, name2, desc, 25f, Assets.GetAnim((HashedString)anim), "idle_loop", Grid.SceneLayer.Creatures, 1, 1, decor, noise);
-            Db.Get().CreateTrait(imposta_trait, name1, name1, (string)null, false, (ChoreGroup[])null, true, true).Add(new AttributeModifier(Db.Get().Amounts.HitPoints.maxAttribute.Id, 80f, name1));
+            GameObject placedEntity = EntityTemplates.CreatePlacedEntity(ID, name, desc, 25f, Assets.GetAnim((HashedString)anim), "idle_loop", Grid.SceneLayer.Creatures, 1, 1, decor, noise);
+            Db.Get().CreateTrait(imposta_trait, name, name, (string)null, false, (ChoreGroup[])null, true, true).Add(new AttributeModifier(Db.Get().Amounts.HitPoints.maxAttribute.Id, 80f, name));
             KPrefabID component = placedEntity.GetComponent<KPrefabID>();
             component.AddTag(GameTags.Creatures.Walker);
             component.AddTag(GameTags.OriginalCreature);
@@ -40,14 +35,16 @@ namespace AmogusMorb.TwitchEvents.TwitchEventAddons
             EntityTemplates.ExtendEntityToBasicCreature(placedEntity, FactionManager.FactionID.Hostile, imposta_trait, "RobotNavGrid", onDeathDropCount: 6, warningLowTemperature: 250.15f, warningHighTemperature: 2393.15f, lethalLowTemperature: 200.15f, lethalHighTemperature: 2623.15f);
             placedEntity.AddWeapon(5f, 10f).AddEffect();
             placedEntity.AddOrGetDef<ThreatMonitor.Def>();
+            placedEntity.AddOrGet<Trappable>();
             placedEntity.AddOrGetDef<CreatureFallMonitor.Def>();
 
             placedEntity.AddOrGetDef<OvercrowdingMonitor.Def>().spaceRequiredPerCreature = 0;
             placedEntity.AddOrGet<LoopingSounds>().updatePosition = true;
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_movement_short", NOISE_POLLUTION.CREATURES.TIER2);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_jump", NOISE_POLLUTION.CREATURES.TIER3);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_land", NOISE_POLLUTION.CREATURES.TIER3);
-            SoundEventVolumeCache.instance.AddVolume(anim, "Morb_expel", NOISE_POLLUTION.CREATURES.TIER4);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_movement_short", NOISE_POLLUTION.CREATURES.TIER2);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_jump", NOISE_POLLUTION.CREATURES.TIER3);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_land", NOISE_POLLUTION.CREATURES.TIER3);
+            //SoundEventVolumeCache.instance.AddVolume(anim, "Morb_expel", NOISE_POLLUTION.CREATURES.TIER4);
+
             EntityTemplates.CreateAndRegisterBaggedCreature(placedEntity, true, false);
             EntityTemplates.AddCreatureBrain(placedEntity,
                 new ChoreTable.Builder()
@@ -79,14 +76,14 @@ namespace AmogusMorb.TwitchEvents.TwitchEventAddons
 
         public void OnSpawn(GameObject inst)
         {
-            inst.AddOrGet<CrewColoursSetter>();
-            Navigator component2 = inst.AddOrGet<Navigator>(); 
-            component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new BipedTransitionLayer(component2, 3.325f, 2.5f));
-            component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new DoorTransitionLayer(component2));
-            component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new LadderDiseaseTransitionLayer(component2));
-            component2.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new SplashTransitionLayer(component2));
-            component2.SetFlags(PathFinder.PotentialPath.Flags.None);
-            component2.CurrentNavType = NavType.Floor;
+            //inst.AddOrGet<CrewColoursSetter>();
+            Navigator navigator = inst.AddOrGet<Navigator>(); 
+            navigator.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new BipedTransitionLayer(navigator, 3.325f, 2.5f));
+            navigator.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new DoorTransitionLayer(navigator));
+            navigator.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new LadderDiseaseTransitionLayer(navigator));
+            navigator.transitionDriver.overrideLayers.Add((TransitionDriver.OverrideLayer)new SplashTransitionLayer(navigator));
+            navigator.SetFlags(PathFinder.PotentialPath.Flags.None);
+            navigator.CurrentNavType = NavType.Floor;
         }
 
     }
