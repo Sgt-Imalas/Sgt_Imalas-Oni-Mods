8bdd80a1cbf2fea2594662ab597d3fceee56e071
Sgt_Imalas <bene.schlai@gmx.de>
2024-01-06 12:43:02 +0100

    SkillQueue initial commit

---
+++ SkillingQueue/Patches.cs
@@ -0,0 +1,126 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UtilLibs;
+using static STRINGS.UI;
+
+namespace SkillingQueue
+{
+    internal class Patches
+    {
+        //SimHashes.SkillPointAquired, MinionResume data
+        /// <summary>
+        /// Init. auto translation
+        /// </summary>
+        /// 
+
+
+        public static Dictionary<MinionResume, SavedSkillQueue> ResumeQueues = new Dictionary<MinionResume, SavedSkillQueue>();
+
+        [HarmonyPatch(typeof(MinionConfig), nameof(MinionConfig.CreatePrefab))]
+        public static class Localization_Initialize_Patch
+        {
+            public static void Postfix(GameObject __result)
+            {
+                __result.AddOrGet<SavedSkillQueue>();
+            }
+        }
+        [HarmonyPatch(typeof(MinionResume), nameof(MinionResume.MasterSkill))]
+        public static class MinionResume_MasterSkill_Patch
+        {
+            public static void Postfix(MinionResume __instance, string skillId)
+            {
+                if (ResumeQueues.ContainsKey(__instance))
+                {
+                    ResumeQueues[__instance].LearnedSkill(skillId);
+                }
+            }
+        }
+        [HarmonyPatch(typeof(SkillWidget), nameof(SkillWidget.Refresh))]
+        public static class SkillWidget_Refresh_Patch
+        {
+            public static void Postfix(SkillWidget __instance, string skillID)
+            {
+
+                __instance.skillsScreen.GetMinionIdentity(__instance.skillsScreen.CurrentlySelectedMinion, out var minionIdentity, out _);
+                if(minionIdentity.TryGetComponent<MinionResume>(out var resume) && ResumeQueues.ContainsKey(resume) && ResumeQueues[resume].HasSkillQueued(skillID,out int index))
+                {
+                    Skill skill = Db.Get().Skills.Get(skillID);
+
+                    float min = index;
+                    float max = Mathf.Max(8,ResumeQueues[resume].QueuedSkillCount);
+                    float gradientValue  =(min/max)*0.9f;
+                    var targetColor = Color.HSVToRGB(gradientValue, 0.45f, 1f);
+
+                    __instance.Name.text = skill.Name + UIUtils.ColorText(" <b>["+(++index) +"]</b>", targetColor) +"\n(" + Db.Get().SkillGroups.Get(skill.skillGroup).Name + ")" ;
+                }
+            }
+        }
+        [HarmonyPatch(typeof(SkillWidget), nameof(SkillWidget.OnPointerClick))]
+        public static class SkillWidget_OnPointerClick_Patch
+        {
+            public static void Postfix(SkillWidget __instance, PointerEventData eventData)
+            {
+                bool shiftClicked = (Global.GetInputManager().GetDefaultController().mActiveModifiers & Modifier.Shift) != 0;
+
+                __instance.skillsScreen.GetMinionIdentity(__instance.skillsScreen.CurrentlySelectedMinion, out var minionIdentity, out var _);
+                if (minionIdentity == null || !minionIdentity.TryGetComponent<MinionResume>(out var resume) || !ResumeQueues.ContainsKey(resume))
+                    return;
+
+                SavedSkillQueue queue = ResumeQueues[resume];
+                if (shiftClicked)
+                {
+                    if (queue.HasSkillQueued(__instance.skillID, out _))
+                    {
+                        queue.RemoveSkillAndPostrequisites(__instance.skillID);
+                        __instance.skillsScreen.RefreshAll();
+                    }
+                    else
+                    {
+                        if (queue.CanMasterSkillAndPrerequisites(__instance.skillID) && !resume.HasMasteredSkill(__instance.skillID))
+                        {
+                            queue.AddSkillWithPrerequisites(__instance.skillID);
+                            queue.TryLearnSkills();
+                            __instance.skillsScreen.RefreshAll();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        [HarmonyPatch(typeof(SkillsScreen), nameof(SkillsScreen.RefreshProgressBars))]
+        public static class SkillsScreen_RefreshProgressBars_Patch
+        {
+            public static void Postfix(SkillsScreen __instance)
+            {
+                if (__instance.currentlySelectedMinion == null || __instance.currentlySelectedMinion.IsNull())
+                {
+                    return;
+                }
+                __instance.GetMinionIdentity(__instance.currentlySelectedMinion, out var minionIdentity, out _);
+                if (minionIdentity.TryGetComponent<MinionResume>(out var resume) && ResumeQueues.ContainsKey(resume))
+                {
+                    int finalMorale = ResumeQueues[resume].GetFinalMorale();
+                    int finalMoraleExpectation = ResumeQueues[resume].GetFinalMoraleExpectancy();
+
+                    if (finalMorale > 0)
+                    {
+                        __instance.moraleProgressLabel.text = __instance.moraleProgressLabel.text + GameUtil.ApplyBoldString(GameUtil.ColourizeString(__instance.moraleNotchColor," [" + finalMorale + "] "));
+                    }
+                    if (finalMoraleExpectation > 0)
+                    {
+                        __instance.expectationsProgressLabel.text = __instance.expectationsProgressLabel.text + GameUtil.ApplyBoldString(GameUtil.ColourizeString(__instance.expectationNotchColor, " [" + finalMoraleExpectation + "] "));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SkillingQueue/SavedSkillQueue.cs
@@ -0,0 +1,207 @@
+﻿using Database;
+using KSerialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUNING;
+using UtilLibs;
+using static ResearchTypes;
+
+namespace SkillingQueue
+{
+    internal class SavedSkillQueue : KMonoBehaviour, ISaveLoadable
+    {
+        [Serialize]
+        private List<string> SkillIds;
+
+        [MyCmpGet] MinionResume refResume;
+
+        public int QueuedSkillCount => SkillIds.Count;
+
+        internal SavedSkillQueue()
+        {
+            SkillIds = new List<string>(32);
+        }
+        public override void OnSpawn()
+        {
+            Patches.ResumeQueues.Add(refResume, this);
+            Game.Instance.Subscribe((int)GameHashes.SkillPointAquired, OnSkillPointAquired);
+            base.OnSpawn();
+        }
+        public override void OnCleanUp()
+        {
+            Patches.ResumeQueues.Remove(refResume);
+            Game.Instance.Unsubscribe((int)GameHashes.SkillPointAquired, OnSkillPointAquired);
+            base.OnCleanUp();
+        }
+
+        public int GetFinalMorale()
+        {
+            int value = 0;
+            var db = Db.Get().Skills;
+
+            foreach (string id in SkillIds)
+            {
+                if (!refResume.HasMasteredSkill(id))
+                {
+                    Skill skill = db.Get(id);
+                    if(refResume.HasSkillAptitude(skill))
+                    {
+                        value++;
+                    }
+                }
+            }
+            return value;
+        }
+        public int GetFinalMoraleExpectancy()
+        {            int value = 0;
+            var db = Db.Get().Skills;
+
+            foreach (string id in SkillIds)
+            {
+                if (!refResume.HasMasteredSkill(id))
+                {
+                    Skill skill = db.Get(id);
+                    value += skill.tier + 1;
+                }
+            }
+            return value;
+        }
+
+
+        public void TryLearnSkills()
+        {
+            if (refResume.AvailableSkillpoints > 0)
+            {
+                OnSkillPointAquired(refResume);
+            }
+        }
+
+        private void OnSkillPointAquired(object data)
+        {
+            if (data is not MinionResume resume)
+                return;
+            if (refResume != resume)
+                return;
+            if (SkillIds.Count == 0)
+                return;
+
+            int maxCount = SkillIds.Count;
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                if (refResume.AvailableSkillpoints <= 0)
+                    break;
+
+                var SkillId = SkillIds.First();
+
+                MinionResume.SkillMasteryConditions[] masteryConditions = refResume.GetSkillMasteryConditions(SkillId);
+
+                if (refResume.HasMasteredSkill(SkillId))
+                    SkillIds.Remove(SkillId);
+
+                if (refResume.CanMasterSkill(masteryConditions) && !refResume.HasMasteredSkill(SkillId))
+                {
+                    SkillIds.Remove(SkillId);
+                    refResume.MasterSkill(SkillId);
+                }
+            }
+        }
+
+        public bool HasSkillQueued(string skillId, out int index)
+        {
+            index = SkillIds.FindIndex(item => item == skillId);
+
+            return SkillIds.Contains(skillId);
+        }
+
+        private void RemoveSkill(string skillId)
+        {
+            SkillIds.RemoveAll(id => id == skillId);
+        }
+        private void AddSkillToQueue(string skillId, int InsertOverride = -1)
+        {
+            if (SkillIds.Contains(skillId))
+                return;
+
+            if (InsertOverride == -1)
+                SkillIds.Add(skillId);
+            else
+                SkillIds.Insert(InsertOverride, skillId);
+        }
+        public void AddSkillWithPrerequisites(string skillId)
+        {
+            var dbSkills = Db.Get().Skills;
+            AddSkillRecursively(skillId, dbSkills);
+        }
+
+        private void AddSkillRecursively(string skillId, Skills dbSkills, int InsertStart = -1)
+        {
+            if (SkillIds.Contains(skillId))
+                return;
+
+
+            Skill skill = dbSkills.Get(skillId);
+            if (InsertStart == -1)
+                InsertStart = SkillIds.Count;
+            if (!refResume.HasMasteredSkill(skillId))
+                AddSkillToQueue(skillId, InsertStart);
+
+
+            foreach (var prevSkill in skill.priorSkills)
+            {
+                AddSkillRecursively(prevSkill, dbSkills, InsertStart);
+            }
+
+        }
+        public void LearnedSkill(string skillId)
+        {
+            RemoveSkill(skillId);
+        }
+        public void RemoveSkillAndPostrequisites(string skillId)
+        {
+            RemoveSkillRecursively(skillId, Db.Get().Skills);
+        }
+        private void RemoveSkillRecursively(string skillId, Skills dbSkills)
+        {
+            if (!SkillIds.Contains(skillId))
+                return;
+
+            RemoveSkill(skillId);
+
+            for (int i = SkillIds.Count - 1; i >= 0; --i)
+            {
+                var nextSkill = SkillIds[i];
+
+                Skill skill = dbSkills.Get(nextSkill);
+                if (skill.priorSkills.Contains(skillId))
+                {
+                    RemoveSkillRecursively(nextSkill, dbSkills);
+                }
+            }
+
+        }
+
+        public bool CanMasterSkillAndPrerequisites(string skillId)
+        {
+            return CanMasterSkillRecursively(skillId, Db.Get().Skills);
+        }
+
+        private bool CanMasterSkillRecursively(string skillId, Skills dbSkills)
+        {
+            Skill skill = dbSkills.Get(skillId);
+            MinionResume.SkillMasteryConditions[] masteryConditions = refResume.GetSkillMasteryConditions(skillId);
+            if (masteryConditions.Contains(MinionResume.SkillMasteryConditions.UnableToLearn))
+                return false;
+
+            foreach (var prevSkill in skill.priorSkills)
+            {
+                if (!CanMasterSkillRecursively(prevSkill, dbSkills))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
