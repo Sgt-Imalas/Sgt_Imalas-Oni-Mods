3597d1cfa9be4293e4caba21647cda83eed99192
Sgt_Imalas <bene.schlai@gmx.de>
2024-03-22 00:54:44 +0100

    April fools mod

---
+++ WeebDupe/ModAssets.cs
@@ -0,0 +1,107 @@
+﻿using Database;
+using Klei.AI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static WeebDupe.STRINGS.DUPLICANTS.ROLES;
+
+namespace WeebDupe
+{
+    internal class ModAssets
+    {
+        public static string
+            WEEB_ID = "WD_WEEB"
+            , SpriteID = "CatEarSprite";
+        
+        public class WAccessories
+        {
+            public static void Register(AccessorySlots slots, Accessories accessories)
+            {
+
+                var catEars = Assets.GetAnim("hat_role_weeb1_kanim");
+                AddAccessories(catEars, slots.Hat, accessories);
+            }
+            public static void AddAccessories(KAnimFile file, AccessorySlot slot, ResourceSet parent)
+            {
+                SgtLogger.l(slot.Id);
+
+                var build = file.GetData().build;
+                var id = slot.Id.ToLower();
+
+                for (var i = 0; i < build.symbols.Length; i++)
+                {
+                    var symbolName = HashCache.Get().Get(build.symbols[i].hash);
+                    SgtLogger.l(symbolName);
+
+                    if (symbolName.StartsWith(id))
+                    {
+                        var accessory = new Accessory(symbolName, parent, slot, file.batchTag, build.symbols[i]);
+                        slot.accessories.Add(accessory);
+                        HashCache.Get().Add(accessory.IdHash.HashValue, accessory.Id);
+
+                        SgtLogger.l("Added accessory: " + accessory.Id);
+                    }
+                    else
+                    {
+                        SgtLogger.l($"Symbol {symbolName} in file {file.name} is not starting with {id}");
+                    }
+                }
+            }
+        }
+
+        public class WSkills
+        {
+            public static Skill weeb;
+
+            public static void Register(Skills __instance)
+            {
+                weeb = __instance.Add(new Skill(
+                        WEEB_ID,
+                        STRINGS.DUPLICANTS.ROLES.WEEB.NAME,
+                        STRINGS.DUPLICANTS.ROLES.WEEB.DESCRIPTION,
+                        "",
+                        0,
+                        "hat_role_weeb1",
+                        "skillbadge_role_research1",
+                        Db.Get().SkillGroups.Research.Id,
+                        new List<SkillPerk>
+                        {
+                            WSkillPerks.Weeb1,
+                            WSkillPerks.Weeb2
+
+                        }, new List<string>()
+                        {
+                        }));
+
+            }
+        }
+        public class WSkillPerks
+        {
+            public static SkillPerk Weeb1;
+            public static SkillPerk Weeb2;
+
+            public const string WeebPerk1ID = "WD_WeebPerk1";
+            public const string WeebPerk2ID = "WD_WeebPerk2";
+
+            public static void Register(SkillPerks __instance)
+            {
+                Weeb1 = __instance.Add(new SkillAttributePerk(
+                    WeebPerk1ID,
+                    Db.Get().Attributes.Learning.Id,
+                    2,
+                    STRINGS.DUPLICANTS.ROLES.WEEB.NAME));
+                Weeb2 = __instance.Add(new SkillAttributePerk(
+                    WeebPerk2ID,
+                    Db.Get().Attributes.Decor.Id,
+                    25,
+                    STRINGS.DUPLICANTS.ROLES.WEEB.NAME));
+
+            }
+        }
+    }
+}
---
+++ WeebDupe/Patches.cs
@@ -0,0 +1,58 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static WeebDupe.ModAssets;
+
+namespace WeebDupe
+{
+    internal class Patches
+    {
+        /// <summary>
+        /// add buildings to plan screen
+        /// </summary>
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch(nameof(GeneratedBuildings.LoadGeneratedBuildings))]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+
+            public static void Prefix()
+            {
+                //ModUtil.AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.XXXX, XXXX.ID);
+            }
+        }
+        /// <summary>
+        /// Init. auto translation
+        /// </summary>
+        [HarmonyPatch(typeof(Localization), "Initialize")]
+        public static class Localization_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                LocalisationUtil.Translate(typeof(STRINGS), true);
+            }
+        }
+       
+
+        static class AccessoryPatch
+        {
+            [HarmonyPatch(typeof(Db), "Initialize")]
+            public class Db_Initialize_Patch
+            {
+                public static void Postfix(Db __instance)
+                {
+                    WSkillPerks.Register(__instance.SkillPerks);
+                    WSkills.Register(__instance.Skills);
+                    WAccessories.Register(__instance.AccessorySlots, __instance.Accessories);
+                }
+
+            }
+        }
+    }
+}
