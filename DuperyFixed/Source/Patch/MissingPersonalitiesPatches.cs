37873776266190364ae5bd90af761d9bff6cc50b
Sgt_Imalas <bene.schlai@gmx.de>
2024-04-08 15:18:33 +0200

    dupery fix missing asset check

--- DuperyFixed/Source/Patch/MissingPersonalitiesPatches.cs
+++ DuperyFixed/Source/Patch/MissingPersonalitiesPatches.cs
@@ -1,17 +1,24 @@
-﻿using HarmonyLib;
+﻿using Database;
+using FMOD;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.Assertions.Must;
 using UtilLibs;
+using static HoverTextDrawer;
+using static KCompBuilder;
+using static STRINGS.DUPLICANTS;
+using static STRINGS.NAMEGEN;
 
 namespace DuperyFixed.Source.Patch
 {
     internal class MissingPersonalitiesPatches
     {
 
-        [HarmonyPatch(typeof(Accessorizer), "OnDeserialized")]
+        [HarmonyPatch(typeof(Accessorizer), nameof(Accessorizer.OnDeserialized))]
         public class Accessorizer_OnDeserialized_Patch
         {
             public static void Prefix(Accessorizer __instance)
@@ -19,44 +26,67 @@ namespace DuperyFixed.Source.Patch
                 if (__instance.TryGetComponent<MinionIdentity>(out var identity))
                 {
                     var personality = Db.Get().Personalities.TryGet(identity.personalityResourceId);
-                    if(personality != null)
+                    if (personality != null)
                     {
                         return;
                     }
-                    SgtLogger.l("Personality missing, defaulting to jorge...");
-                    personality = Db.Get().Personalities.GetPersonalityFromNameStringKey(LonelyMinionConfig.PERSONALITY_ID);
+                    SgtLogger.l("Personality of "+__instance.gameObject.name+" is missing, defaulting to jorge. for ui keys");
+                    personality = Db.Get().Personalities.GetPersonalityFromNameStringKey("JORGE");
                     identity.personalityResourceId = personality.Id;
-                    identity.nameStringKey = null;
-
-                    __instance.bodyData = MinionStartingStats.CreateBodyData(personality);
-
-                    //__instance.accessories.RemoveAll(x => x.Get() == null);
-
-                    //var accessorySlots = Db.Get().AccessorySlots;
-                    //Switch(__instance, accessorySlots.HeadShape, __instance.bodyData.headShape);
-                    //Switch(__instance, accessorySlots.Mouth, __instance.bodyData.mouth);
-                    //Switch(__instance, accessorySlots.Eyes, __instance.bodyData.eyes);
-                    //Switch(__instance, accessorySlots.Body, __instance.bodyData.body);
-                    //Switch(__instance, accessorySlots.Arm, __instance.bodyData.arms);
-                    //Switch(__instance, accessorySlots.ArmLower, __instance.bodyData.armslower);
-                    //Switch(__instance, accessorySlots.ArmLowerSkin, __instance.bodyData.armLowerSkin);
-                    //Switch(__instance, accessorySlots.ArmUpperSkin, __instance.bodyData.armUpperSkin);
-                    //Switch(__instance, accessorySlots.Leg, __instance.bodyData.legs);
-                    //Switch(__instance, accessorySlots.LegSkin, __instance.bodyData.legSkin);
-                    //Switch(__instance, accessorySlots.Pelvis, __instance.bodyData.pelvis);
-                    //Switch(__instance, accessorySlots.Cuff, __instance.bodyData.cuff);
-                    //Switch(__instance, accessorySlots.Hand, __instance.bodyData.hand);
-                    //Switch(__instance, accessorySlots.Belt, __instance.bodyData.belt);
-                    //Switch(__instance, accessorySlots.Neck, __instance.bodyData.neck);
+                    identity.nameStringKey = LonelyMinionConfig.PERSONALITY_ID;
+                    SwitchToBackupBodyParts(__instance, personality);
+                    //SwitchToBackupBodyParts__instance.bodyData = Accessorizer.UpdateAccessorySlots(identity.nameStringKey, ref __instance.accessories);           
+                    //__instance.accessories.RemoveAll((ResourceRef<Accessory> x) => x.Get() == null);
+                    //SwitchToBackupBodyParts(__instance, personality);
                 }
             }
-
-            private static void Switch(Accessorizer __instance, AccessorySlot slot, HashedString id)
+            static void SwitchToBackupBodyParts(Accessorizer __instance, Personality fallback)
             {
-                if (__instance.GetAccessory(slot) == null)
-                    __instance.AddAccessory(slot.Lookup(id));
+                //for (int i = __instance.accessories.Count - 1; i >= 0; i--)
+                //{
+                //    if (__instance.accessories[i].Get() == null)
+                //    {
+                //        SgtLogger.l("removing missing accessory");
+                //    }
+                //}
+                __instance.accessories.RemoveAll(x => x.Get() == null);
+                KCompBuilder.BodyData result = MinionStartingStats.CreateBodyData(fallback);
+                {
+                    foreach (AccessorySlot resource in Db.Get().AccessorySlots.resources)
+                    {
+                        if (resource.accessories.Count == 0)
+                        {
+                            SgtLogger.warning(resource.Name + " had not valid resource");
+                            continue;
+                        }
+                        if (__instance.accessories.Any(accessory => accessory.Get().slot == resource))
+                        {
+                            continue;
+                        }
+
+
+                        var accessory = resource.accessories.GetRandom();
+                        if (accessory == null)
+                        {
+                            SgtLogger.warning(resource.Name + " had not valid resource");
+                        }
+                        ResourceRef<Accessory> item = new ResourceRef<Accessory>(accessory);
+                        SgtLogger.l(resource.Name + " was not found, adding " + item.Get().Name);
+                        __instance.accessories.Add(item);
+                    }
+                }
+
+                __instance.bodyData = result;
             }
+
         }
 
+        private static void Switch(Accessorizer __instance, AccessorySlot slot, HashedString id)
+        {
+            if (__instance.GetAccessory(slot) == null)
+                __instance.AddAccessory(slot.Lookup(id));
+        }
     }
+
 }
+
