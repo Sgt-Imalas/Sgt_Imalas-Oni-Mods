404bab9ef44a846dcfdebc65d2d9cae6ee24bac8
Admin <Admin@DESKTOP-I46AJNG>
2024-07-07 00:02:23 +0200

    MPM_Lite: crash fix when folders not instantiated, version check

--- ModProfileManager_Addon/Mod.cs
+++ ModProfileManager_Addon/Mod.cs
@@ -17,6 +17,9 @@ namespace ModProfileManager_Addon
 
             ModAssets.ModPath =  FileSystem.Normalize(Path.Combine(Path.Combine(KMod.Manager.GetDirectory(), "config"), "[ModSync]StoredModConfigs"));
             ModAssets.ModPacksPath = FileSystem.Normalize(Path.Combine(ModAssets.ModPath, "[StandAloneModLists]"));
+
+            System.IO.Directory.CreateDirectory(ModAssets.ModPath);
+            System.IO.Directory.CreateDirectory(ModAssets.ModPacksPath);
             ModAssets.LoadAssets();
 
             ModSyncUtils.RegisterModAsSyncMod(this.mod);
