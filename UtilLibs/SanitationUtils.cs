93ba8f20d6456ed2e5b567053b9693300d462b8b
Admin <Admin@DESKTOP-I46AJNG>
2024-07-12 15:18:04 +0200

    sanitation util expanded

--- UtilLibs/SanitationUtils.cs
+++ UtilLibs/SanitationUtils.cs
@@ -54,5 +54,28 @@ namespace UtilLibs
             return modName;
         }
 
+        public static string GetSanitizedNamePath(string source)
+        {
+            //SgtLogger.l("Sanitizing...");
+            //SgtLogger.l(source, "1");
+            source = Path.GetFileName(source);
+            //SgtLogger.l(source, "2");
+            source = ReplaceInvalidChars(source);
+            //SgtLogger.l(source, "3");
+
+            if (ForbiddenNames.Contains(source.ToUpperInvariant()))
+            {
+                SgtLogger.l("file name was one of the forbidden ones, replacing..");
+                source = Path.GetRandomFileName();
+                //SgtLogger.l(source, "4");
+            }
+
+            return source;
+        }
+
+        public static string ReplaceInvalidChars(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
     }
 }
