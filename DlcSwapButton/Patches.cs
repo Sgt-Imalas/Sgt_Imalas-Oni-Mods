490decad8eedef9d46517891f2d61a441c2a9969
Sgt_Imalas <bene.schlai@gmx.de>
2024-07-26 14:52:44 +0200

    dlc button fix + dss version++

--- DlcSwapButton/Patches.cs
+++ DlcSwapButton/Patches.cs
@@ -22,7 +22,7 @@ namespace DlcSwapButton
             public static void Postfix(MainMenu __instance)
             {
                 bool expansion1Active = DlcManager.IsExpansion1Active();
-                System.Action action = ()=> { };
+                System.Action action = ()=> { DlcManager.ToggleDLC("EXPANSION1_ID"); };
 
                 var btn = __instance.MakeButton(new MainMenu.ButtonInfo(
                     expansion1Active ? UI.FRONTEND.MAINMENU.DLC.DEACTIVATE_EXPANSION1 : UI.FRONTEND.MAINMENU.DLC.ACTIVATE_EXPANSION1,
@@ -30,8 +30,6 @@ namespace DlcSwapButton
                     22, __instance.normalButtonStyle)
                 );
                 btn.gameObject.SetActive(false);
-                var toggle = btn.gameObject.AddComponent<DLCToggle>();
-                toggle.button = btn;
 
                 btn.gameObject.SetActive(true);
             }
