acff457060612cf96cabd8c25451aa33269731b3
Sgt_Imalas <bene.schlai@gmx.de>
2024-11-08 02:29:50 +0100

    satisfying powershards: anim border fixes, crystal growth thing

--- SatisfyingPowerShards/Components/PowerShardGrowthMonitor.cs
+++ SatisfyingPowerShards/Components/PowerShardGrowthMonitor.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Klei.AI;
+using KSerialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SatisfyingPowerShards.Components
 {
-	internal class PowerShardGrowthMonitor:KMonoBehaviour
+	internal class PowerShardGrowthMonitor:KMonoBehaviour, ISim4000ms
 	{
+		[Serialize][SerializeField]
+		public float ShardGrowth = 0;
+
+		[Serialize][SerializeField]
+		public float ShardGrowthPerDay;
+
+
+		AttributeInstance metabolism;
+
+		public override void OnSpawn()
+		{
+			base.OnSpawn();
+			this.metabolism = this.gameObject.GetAttributes().Add(Db.Get().CritterAttributes.Metabolism);
+		}
+
+		public void Sim4000ms(float dt)
+		{
+			
+		}
 	}
 }
