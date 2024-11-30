1c1fbcdcd2ad66320509509c78ec564e469cd625
Sgt_Imalas <bene.schlai@gmx.de>
2024-11-30 03:14:45 +0100

    trait finder export moved to export tool

--- AnimExportTool/Patches.cs
+++ AnimExportTool/Patches.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Klei;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -381,6 +382,115 @@ namespace AnimExportTool
 		//        }
 		//    }
 		//}
+
+
+		[HarmonyPatch(typeof(MainMenu), nameof(MainMenu.OnPrefabInit))]
+		public class MainMenu_OnPrefabInit
+		{
+			public class VanillaStarmapLocation
+			{
+				public string Id;
+				public string Name;
+				public string Image;
+			}
+
+			public class Asteroid
+			{
+				public string Id;
+				public string Name;
+				public string Image;
+				public bool DisableWorldTraits = false;
+				public List<ProcGen.World.TraitRule> TraitRules;
+				public float worldTraitScale;
+			}
+			public class ClusterLayout
+			{
+				public string Id;
+				public string Name;
+				public string Prefix;
+				public int menuOrder;
+				public int startWorldIndex;
+				public string[] RequiredDlcsIDs;
+				public string[] ForbiddenDlcIDs;
+				public List<string> WorldPlacementIDs;
+				public int clusterCategory;
+				public int fixedCoordinate;
+			}
+			public class DataExport
+			{
+				public List<ClusterLayout> clusters = new();
+				public List<Asteroid> asteroids = new();
+				public List<WorldTrait> worldTraits = new();
+
+			}
+			public class WorldTrait
+			{
+				public string Id;
+				public string Name, ColorHex;
+				public List<string> forbiddenDLCIds, exclusiveWith, exclusiveWithTags, traitTags;
+				public Dictionary<string, int> globalFeatureMods { get; set; }
+
+				public WorldTrait()
+				{
+					exclusiveWith = new List<string>();
+					exclusiveWithTags = new List<string>();
+					forbiddenDLCIds = new List<string>();
+					traitTags = new List<string>();
+					Name = string.Empty;
+					Id = string.Empty;
+				}
+			}
+
+
+			public static void Postfix()
+			{
+				var export = new DataExport();
+				foreach (var cluster in ProcGen.SettingsCache.clusterLayouts.clusterCache.Values)
+				{
+					var data = new ClusterLayout();
+					data.Id = cluster.filePath;
+					data.Name = Strings.Get(cluster.name);
+					data.Prefix = cluster.coordinatePrefix;
+					data.menuOrder = cluster.menuOrder;
+					data.RequiredDlcsIDs = cluster.requiredDlcIds;
+					data.ForbiddenDlcIDs = cluster.forbiddenDlcIds;
+					//data.WorldPlacements = cluster.worldPlacements;
+					data.startWorldIndex = cluster.startWorldIndex;
+
+					data.WorldPlacementIDs = cluster.worldPlacements.Select(pl => pl.world).ToList();
+					data.clusterCategory = (int)cluster.clusterCategory;
+					data.fixedCoordinate = cluster.fixedCoordinate;
+					export.clusters.Add(data);
+				}
+				foreach (var world in ProcGen.SettingsCache.worlds.worldCache.Values)
+				{
+					var data = new Asteroid();
+					data.Id = world.filePath;
+					data.Name = Strings.Get(world.name);
+					data.DisableWorldTraits = world.disableWorldTraits;
+					data.TraitRules = world.worldTraitRules;
+					data.worldTraitScale = world.worldTraitScale;
+
+					export.asteroids.Add(data);
+				}
+				foreach (var trait in ProcGen.SettingsCache.worldTraits.Values)
+				{
+					var data = new WorldTrait();
+					data.Id = trait.filePath;
+					data.Name = Strings.Get(trait.name);
+					data.ColorHex = trait.colorHex;
+					data.forbiddenDLCIds = trait.forbiddenDLCIds;
+					data.exclusiveWith = trait.exclusiveWith;
+					data.exclusiveWithTags = trait.exclusiveWithTags;
+					data.traitTags = trait.traitTags;
+					data.globalFeatureMods = trait.globalFeatureMods;
+
+					export.worldTraits.Add(data);
+				}
+				Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+				Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(export));
+			}
+		}
 
 	}
 }
