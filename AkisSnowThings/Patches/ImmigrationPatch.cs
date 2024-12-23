e44949183a1fdd1602a6b1f0f1725b1be113923b
Sgt_Imalas <bene.schlai@gmx.de>
2024-12-23 02:16:33 +0100

    SnowMod; seed conversion recipe + care package

---
+++ AkisSnowThings/Patches/ImmigrationPatch.cs
@@ -0,0 +1,23 @@
+﻿using AkisSnowThings.Content.Defs.Plants;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkisSnowThings.Patches
+{
+	internal class ImmigrationPatch
+	{
+		[HarmonyPatch(typeof(Immigration), nameof(Immigration.ConfigureCarePackages))]
+		public class AdditionalCarePackages
+		{
+			public static void Postfix(Immigration __instance)
+			{
+				__instance.carePackages.Add(new CarePackageInfo(EvergreenTreeConfig.SEED_ID, 3f, null));
+
+			}
+		}
+	}
+}
---
+++ AkisSnowThings/Patches/SupermaterialRefineryPatch.cs
@@ -0,0 +1,59 @@
+﻿using AkisSnowThings.Content.Defs.Plants;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ComplexRecipe;
+
+namespace AkisSnowThings.Patches
+{
+	internal class SupermaterialRefineryPatch
+	{
+		[HarmonyPatch(typeof(SupermaterialRefineryConfig), nameof(SupermaterialRefineryConfig.ConfigureBuildingTemplate))]
+		public class AddSeedRecipes
+		{
+			static ComplexRecipe AcornToPinecone;
+			public static void Postfix(Immigration __instance)
+			{
+				AddArborAcornConversionRecipe();
+
+				if(DlcManager.IsContentSubscribed(DlcManager.DLC2_ID))
+					AddBonBonSeedConversionRecipe();
+			}
+			public static void AddArborAcornConversionRecipe() {
+				//arbor acorn to pine cone
+				RecipeElement[] input = [new RecipeElement(ForestTreeConfig.SEED_ID, 1f)];
+
+				RecipeElement[] output = [new RecipeElement(EvergreenTreeConfig.SEED_ID, 1f)];
+				string recipeID = ComplexRecipeManager.MakeRecipeID(CraftingTableConfig.ID, input, output);
+
+				AcornToPinecone = new ComplexRecipe(recipeID, input, output)
+				{
+					time = 40,
+					description = STRINGS.CREATURES.SPECIES.SEEDS.SNOWSCULPTURES_EVERGREEN_TREE.DESC,
+					nameDisplay = RecipeNameDisplay.IngredientToResult,
+					fabricators = new List<Tag> { SupermaterialRefineryConfig.ID }
+				};
+			}
+			public static void AddBonBonSeedConversionRecipe()
+			{
+
+				//bonbon tree seed to arbor acorn
+				RecipeElement[] input = [new RecipeElement(SpaceTreeConfig.SEED_ID, 1f)];
+
+				RecipeElement[] output = [new RecipeElement(EvergreenTreeConfig.SEED_ID, 1f)];
+				string recipeID = ComplexRecipeManager.MakeRecipeID(CraftingTableConfig.ID, input, output);
+
+				AcornToPinecone = new ComplexRecipe(recipeID, input, output)
+				{
+					time = 40,
+					description = STRINGS.CREATURES.SPECIES.SEEDS.SNOWSCULPTURES_EVERGREEN_TREE.DESC,
+					nameDisplay = RecipeNameDisplay.IngredientToResult,
+					fabricators = new List<Tag> { SupermaterialRefineryConfig.ID }
+				};
+			}
+		}
+	}
+}
