a17dd006cf0b397d7f1716dd437b8863aab88c8b
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-06 21:42:05 +0100

    DSS: safeguard preventing other mods crashing bionic generation by having interest bonus >0

---
+++ SetStartDupes/Patches/CrashMitigationPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetStartDupes.Patches
+{
+	internal class CrashMitigationPatches
+	{
+		[HarmonyPatch(typeof(MinionStartingStats), nameof(MinionStartingStats.GenerateAttributes))]
+		public class FixBionicCrash
+		{
+			/// <summary>
+			/// forces bionic dupes to have 0 attribute bonus, as this crashes otherwise
+			/// </summary>
+			/// <param name="pointsDelta"></param>
+			/// <param name="__instance"></param>
+			[HarmonyPriority(Priority.LowerThanNormal)]
+			public static void Prefix(ref int pointsDelta, MinionStartingStats __instance)
+			{
+				if(__instance.personality.model == GameTags.Minions.Models.Bionic)
+				{
+					pointsDelta = 0;
+				}
+			}
+		}
+	}
+}
