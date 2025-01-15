13520125b1a6212955720dedba52c8b7daaa485e
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-15 01:33:31 +0100

    loading pip mod

---
+++ LoadingPip/Mod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+using UtilLibs;
+
+namespace LoadingPip
+{
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+			SgtLogger.LogVersion(this, harmony);
+		}
+	}
+}
---
+++ LoadingPip/Patches.cs
@@ -0,0 +1,48 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UtilLibs;
+using static LoadingPip.ModAssets;
+
+namespace LoadingPip
+{
+	internal class Patches
+	{
+
+		[HarmonyPatch(typeof(LoadingOverlay), nameof(LoadingOverlay.Load))]
+		public class Overlay_Icon_Replace
+		{
+			//replace loading dupe face with pip
+			public static void Postfix()
+			{
+				var instance = LoadingOverlay.instance;
+				var image = instance.transform.Find("Image").GetComponent<Image>();
+				var pipSprite = Def.GetUISprite(Assets.GetPrefab(SquirrelConfig.ID));
+				image.preserveAspect = true;
+				image.sprite = pipSprite.first;
+
+				var rect = image.rectTransform();
+				rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 200);
+			}
+		}
+
+		/// <summary>
+		/// Init. auto translation
+		/// </summary>
+		[HarmonyPatch(typeof(Localization), "Initialize")]
+		public static class Localization_Initialize_Patch
+		{
+			public static void Postfix()
+			{
+				LocalisationUtil.Translate(typeof(STRINGS), true);
+			}
+		}
+	}
+}
