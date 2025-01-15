a99eae139d8b31ed6159b4c3c52a96c57b52344f
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-15 23:57:26 +0100

    FieldGetterSetter delegate util

--- UtilLibs/InjectionMethods.cs
+++ UtilLibs/InjectionMethods.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
 using UnityEngine;
 
 namespace UtilLibs
@@ -72,7 +74,43 @@ namespace UtilLibs
 				SgtLogger.l(anim+"; "+anim.name+" added to group");
 			}
 		}
+		public static Func<S, T> CreateGetter<S, T>(FieldInfo field)
+		{
+			string methodName = field.ReflectedType.FullName + ".get_" + field.Name;
+			DynamicMethod setterMethod = new DynamicMethod(methodName, typeof(T), new Type[1] { typeof(S) }, true);
+			ILGenerator gen = setterMethod.GetILGenerator();
+			if (field.IsStatic)
+			{
+				gen.Emit(OpCodes.Ldsfld, field);
+			}
+			else
+			{
+				gen.Emit(OpCodes.Ldarg_0);
+				gen.Emit(OpCodes.Ldfld, field);
+			}
+			gen.Emit(OpCodes.Ret);
+			return (Func<S, T>)setterMethod.CreateDelegate(typeof(Func<S, T>));
+		}
 
+		public static Action<S, T> CreateSetter<S, T>(FieldInfo field)
+		{
+			string methodName = field.ReflectedType.FullName + ".set_" + field.Name;
+			DynamicMethod setterMethod = new DynamicMethod(methodName, null, new Type[2] { typeof(S), typeof(T) }, true);
+			ILGenerator gen = setterMethod.GetILGenerator();
+			if (field.IsStatic)
+			{
+				gen.Emit(OpCodes.Ldarg_1);
+				gen.Emit(OpCodes.Stsfld, field);
+			}
+			else
+			{
+				gen.Emit(OpCodes.Ldarg_0);
+				gen.Emit(OpCodes.Ldarg_1);
+				gen.Emit(OpCodes.Stfld, field);
+			}
+			gen.Emit(OpCodes.Ret);
+			return (Action<S, T>)setterMethod.CreateDelegate(typeof(Action<S, T>));
+		}
 		public static void AddStatusItem(string status_id, string category, string name, string desc)
 		{
 			status_id = status_id.ToUpperInvariant();
