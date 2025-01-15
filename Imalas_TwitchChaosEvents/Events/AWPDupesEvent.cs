cbf3b6048c5ec71a2285fa6a38cfb9c98e12134c
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-15 01:31:20 +0100

    new twitch event idea

---
+++ Imalas_TwitchChaosEvents/Events/AWPDupesEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imalas_TwitchChaosEvents.Events
+{
+	/// <summary>
+	/// dupes gain an AWP gun for killing dupes (csgo cutout)
+	/// also add sound when they "fire" it
+	/// </summary>
+	internal class AWPDupesEvent
+	{
+	}
+}
