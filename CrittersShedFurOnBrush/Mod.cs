090dfa66604cc974589180a4edb6571348ff8f77
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-15 23:56:54 +0100

    Critter Shedding temp integration, zh localization

--- CrittersShedFurOnBrush/Mod.cs
+++ CrittersShedFurOnBrush/Mod.cs
@@ -2,7 +2,9 @@
 using KMod;
 using PeterHan.PLib.Core;
 using PeterHan.PLib.Options;
+using System.Collections.Generic;
 using UtilLibs;
+using static CrittersShedFurOnBrush.ModIntegrations;
 
 namespace CrittersShedFurOnBrush
 {
@@ -15,6 +17,13 @@ namespace CrittersShedFurOnBrush
 			ModAssets.InitSheddables();
 			SgtLogger.LogVersion(this, harmony);
 			base.OnLoad(harmony);
+		}
+		public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<KMod.Mod> mods)
+		{
+			base.OnAllModsLoaded(harmony, mods);
+			CompatibilityNotifications.FixBrokenTimeout(harmony);
+			UpgradableCritters_Integration.Init();
+
 		}
 	}
 }
