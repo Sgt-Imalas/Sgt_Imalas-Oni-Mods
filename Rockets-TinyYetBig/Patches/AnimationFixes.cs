8def13821f821cc069ee5aeca887acab0fe335d9
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-01-24 00:57:52 +0100

    RE: Remove LOX meter fix - Klei fixed the bug in latest update; libs updated to latest

--- Rockets-TinyYetBig/Patches/AnimationFixes.cs
+++ Rockets-TinyYetBig/Patches/AnimationFixes.cs
@@ -8,57 +8,6 @@ namespace Rockets_TinyYetBig.Patches
 {
 	internal class AnimationFixes
 	{
-
-		/// <summary>
-		/// Vanilla Liquid Oxidizer Tank meter animation broke sometime in early 2023, this fixes it, part 1:
-		/// Registering the meter controller
-		/// </summary>
-		[HarmonyPatch(typeof(OxidizerTank), nameof(OxidizerTank.OnSpawn))]
-		public static class FixLOXMeterV1
-		{
-
-			public static void Postfix(OxidizerTank __instance)
-			{
-				///this is a LOX module, it needs a meter.. but it wont get one in kleis implementation, so we need to add one
-				if (!__instance.supportsMultipleOxidizers)
-				{
-					__instance.meter = new MeterController(__instance.GetComponent<KBatchedAnimController>(), "meter_target", "meter", Meter.Offset.Infront, Grid.SceneLayer.NoLayer, new string[4]
-						{
-							"meter_target",
-							"meter_fill",
-							"meter_frame",
-							"meter_OL"
-					});
-					__instance.meter.gameObject.GetComponent<KBatchedAnimTracker>().matchParentOffset = true;
-					__instance.RefreshMeter();
-				}
-				//this.meter.SetPositionPercent(this.storage.MassStored() / this.storage.capacityKg);
-			}
-		}
-
-		/// <summary>
-		/// Vanilla Liquid Oxidizer Tank meter animation broke sometime in early 2023, this fixes it, part 2:
-		/// Refreshing the meter 
-		/// </summary>
-		[HarmonyPatch(typeof(OxidizerTank), nameof(OxidizerTank.RefreshMeter))]
-		public static class FixLOXMeterV2
-		{
-			/// <summary>
-			/// Apply Meter changes on LOX module
-			/// </summary>
-			/// <param name="__instance"></param>
-			public static void Postfix(OxidizerTank __instance)
-			{
-				if (!__instance.supportsMultipleOxidizers && __instance.meter != null)
-				{
-					float percentage = __instance.storage.MassStored() / __instance.storage.capacityKg;
-					if (percentage.IsNaNOrInfinity())
-						percentage = 0f;
-					__instance.meter.SetPositionPercent(percentage);
-				}
-			}
-		}
-
 		//[HarmonyPatch(typeof(CraftModuleInterface), nameof(CraftModuleInterface.DoLand))]
 		////[HarmonyPatch(new Type[] { typeof(LaunchPad), typeof(bool) })]
 		//public static class Fix_SmallRocketModulesLosetheirAttachmentsOnLand
