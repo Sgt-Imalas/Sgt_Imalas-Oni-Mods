521dfd7df1bec47f1214958aa184c617887df1c2
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-02-08 21:09:35 +0100

    void world for TabeYukiro

--- 1LineCrasher/Mod.cs
+++ 1LineCrasher/Mod.cs
@@ -1,13 +1,16 @@
 ﻿
 
+using HarmonyLib;
+using KMod;
+
 namespace _1LineCrasher
 {
-	//public class Mod : UserMod2
-	//{
-	//    public override void OnLoad(Harmony harmony)
-	//    {
-	//        base.OnLoad(harmony);
-	//    }
-	//}
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+		}
+	}
 
 }
