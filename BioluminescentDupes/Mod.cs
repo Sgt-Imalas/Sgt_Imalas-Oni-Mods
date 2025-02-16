2c6bd46bf685b40f6f26de38318e6cd2a39967a4
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-02-16 20:08:17 +0100

    GlowDupes: fix translations

--- BioluminescentDupes/Mod.cs
+++ BioluminescentDupes/Mod.cs
@@ -1,16 +1,25 @@
 ﻿using HarmonyLib;
 using KMod;
 using System;
+using System.Collections.Generic;
 using UtilLibs;
 
 namespace BioluminescentDupes
 {
 	public class Mod : UserMod2
 	{
+		public static Harmony Harmony;
 		public override void OnLoad(Harmony harmony)
 		{
 			base.OnLoad(harmony);
 			SgtLogger.LogVersion(this, harmony);
+			Harmony = harmony;
+		}
+		public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<KMod.Mod> mods)
+		{
+			base.OnAllModsLoaded(harmony, mods);
+			CompatibilityNotifications.FlagLoggingPrevention(mods);
+			CompatibilityNotifications.FixBrokenTimeout(harmony);
 		}
 	}
 }
--- BioluminescentDupes/Patches/LocalizationPatch.cs
+++ BioluminescentDupes/Patches/LocalizationPatch.cs
@@ -1,10 +1,14 @@
-﻿using HarmonyLib;
+﻿using BioluminescentDupes.Content.Scripts;
+using HarmonyLib;
+using Klei.AI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TUNING;
 using UtilLibs;
+using static BioluminescentDupes.STRINGS.DUPLICANTS;
 
 namespace BioluminescentDupes.Patches
 {
@@ -16,6 +20,28 @@ namespace BioluminescentDupes.Patches
 			public static void Postfix()
 			{
 				LocalisationUtil.Translate(typeof(STRINGS), true);
+				RegisterTrait();
+			}
+
+			static void RegisterTrait()
+			{
+				TUNING.TRAITS.TRAIT_CREATORS.Add(TraitUtil.CreateComponentTrait<BD_Bioluminescence>(BD_Bioluminescence.ID, STRINGS.DUPLICANTS.TRAITS.BD_BIOLUMINESCENCE.NAME, STRINGS.DUPLICANTS.TRAITS.BD_BIOLUMINESCENCE.DESC, true));
+				if (!DUPLICANTSTATS.GOODTRAITS.Contains(BD_Bioluminescence.GetTrait()))
+				{
+					DUPLICANTSTATS.GOODTRAITS.Add(BD_Bioluminescence.GetTrait());
+					if (DUPLICANTSTATS.GOODTRAITS.Any(traitval => traitval.id == nameof(GlowStick)))
+					{
+						var glowstick = DUPLICANTSTATS.GOODTRAITS.First(traitval => traitval.id == nameof(GlowStick));
+						if (glowstick.mutuallyExclusiveTraits == null)
+						{
+							glowstick.mutuallyExclusiveTraits = [BD_Bioluminescence.ID];
+						}
+						else
+						{
+							glowstick.mutuallyExclusiveTraits.Add(BD_Bioluminescence.ID);
+						}
+					}
+				}
 			}
 		}
 	}
