2c94c2305e1fc3bcd565df81ac54a41af204680d
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-02-28 13:37:54 +0100

    RetroOni: powerbank building anim restoration, fried steak

---
+++ OniRetroEdition/ModPatches/DeepFryerConfigPatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OniRetroEdition.ModPatches
+{
+	internal class DeepFryerConfigPatch
+	{
+
+        [HarmonyPatch(typeof(DeepfryerConfig), nameof(DeepfryerConfig.ConfigureRecipes))]
+        public class DeepfryerConfig_ConfigureRecipes_Patch
+        {
+            public static void Postfix(DeepfryerConfig __instance)
+			{
+				ComplexRecipe.RecipeElement[] ingredients = [
+					new ComplexRecipe.RecipeElement((Tag) "Meat", 1f),
+					new ComplexRecipe.RecipeElement((Tag) "Tallow", 2f)	];
+				ComplexRecipe.RecipeElement[] products = [new ComplexRecipe.RecipeElement((Tag) "DeepFriedMeat", 1f, ComplexRecipe.RecipeElement.TemperatureOperation.Heated)];
+				DeepFriedMeatConfig.recipe = new ComplexRecipe(ComplexRecipeManager.MakeRecipeID("Deepfryer", (IList<ComplexRecipe.RecipeElement>)ingredients, (IList<ComplexRecipe.RecipeElement>)products), ingredients, products, DlcManager.AVAILABLE_DLC_2)
+				{
+					time = TUNING.FOOD.RECIPES.STANDARD_COOK_TIME,
+					description = global::STRINGS.ITEMS.FOOD.DEEPFRIEDMEAT.RECIPEDESC,
+					nameDisplay = ComplexRecipe.RecipeNameDisplay.Result,
+					fabricators = new List<Tag>() { (Tag)"Deepfryer"},
+					sortOrder = 300
+				};
+			}
+        }
+	}
+}
---
+++ OniRetroEdition/ModPatches/ElectrobankDischargerPatches.cs
@@ -0,0 +1,89 @@
+﻿using FMOD;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace OniRetroEdition.ModPatches
+{
+	internal class ElectrobankDischargerPatches
+    {
+        [HarmonyPatch(typeof(ElectrobankDischarger.States), nameof(ElectrobankDischarger.States.InitializeStates))]
+        public class ElectrobankDischarger_States_InitializeStates_Patch
+        {
+            public static void Postfix(ElectrobankDischarger.States __instance)
+            {
+                __instance.inoperational.enterActions.Clear();
+				__instance.inoperational
+					.PlayAnim("on")
+					.Enter((smi) => smi.master.UpdateMeter())
+					.EnterTransition(__instance.discharging_pst, (smi) => smi.master.storage.items.Count == 0);
+                __instance.discharging_pst.OnAnimQueueComplete(__instance.noBattery);
+				__instance.discharging_pst.Exit(smi=> smi.master.UpdateSymbolSwap());	
+			}
+        }
+
+		/// <summary>
+		/// Prevent the anim override clearing when the battery gets ejected to keep it in the pst anim
+		/// </summary>
+		[HarmonyPatch(typeof(ElectrobankDischarger), nameof(ElectrobankDischarger.UpdateSymbolSwap))]
+		public class ElectrobankDischarger_UpdateSymbolSwap_Patch
+		{
+			public static bool Prefix(ElectrobankDischarger __instance)
+			{
+				return __instance.storage.items.Count > 0;
+			}
+		}
+
+		[HarmonyPatch(typeof(ElectrobankCharger), nameof(ElectrobankCharger.InitializeStates))]
+		public class ElectrobankCharger_InitializeStates_Patch
+		{
+			public static void Postfix(ElectrobankCharger __instance)
+			{
+				__instance.inoperational.enterActions.Clear();
+				__instance.inoperational
+					.PlayAnim("working_pst")
+					.QueueAnim("on");
+			}
+		}
+
+		[HarmonyPatch(typeof(ElectrobankCharger.Instance), nameof(ElectrobankCharger.Instance.UpdateMeter))]
+		public class ElectrobankCharger_Instance_UpdateMeter_Patch
+		{
+			public static void Postfix(ElectrobankCharger.Instance __instance)
+			{
+				UpdateSymbolSwap(__instance);
+			}
+			public static void UpdateSymbolSwap(ElectrobankCharger.Instance smi)
+			{
+				KBatchedAnimController component = smi.GetComponent<KBatchedAnimController>();
+				SymbolOverrideController component2 = component.GetComponent<SymbolOverrideController>();
+				//component.SetSymbolVisiblity("electrobank_l", is_visible: false);
+				if (smi.Storage.items.Count > 0)
+				{
+					KAnim.Build.Symbol source_symbol = smi.Storage.items[0].GetComponent<KBatchedAnimController>().AnimFiles[0].GetData().build.symbols[0];
+					component2.AddSymbolOverride("electrobank_l", source_symbol);
+				}
+				else
+				{
+					component2.RemoveSymbolOverride("electrobank_l");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Register SOC
+		/// </summary>
+		[HarmonyPatch(typeof(ElectrobankChargerConfig), nameof(ElectrobankChargerConfig.DoPostConfigureComplete))]
+		public class ElectrobankChargerConfig_DoPostConfigureComplete_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				SymbolOverrideControllerUtil.AddToPrefab(go);
+			}
+		}
+	}
+}
