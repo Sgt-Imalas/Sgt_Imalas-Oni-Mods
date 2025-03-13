94f2298b1cbbf61cd3bb8900ae599df3a0ca6253
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-03-13 02:44:19 +0100

    RE: battery meter fix, finalized advLaunchpad anim, added launchpad anim fix

---
+++ Rockets-TinyYetBig/Patches/Generator_Patches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockets_TinyYetBig.Patches
+{
+	internal class Generator_Patches
+	{
+        ///// <summary>
+        ///// Prevent engines from showing the "no power wire connected" statusitem
+        ///// </summary>
+        //[HarmonyPatch(typeof(Generator), nameof(Generator.SetStatusItem))]
+        //public class Generator_SetStatusItem_Patch
+        //{
+        //    public static bool Prefix(Generator __instance)
+        //    {
+        //        return !__instance.TryGetComponent<RocketModuleCluster>(out _);
+        //    }
+        //}
+        
+	}
+}
