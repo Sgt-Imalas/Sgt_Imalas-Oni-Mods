dcac8ac688c9b7a1f2f9168d5f96db03479ea0c5
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-03-15 21:51:39 +0100

    Fix MPML image load error

--- ModProfileManager_Addon/Patches/SpritePatch.cs
+++ ModProfileManager_Addon/Patches/SpritePatch.cs
@@ -6,20 +6,20 @@ namespace ModProfileManager_Addon.Patches
 {
 	internal class SpritePatch
 	{
-		public static string import = "MPM_IMPORT";
-		public static string export = "MPM_EXPORT";
+		//public static string import = "MPM_IMPORT";
+		//public static string export = "MPM_EXPORT";
 
 
-		[HarmonyPatch(typeof(Assets), "OnPrefabInit")]
-		public class Assets_OnPrefabInit_Patch
-		{
-			[HarmonyPriority(Priority.LowerThanNormal)]
-			public static void Prefix(Assets __instance)
-			{
+		//[HarmonyPatch(typeof(Assets), "OnPrefabInit")]
+		//public class Assets_OnPrefabInit_Patch
+		//{
+		//	[HarmonyPriority(Priority.LowerThanNormal)]
+		//	public static void Prefix(Assets __instance)
+		//	{
 
-				ModAssets.ImportSprite = InjectionMethods.AddSpriteToAssets(__instance, import);
-				ModAssets.ExportSprite = InjectionMethods.AddSpriteToAssets(__instance, export);
-			}
-		}
+		//		ModAssets.ImportSprite = InjectionMethods.AddSpriteToAssets(__instance, import);
+		//		ModAssets.ExportSprite = InjectionMethods.AddSpriteToAssets(__instance, export);
+		//	}
+		//}
 	}
 }
