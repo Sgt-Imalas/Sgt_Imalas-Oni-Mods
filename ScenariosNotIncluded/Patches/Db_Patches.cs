f05ddbe051a5987e067dc65229115e553dd77d02
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-04-05 01:34:51 +0200

    scenario loader idea

---
+++ ScenariosNotIncluded/Patches/Db_Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScenariosNotIncluded.Patches
+{
+    class Db_Patches
+    {
+
+        [HarmonyPatch(typeof(Db), nameof(Db.Initialize))]
+        public class Db_Initialize_Patch
+        {
+            public static void Postfix(Db __instance)
+            {
+            }
+        }
+    }
+}
---
+++ ScenariosNotIncluded/Patches/MainMenu_Patches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Klei;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static StateMachine;
+
+namespace ScenariosNotIncluded.Patches
+{
+    class MainMenu_Patches
+	{
+		[HarmonyPatch(typeof(MainMenu), nameof(MainMenu.OnPrefabInit))]
+		public static class MainMenu_OnPrefabInit_Patch
+		{
+			public static void Prefix(MainMenu __instance)
+			{
+				var openMenu =  () =>
+				{
+					var ModUploader = Util.KInstantiateUI(ScreenPrefabs.Instance.RailModUploadMenu.gameObject, Global.Instance.globalCanvas, true);
+				};
+				
+				__instance.MakeButton(new MainMenu.ButtonInfo("Open Mod Menu", openMenu, 22, __instance.normalButtonStyle));
+
+			}
+		}
+	}
+}
