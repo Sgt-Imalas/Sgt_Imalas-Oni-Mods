87f92c8009acd3992cd262104358412f7bafc32b
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-04-05 22:38:28 +0200

    legacy

---
+++ RonivansLegacy_ChemicalProcessing/Mod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+using UtilLibs;
+
+namespace RonivansLegacy_ChemicalProcessing
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            SgtLogger.LogVersion(this, harmony); 
+		}
+    }
+}
