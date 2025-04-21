cf85b536595f730b553a9481660ed669762e5085
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-04-22 00:54:49 +0200

    TwitchLib: Get Events from other mods

--- Util_TwitchIntegrationLib/EventRegistration.cs
+++ Util_TwitchIntegrationLib/EventRegistration.cs
@@ -1,15 +1,20 @@
-﻿using ONITwitchLib;
+﻿using HarmonyLib;
+using ONITwitchLib;
 using ONITwitchLib.Core;
+using ONITwitchLib.Logger;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Util_TwitchIntegrationLib
 {
 	public static class EventRegistration
 	{
 		public static Dictionary<string, ONITwitchLib.EventInfo> Events = new Dictionary<string, ONITwitchLib.EventInfo>();
+		public static Dictionary<string, ONITwitchLib.EventInfo> GlobalCachedEvents = null;
 		public static void InitializeTwitchEventsInNameSpace(string nameSpace)
 		{
 			if (!TwitchModInfo.TwitchIsPresent)
@@ -24,10 +29,105 @@ namespace Util_TwitchIntegrationLib
 		public static bool TryGetEvent(string eventId, out ONITwitchLib.EventInfo eventInfo)
 		{
 			eventInfo = null;
-			return Events.TryGetValue(eventId, out eventInfo);
+			if (Events.TryGetValue(eventId, out eventInfo) && eventInfo != null)
+			{
+				return true;
+			}
+			CacheGlobalEventList();
+			if (GlobalCachedEvents.TryGetValue(eventId, out eventInfo) && eventInfo != null)
+			{
+				return true;
+			}
+			return false;
 		}
+		static void CacheGlobalEventList()
+		{
+			if (GlobalCachedEvents != null)
+				return;
+			GlobalCachedEvents = new();
+
+			var t_EventManager = Type.GetType("ONITwitch.EventLib.EventManager, ONITwitch");
+
+			if (t_EventManager == null)
+			{
+				Debug.LogWarning("t_EventManager type is null");
+				return;
+			}
+
+			var f_Instance = AccessTools.Field(t_EventManager, "instance");
+
+			if (f_Instance == null)
+			{
+				Debug.LogWarning("f_Instance is null");
+				return;
+			}
+			var instance = f_Instance.GetValue(null);
+			if (instance == null)
+			{
+				Debug.LogWarning("EventManager instance is null");
+				return;
+			}
+
+			foreach (var field in AccessTools.GetFieldNames(t_EventManager.GetType()))
+			{
+				Console.WriteLine("field in eventManager: "+field);
+			}
+
+			var regex = new Regex(@"(.*)\.(.*)");
+
+			try
+			{
+				var events = Traverse.Create(instance).Field("registeredEvents")?.GetValue<IDictionary>();
+				Debug.Log(events);
+				Debug.Log("count: "+events.Count);
+				var eventmanager = EventManager.Instance;
+					
+				foreach (var nameSpacedEvent in events.Keys)
+				{
+					var eventIdWithNameSpace = nameSpacedEvent.ToString();
+					var result = regex.Match(eventIdWithNameSpace);
 
 
+					if (!result.Success ||result.Groups.Count<3)
+					{
+						Debug.LogWarning("Failed to match regex for " + eventIdWithNameSpace);
+						continue;
+					}
+
+					var eventNameSpace = result.Groups[1].Value;
+					var eventId = result.Groups[2].Value;
+					var twitchEvent = eventmanager.GetEventByID(eventNameSpace,eventId);
+
+					if (Events.ContainsKey(eventId))
+					{
+						Debug.LogWarning("Event " + eventId + " already exists in local events");
+						continue;
+					}
+
+					if (twitchEvent != null)
+					{
+						if (GlobalCachedEvents.ContainsKey(eventId))
+						{
+							Debug.LogWarning("Event " + eventId + " already exists in global cached events");
+							continue;
+						}
+						Debug.Log("Caching event " + eventId);
+						GlobalCachedEvents[eventId] = twitchEvent;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.LogWarning("Error while caching global events: " + e);
+			}
+		}
+		private static IEnumerable<DictionaryEntry> CastDict(IDictionary dictionary)
+		{
+			foreach (DictionaryEntry entry in dictionary)
+			{
+				yield return entry;
+			}
+		}
 
 		static void RegisterAllEventsInNamespace(string nameSpace)
 		{
@@ -46,7 +146,7 @@ namespace Util_TwitchIntegrationLib
 				if (Instance != default)
 				{
 					RegisterEvent(Instance);
-					Debug.Log("["+nameSpace.Split('.').First() + "]: Registered event: "+Instance.EventName);
+					Debug.Log("[" + nameSpace.Split('.').First() + "]: Registered event: " + Instance.EventName);
 				}
 			}
 			Debug.Log(nameSpace.Split('.').First() + ": Added " + events.Count + " Twitch Events");
