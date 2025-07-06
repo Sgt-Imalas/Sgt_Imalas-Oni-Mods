c7c2798a5639d74019281cbdb49c0c991c5cbc0a
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-07-06 02:39:51 +0200

    yellowcake texture fix

--- RonivansLegacy_ChemicalProcessing/Content/ModDb/ModElements.cs
+++ RonivansLegacy_ChemicalProcessing/Content/ModDb/ModElements.cs
@@ -220,10 +220,10 @@ namespace RonivansLegacy_ChemicalProcessing.Content.ModDb
 			Material material = new Material(ElementLoader.FindElementByHash(SimHashes.Sulfur).substance.material)
 			{
 				name = "matYellowcake",
-				mainTexture = Assets.GetAnim("occuring_yellowcake_kanim").textureList[0]
+				mainTexture = Assets.GetAnim("new_yellowcake_kanim").textureList[0]
 			};
 			yellowcake_substance.material = material;
-			KAnimFile yellowcake_anim = Assets.GetAnim("solid_chemical_yellowcake_kanim");
+			KAnimFile yellowcake_anim = Assets.GetAnim("solid_yellowcake_kanim");
 			if (yellowcake_anim != null)
 			{
 				yellowcake_substance.anim = yellowcake_anim;
