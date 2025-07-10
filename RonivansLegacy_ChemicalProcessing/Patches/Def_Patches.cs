d0299438eaff9a7db37b336474bcd3efa776eee7
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-07-11 01:21:24 +0200

    Combustible Codex Icons

---
+++ RonivansLegacy_ChemicalProcessing/Patches/Def_Patches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	class Def_Patches
+	{
+
+		[HarmonyPatch(typeof(Def), nameof(Def.GetUISprite), [typeof(object), typeof(string), typeof(bool)])]
+		public class Def_GetUISprite_Patch
+		{
+			public static void Postfix(object item, Tuple<Sprite, Color> __result)
+			{
+				if(__result.first.name == "unknown" && item is Tag t)
+				{
+					if(t == GameTags.CombustibleGas)
+					{
+						__result.first = Assets.GetSprite("ui_combustible_gases");
+						__result.second = Color.white;
+					} 
+					else if (t == GameTags.CombustibleLiquid)
+					{
+						__result.first = Assets.GetSprite("ui_combustible_liquids");
+						__result.second = Color.white;
+					}
+				}
+			}
+		}
+	}
+}
