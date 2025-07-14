827af0d21e960866fd3c4cbab0cb1c9146e61a24
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-07-14 07:33:40 +0200

    more transpiler work

--- RonivansLegacy_ChemicalProcessing/Patches/HPA/ConduitFlowVisualizer_Patches.cs
+++ RonivansLegacy_ChemicalProcessing/Patches/HPA/ConduitFlowVisualizer_Patches.cs
@@ -4,6 +4,7 @@ using RonivansLegacy_ChemicalProcessing.Content.Scripts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
@@ -89,5 +90,50 @@ namespace RonivansLegacy_ChemicalProcessing.Patches.HPA
             }
 
         }
+
+		/// <summary>
+		/// This transpiler adjusts the blob filled state on high pressure pipes
+		/// its remade since the original target no longer exists
+		/// </summary>
+		[HarmonyPatch(typeof(ConduitFlowVisualizer.RenderMeshBatchJob), nameof(ConduitFlowVisualizer.RenderMeshBatchJob.RunItem))]
+		public class ConduitFlowVisualizer_RenderMeshBatchJob_TargetMethod_Patch
+		{
+			public static IEnumerable<CodeInstruction> Transpiler(ILGenerator _, IEnumerable<CodeInstruction> orig)
+			{
+				MethodInfo ConduitFlowVisualite_CalculateMassScale = AccessTools.Method(typeof(ConduitFlowVisualizer), nameof(ConduitFlowVisualizer.CalculateMassScale));
+				MethodInfo Conduit_GetCell = AccessTools.Method(typeof(ConduitFlow.Conduit), nameof(ConduitFlow.Conduit.GetCell));
+				MethodInfo normalizeConduitFillState = AccessTools.Method(typeof(ConduitFlowVisualizer_RenderMeshBatchJob_TargetMethod_Patch), nameof(NormalizeFillState));
+
+				var codes = orig.ToList();
+
+				int searchIndex = codes.FindIndex(ci => ci.Calls(Conduit_GetCell));
+
+				//either 17 or 7
+				int cellidx_index = TranspilerHelper.FindIndexOfNextLocalIndex(codes, searchIndex);
+				SgtLogger.l("RenderMeshBatchJob cellidx: " + cellidx_index);
+
+				///Inject a check if the pipe is a high pressure pipe.
+				///if yes, divide the actual pipe mass by the capacity multiplier, so it calculates the blob size based on the relative fill state
+				foreach (CodeInstruction original in orig)
+				{
+					if (original.Calls(ConduitFlowVisualite_CalculateMassScale))
+					{
+						yield return new CodeInstruction(OpCodes.Ldloc_S,cellidx_index); //result of conduit.GetCell(shared_data.outer.flowManager)
+						yield return new CodeInstruction(OpCodes.Call, normalizeConduitFillState); //normalizes the amount injected into CalculateMassScale
+						yield return original; //ConduitFlowVisualizer.CalculateMassScale
+					}
+					else
+						yield return original;
+				}
+			}
+
+			private static float NormalizeFillState(float absPipeMass, int cell)
+			{
+				//If IsHPA 
+				// return absPipeMass/= HPACapacity
+
+				return absPipeMass;
+			}
+		}
 	}
 }
