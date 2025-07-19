1d75f4eb6a319d3abd31e72933e90b6e1b71a0e8
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-07-19 20:15:07 +0200

    element injection start

---
+++ RonivansLegacy_ChemicalProcessing/Content/ModDb/BiomeElementInjections.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilLibs;
+
+namespace RonivansLegacy_ChemicalProcessing.Content.ModDb
+{
+	class BiomeElementInjections
+	{
+		public class ElementInjection
+		{
+			public SimHashes Element;
+			public float bandSize;
+		}
+		public class BiomeElementInjection
+		{
+			public string activeSubBiome;
+			Dictionary<string, List<ElementInjection>> SubBiomeInjections = [];
+			public static BiomeElementInjection Create(string biome)
+			{
+				Debug.Assert(BiomeInjections.ContainsKey(biome) == false, "BiomeElementInjection for biome already exists: " + biome);
+				var injection = new BiomeElementInjection();
+				BiomeInjections[biome] = injection;
+				return injection;
+			}
+			public BiomeElementInjection SubBiome(string subBiome)
+			{
+				if (!SubBiomeInjections.ContainsKey(subBiome))
+				{
+					SubBiomeInjections[subBiome] = new List<ElementInjection>();
+				}
+				activeSubBiome = subBiome;
+				return this;
+			}
+			public BiomeElementInjection Element(SimHashes element, float bandSize = 1f)
+			{
+				if (activeSubBiome == null)
+				{
+					throw new InvalidOperationException("No active sub-biome set for ElementInjection, please call SubBiome() first.");
+				}
+				if (!SubBiomeInjections.ContainsKey(activeSubBiome))
+				{
+					SgtLogger.l("Active sub-biome not found in SubBiomeInjections dictionary.");
+					return this;
+				}
+				SubBiomeInjections[activeSubBiome].Add(new ElementInjection { Element = element, bandSize = bandSize });
+				return this;
+			}
+		}
+
+		static Dictionary<string, BiomeElementInjection> BiomeInjections = new();
+		static BiomeElementInjections()
+		{
+			BiomeElementInjection.Create("Ocean")
+				.SubBiome("Basic")
+					.Element(ModElements.Chloroschist_Solid, 0.05f)
+				.SubBiome("Dry")
+					.Element(ModElements.Chloroschist_Solid, 0.05f)
+				.SubBiome("Briny")
+					.Element(ModElements.Chloroschist_Solid, 0.05f)
+				.SubBiome("Frozen")
+					.Element(ModElements.Chloroschist_Solid, 0.05f)
+					;
+
+
+		}
+
+		internal static void Inject(string biomePath, ProcGen.BiomeSettings biomeSettings)
+		{
+			string biome = Directory.GetParent(biomePath).Name;
+			string subBiome = Path.GetFileNameWithoutExtension(biomePath);
+			SgtLogger.l("Injecting Chemical Processing elements into biome: " + biome+" with sub-biome: "+ subBiome);
+		}	
+	}
+}
---
+++ RonivansLegacy_ChemicalProcessing/Content/Scripts/SettingsCache_Patches.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using Klei;
+using ProcGen;
+using RonivansLegacy_ChemicalProcessing.Content.ModDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonivansLegacy_ChemicalProcessing.Content.Scripts
+{
+	class SettingsCache_Patches
+	{
+
+		[HarmonyPatch(typeof(SettingsCache), nameof(SettingsCache.LoadBiome))]
+		public class SettingsCache_LoadBiome_Patch
+		{
+
+			[HarmonyPrepare]
+			public static bool Prepare() => Config.Instance.WorldgenElementInjection;
+
+			public static void Prefix(string longName, List<YamlIO.Error> errors, ref bool __state)
+			{
+				//getting biome path
+				SettingsCache.SplitNameFromPath(longName, out var path, out var name);
+				if (!SettingsCache.biomeSettingsCache.ContainsKey(path))
+				{
+					///biome is loaded for the first time, only inserting custom elements here
+					__state = true;
+				}
+			}
+			public static void Postfix(string longName, List<YamlIO.Error> errors, ref bool __state)
+			{
+				if (!__state)
+					return;
+
+				SettingsCache.SplitNameFromPath(longName, out var path, out var name);
+				//for whatever reason the biome failed to load, abort injections
+				if (!SettingsCache.biomeSettingsCache.ContainsKey(path))
+				{
+					return;
+				}
+
+				var biomeSettings = SettingsCache.biomeSettingsCache[path];
+
+				BiomeElementInjections.Inject(longName, biomeSettings);
+			}
+		}
+	}
+}
