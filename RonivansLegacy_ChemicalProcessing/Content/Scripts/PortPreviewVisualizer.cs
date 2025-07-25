64158ffa4627c72c9282506febdc148784160301
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-07-25 09:59:41 +0200

    material selector height adjustment for large number of ingredient variants

--- RonivansLegacy_ChemicalProcessing/Content/Scripts/PortPreviewVisualizer.cs
+++ RonivansLegacy_ChemicalProcessing/Content/Scripts/PortPreviewVisualizer.cs
@@ -14,30 +14,57 @@ namespace RonivansLegacy_ChemicalProcessing.Content.Scripts
 		[MyCmpReq]
 		PortDisplayController PortController;
 
-		//public void DrawUtilityIcon(int cell, Sprite icon_img, ref GameObject visualizerObj, Color tint, float scaleMultiplier = 1.5f, bool hideBG = false)
-		//{
-		//	Vector3 position = Grid.CellToPosCCC(cell, Grid.SceneLayer.Building);
-		//	if (visualizerObj == null)
-		//	{
-		//		visualizerObj = Util.KInstantiate(Assets.UIPrefabs.ResourceVisualizer, GameScreenManager.Instance.worldSpaceCanvas);
-		//		visualizerObj.transform.SetAsFirstSibling();
-		//		icons.Add(visualizerObj, visualizerObj.transform.GetChild(0).GetComponent<Image>());
-		//	}
+		Dictionary<int, ToolTip> PortPreviews = [];
+		HashSet<int> PreviewCells = [];
 
-		//	if (!visualizerObj.gameObject.activeInHierarchy)
-		//	{
-		//		visualizerObj.gameObject.SetActive(value: true);
-		//	}
+		public override void OnSpawn()
+		{
+			base.OnSpawn();
+		}
+		public override void OnCleanUp()
+		{
+			base.OnCleanUp();
+		}
+		void CleanupPorts()
+		{
+			for(int i = PortPreviews.Count()-1; i >= 0; i--)
+			{
+				if (PortPreviews[i] != null)
+				{
+					Util.KDestroyGameObject(PortPreviews[i]);
+				}
+			}			
+		}
+		void SpawnPortPreviews()
+		{
+			//fore
+		}
 
-		//	visualizerObj.GetComponent<Image>().enabled = !hideBG;
-		//	icons[visualizerObj].raycastTarget = enableRaycast;
-		//	icons[visualizerObj].sprite = icon_img;
-		//	visualizerObj.transform.GetChild(0).gameObject.GetComponent<Image>().color = tint;
-		//	visualizerObj.transform.SetPosition(position);
-		//	if (visualizerObj.GetComponent<SizePulse>() == null)
-		//	{
-		//		visualizerObj.transform.localScale = Vector3.one * scaleMultiplier;
-		//	}
-		//}
+
+		public void DrawUtilityIcon(int cell, Sprite icon_img, ref GameObject visualizerObj, Color tint, float scaleMultiplier = 1.5f, bool hideBG = false)
+		{
+			Vector3 position = Grid.CellToPosCCC(cell, Grid.SceneLayer.Building);
+			if (visualizerObj == null)
+			{
+				visualizerObj = Util.KInstantiate(Assets.UIPrefabs.ResourceVisualizer, GameScreenManager.Instance.worldSpaceCanvas);
+				visualizerObj.transform.SetAsFirstSibling();
+				//icons.Add(visualizerObj, visualizerObj.transform.GetChild(0).GetComponent<Image>());
+			}
+
+			if (!visualizerObj.gameObject.activeInHierarchy)
+			{
+				visualizerObj.gameObject.SetActive(value: true);
+			}
+
+			visualizerObj.GetComponent<Image>().enabled = !hideBG;
+			//icons[visualizerObj].raycastTarget = enableRaycast;
+			//icons[visualizerObj].sprite = icon_img;
+			visualizerObj.transform.GetChild(0).gameObject.GetComponent<Image>().color = tint;
+			visualizerObj.transform.SetPosition(position);
+			if (visualizerObj.GetComponent<SizePulse>() == null)
+			{
+				visualizerObj.transform.localScale = Vector3.one * scaleMultiplier;
+			}
+		}
 	}
 }
---
+++ RonivansLegacy_ChemicalProcessing/Patches/MaterialSelector_Patches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UtilLibs;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+    class MaterialSelector_Patches
+	{
+		[HarmonyPatch(typeof(MaterialSelector), nameof(MaterialSelector.UpdateHeader))]
+		public class MaterialSelector_UpdateHeader_Patch
+		{
+			public static void Postfix(MaterialSelector __instance)
+			{
+				LocText headerText = __instance.Headerbar.GetComponentInChildren<LocText>();
+				string stripped = global::STRINGS.UI.StripLinkFormatting(headerText.text);
+				float count = stripped.Length;
+				int linecount = Mathf.CeilToInt(count / 40f);
+				int height = linecount * 24;
+				__instance.Headerbar.GetComponent<LayoutElement>().minHeight = height;
+			}
+		}
+	}
+}
