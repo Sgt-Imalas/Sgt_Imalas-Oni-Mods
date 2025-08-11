7d310cc2d964ca3edf0e4d9478344590b2416b3f
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-08-11 18:12:00 +0200

    shut transfer arm noises

---
+++ RonivansLegacy_ChemicalProcessing/Patches/SolidTransferArm_Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	internal class SolidTransferArm_Patches
+	{
+
+        [HarmonyPatch(typeof(SolidTransferArm), nameof(SolidTransferArm.Sim1000ms))]
+        public class SolidTransferArm_Sim1000ms_Patch
+        {
+            public static void Postfix(SolidTransferArm __instance)
+            {
+                if (__instance.rotation_complete && __instance.rotateSoundPlaying)
+                    __instance.StopRotateSound();
+            }
+        }
+	}
+}
