a5526a4d0f25f65b72cb34c620e6b6373c277ffb
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-08-18 11:55:15 +0200

    rotate co2 pump

--- RonivansLegacy_ChemicalProcessing/Content/Defs/Buildings/ChemicalProcessing_IO/Chemical_Co2PumpConfig.cs
+++ RonivansLegacy_ChemicalProcessing/Content/Defs/Buildings/ChemicalProcessing_IO/Chemical_Co2PumpConfig.cs
@@ -30,6 +30,7 @@ namespace Dupes_Industrial_Overhaul.Chemical_Processing.Buildings
 			def1.UtilityOutputOffset = new CellOffset(0, 0);
 			def1.PowerInputOffset = new CellOffset(0, 0);
 			def1.LogicInputPorts = LogicOperationalController.CreateSingleInputPortList(new CellOffset(0, 0));
+			def1.PermittedRotations = PermittedRotations.R360;
 			GeneratedBuildings.RegisterWithOverlay(OverlayScreen.GasVentIDs, ID);
 			SoundUtils.CopySoundsToAnim("co2_pump_kanim", "minigaspump_kanim");
 			return def1;
