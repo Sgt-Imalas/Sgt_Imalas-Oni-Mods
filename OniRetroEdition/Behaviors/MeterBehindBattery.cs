0cc13f803d72232aebaf4d0f0f5fb2f2f5b82ea8
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-09-02 23:10:21 +0200

    OniRetro: moulding tile precious rock, large battery meter behind

---
+++ OniRetroEdition/Behaviors/MeterBehindBattery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OniRetroEdition.Behaviors
+{
+	internal class MeterBehindBattery : Battery
+	{
+		public override void OnSpawn()
+		{
+			Components.Batteries.Add(this);
+			Building component = GetComponent<Building>();
+			PowerCell = component.GetPowerInputCell();
+			Subscribe(-1582839653, OnTagsChangedDelegate);
+			OnTagsChanged(null);
+			bool flag = GetComponent<PowerTransformer>();
+			meter = (flag ? null : new MeterController(GetComponent<KBatchedAnimController>(), "meter_target", "meter", Meter.Offset.Behind, Grid.SceneLayer.NoLayer, "meter_target", "meter_fill", "meter_frame", "meter_OL"));
+			Game.Instance.circuitManager.Connect(this);
+			Game.Instance.energySim.AddBattery(this);
+		}
+	}
+}
---
+++ OniRetroEdition/ModPatches/MouldingTile_Patches.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUNING;
+using UnityEngine;
+
+namespace OniRetroEdition.ModPatches
+{
+	class MouldingTile_Patches
+	{
+
+		[HarmonyPatch(typeof(MouldingTileConfig), nameof(MouldingTileConfig.DoPostConfigureComplete))]
+		public class MouldingTileConfig_DoPostConfigureComplete_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				KPrefabID component = go.GetComponent<KPrefabID>();
+				component.AddTag(GameTags.FloorTiles);
+			}
+		}
+
+		[HarmonyPatch(typeof(MouldingTileConfig), nameof(MouldingTileConfig.CreateBuildingDef))]
+		public class MouldingTileConfig_CreateBuildingDef_Patch
+		{
+			public static void Postfix(BuildingDef __result)
+			{
+				//__result.ShowInBuildMenu = true;
+				//__result.Deprecated = false;
+				//__result.BaseDecor = 15;
+				//__result.BaseDecorRadius = 3;
+				__result.MaterialCategory = MATERIALS.PRECIOUS_ROCKS;
+			}
+		}
+
+		[HarmonyPatch(typeof(MouldingTileConfig), nameof(MouldingTileConfig.ConfigureBuildingTemplate))]
+		public class MouldingTileConfig_ConfigureBuildingTemplate_Patch
+		{
+			public static void Postfix(GameObject go)
+			{
+				var sco = go.AddOrGet<SimCellOccupier>();
+				sco.movementSpeedMultiplier = 1.5f; //== DUPLICANTSTATS.MOVEMENT_MODIFIERS.BONUS_3;
+				sco.notifyOnMelt = true;
+			}
+		}
+	}
+}
