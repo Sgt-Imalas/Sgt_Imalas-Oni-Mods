0e3587c5d45ffdb9f40bb20e85fd990aff54de9c
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-09-16 19:02:43 +0200

    Rename project to PresetManager for easier access

---
+++ DupePrioPresetManager/ResearchQueueUI_Patches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DupePrioPresetManager
+{
+	//internal class ResearchQueueUI_Patches
+	//{
+
+ //       [HarmonyPatch(typeof(ResearchScreenSideBar), nameof(ResearchScreenSideBar.OnSpawn))]
+ //       public class ResearchScreenSideBar_OnSpawn_Patch
+ //       {
+ //           public static void Postfix(ResearchScreenSideBar __instance)
+ //           {
+ //               __instance.transform.Find("Content/Queue").gameObject.SetActive(true);
+ //               __instance.transform.Find("Content/Queue/Scroll/Rect/QueueContent/TechWidgetPrefab").gameObject.SetActive(true);
+ //               __instance.transform.Find("Content/Queue/Scroll/Rect/QueueContent/TechWidgetPrefabAlt").gameObject.SetActive(true);
+ //               __instance.transform.Find("Content/Queue/Scroll/Rect/QueueContent/TechItemWidgetPrefab").gameObject.SetActive(true);
+	//		}
+ //       }
+	//}
+}
