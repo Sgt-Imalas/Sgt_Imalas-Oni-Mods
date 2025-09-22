4af215c455ade31c6e555667f0b738237dcc464a
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-09-22 17:02:58 +0200

    RonivanAIO: fix zh codex crash, customizebuildings steam turbine integration, version bump buildall

--- RonivansLegacy_ChemicalProcessing/Content/ModDb/ModIntegrations/CustomizeBuildings.cs
+++ RonivansLegacy_ChemicalProcessing/Content/ModDb/ModIntegrations/CustomizeBuildings.cs
@@ -24,7 +24,7 @@ namespace RonivansLegacy_ChemicalProcessing.Content.ModDb.ModIntegrations
 			liquidCapacity = ConduitFlow.MAX_LIQUID_MASS;
 			solidCapacity = SolidConduitFlow.MAX_SOLID_MASS;
 			InitTypes();
-			if(ConfigInstance == null)
+			if (ConfigInstance == null)
 				return false;
 
 
@@ -55,6 +55,69 @@ namespace RonivansLegacy_ChemicalProcessing.Content.ModDb.ModIntegrations
 			return true;
 		}
 
+		public static bool TryGetOtherTurbineValues(out float pumpRate, out float heatTransferPercent, out float minActiveTemp, out float idealTemp, out float outputTemp, out float overheatTemp)
+		{
+			pumpRate = 2f;
+			heatTransferPercent = 0.1f;
+			minActiveTemp = 398.15f;
+			idealTemp = 473.15f;
+			outputTemp = 368.15f;
+			overheatTemp = 373.15f;
+
+			InitTypes();
+			if (ConfigInstance == null)
+				return false;
+
+
+			var pumpRate_prop = Traverse.Create(ConfigInstance).Property("SteamTurbinePumpRateKG");
+			if(pumpRate_prop.PropertyExists()) 
+				pumpRate = (float)pumpRate_prop.GetValue();
+
+			var heatTransferPercent_prop = Traverse.Create(ConfigInstance).Property("SteamTurbineHeatTransferPercent");
+			if (heatTransferPercent_prop.PropertyExists())
+				heatTransferPercent = (float)heatTransferPercent_prop.GetValue();
+
+			var minActiveTemp_prop = Traverse.Create(ConfigInstance).Property("SteamTurbineMinActiveTemperature");
+			if (minActiveTemp_prop.PropertyExists())
+				minActiveTemp = (float)minActiveTemp_prop.GetValue();
+
+			var idealTemp_prop = Traverse.Create(ConfigInstance).Property("SteamTurbineIdealTemperature");
+			if (idealTemp_prop.PropertyExists())
+				idealTemp = (float)idealTemp_prop.GetValue();
+
+			var outputTemp_prop = Traverse.Create(ConfigInstance).Property("SteamTurbineOutputTemperature");
+			if (outputTemp_prop.PropertyExists())
+				outputTemp = (float)outputTemp_prop.GetValue();
+
+			var overheatTemp_prop = Traverse.Create(ConfigInstance).Property("SteamTurbineOverheatTemperature");
+			if (overheatTemp_prop.PropertyExists())
+				overheatTemp = (float)overheatTemp_prop.GetValue();
+
+			SgtLogger.l("CustomizeBuildings Steam Turbine Integration:");
+			SgtLogger.l("SteamTurbinePumpRateKG: "+pumpRate);
+			SgtLogger.l("SteamTurbineHeatTransferPercent: " + heatTransferPercent);
+			SgtLogger.l("SteamTurbineMinActiveTemperature: " + minActiveTemp);
+			SgtLogger.l("SteamTurbineIdealTemperature: " + idealTemp);
+			SgtLogger.l("SteamTurbineOutputTemperature: " + outputTemp);
+			SgtLogger.l("SteamTurbineOverheatTemperature: " + overheatTemp);
+
+			return true;
+		}
+
+		public static float GetTurbineBaseValue()
+		{
+			float steamTurbineBaseValue = SteamTurbineConfig2.MAX_WATTAGE;
+
+			InitTypes();
+			if (ConfigInstance != null)
+			{
+				var _steamTurbineWattage = Traverse.Create(ConfigInstance).Property("SteamTurbineWattage");
+				if (_steamTurbineWattage.PropertyExists())
+					steamTurbineBaseValue = (float)_steamTurbineWattage.GetValue();
+				SgtLogger.l("Custom Steam Turbine wattage from customizebuildings: "+steamTurbineBaseValue);
+			}
+			return steamTurbineBaseValue;
+		}
 
 		static void InitTypes()
 		{
@@ -79,9 +142,9 @@ namespace RonivansLegacy_ChemicalProcessing.Content.ModDb.ModIntegrations
 			catch (Exception e)
 			{
 				SgtLogger.error("Failure to get state manager from CustomizeBuildings_CustomizeBuildingsState:\n" + e.Message);
-				return; 
+				return;
 			}
-			if( m_StateManager == null )
+			if (m_StateManager == null)
 			{
 				SgtLogger.error("Failure to get state manager from CustomizeBuildings_CustomizeBuildingsState");
 				return;
