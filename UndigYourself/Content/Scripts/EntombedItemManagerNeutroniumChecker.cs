721fcff3474dfcb9c7f29ff31b4b1c169a033f1a
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-09-23 18:20:47 +0200

    UY: undig cached debris items on load

---
+++ UndigYourself/Content/Scripts/EntombedItemManagerNeutroniumChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilLibs;
+
+namespace UndigYourself.Content.Scripts
+{
+	internal class EntombedItemManagerNeutroniumChecker : KMonoBehaviour
+	{
+		[MyCmpGet] EntombedItemManager eim;
+
+		public override void OnSpawn()
+		{
+			base.OnSpawn();
+			UnburyAllNeutroniumStuckChunks();
+		}
+		void UnburyAllNeutroniumStuckChunks()
+		{
+			var cells = eim.cells;
+			List<int> neutroniumBuridItemIndices = [];
+			for (int i = 0; i < cells.Count; i++)
+			{
+				int cell = cells[i];
+				int worldIdx = Grid.WorldIdx[cell];
+
+
+				if (NeutroniumMole.InvalidCell(cell, worldIdx))
+				{
+					neutroniumBuridItemIndices.Add(i);
+				}
+			}
+			SgtLogger.l("EntombedItemManager.OnSpawn, total buried item count: " + cells.Count + ", neutronium items found: " + neutroniumBuridItemIndices.Count);
+			if (!neutroniumBuridItemIndices.Any())
+				return;
+
+			neutroniumBuridItemIndices.Sort();
+			neutroniumBuridItemIndices.Reverse();
+			EntombedItemVisualizer eiv = Game.Instance.GetComponent<EntombedItemVisualizer>();
+
+			foreach (var buriedItemIndex in neutroniumBuridItemIndices)
+			{
+				EntombedItemManager.Item obj = eim.GetItem(buriedItemIndex);
+
+				int cell = obj.cell;
+				int worldIdx = Grid.WorldIdx[cell];
+				int newCell = NeutroniumMole.FindValidNewCell(cell, worldIdx);
+				if(newCell != cell)
+				{
+					eiv.RemoveItem(obj.cell);
+					eim.RemoveItem(buriedItemIndex); 
+					ElementLoader.FindElementByHash((SimHashes)obj.elementId)?.substance.SpawnResource(Grid.CellToPosCCC(newCell, Grid.SceneLayer.Ore), obj.mass, obj.temperature, obj.diseaseIdx, obj.diseaseCount);
+				}
+			}
+		}
+	}
+}
---
+++ UndigYourself/Patches/EntombedItemManager_Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UndigYourself.Content.Scripts;
+
+namespace UndigYourself.Patches
+{
+	internal class EntombedItemManager_Patches
+	{
+
+        [HarmonyPatch(typeof(EntombedItemManager), nameof(EntombedItemManager.OnDeserialized))]
+        public class EntombedItemManager_OnDeserialized_Patch
+        {
+            public static void Postfix(EntombedItemManager __instance)
+            {
+                __instance.gameObject.AddOrGet<EntombedItemManagerNeutroniumChecker>();
+            }
+        }
+	}
+}
