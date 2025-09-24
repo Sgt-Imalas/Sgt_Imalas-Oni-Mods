054e766941270e02ee54691b4ef50e28258cb5a6
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-09-24 02:00:52 +0200

    prevent drop items from merging

--- ItemDropPrevention/Content/Scripts/DroppablesHolder.cs
+++ ItemDropPrevention/Content/Scripts/DroppablesHolder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HarmonyLib;
+using Klei.AI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +10,13 @@ using UtilLibs;
 
 namespace ItemDropPrevention.Content.Scripts
 {
+
 	internal class DroppablesHolder : KMonoBehaviour
 	{
 		[MyCmpReq] Storage internalStorage;
 		[MyCmpReq] KPrefabID kprefabID;
 		[MyCmpReq] ChoreDriver choreDriver;
+		[MyCmpReq] StandardWorker worker;
 
 		HashSet<int> MarkedForDrop = [];
 
@@ -42,18 +46,27 @@ namespace ItemDropPrevention.Content.Scripts
 			var items = internalStorage.items;
 
 			///if deliverable chunks of the current chore have merged into those marked for drop, prevent them from dropping
-			var currentChore = choreDriver.GetCurrentChore();
+			Chore currentChore = choreDriver.GetCurrentChore();
 			if (currentChore != null && currentChore is FetchAreaChore fac)
 			{
 				var fetchInstance = fac.smi;
 				var deliverables = fetchInstance.deliverables;
-				foreach(var item in deliverables)
+				foreach (var item in deliverables)
 				{
 					var id = item.gameObject?.GetInstanceID();
-					if(id.HasValue)
+					if (id.HasValue)
 						MarkedForDrop.Remove(id.Value);
 				}
 			}
+			///if the workable is something in the hands of the dupe, dont drop it
+			var task = worker.GetWorkable();
+			if (task != null)
+			{
+				var id = task.gameObject?.GetInstanceID();
+				if (id.HasValue)
+					MarkedForDrop.Remove(id.Value);
+			}
+
 
 			for (int i = items.Count - 1; i >= 0; --i)
 			{
@@ -64,13 +77,16 @@ namespace ItemDropPrevention.Content.Scripts
 				int instanceID = item.GetInstanceID();
 				if (MarkedForDrop.Contains(instanceID))
 				{
+					if (item.TryGetComponent<Pickupable>(out var pickupable))
+						pickupable.prevent_absorb_until_stored = false;
 					internalStorage.Drop(item);
-					if(item.TryGetComponent<Clearable>(out var markForSweep) && markForSweep.isClearable)
-					{
-						markForSweep.MarkForClear();
-						if (item.TryGetComponent<Prioritizable>(out var prioritizable))
-							prioritizable.SetMasterPriority(new(PriorityScreen.PriorityClass.basic,5));
-					}
+
+					//if (item.TryGetComponent<Clearable>(out var markForSweep) && markForSweep.isClearable)
+					//{
+					//	markForSweep.MarkForClear();
+					//	if (item.TryGetComponent<Prioritizable>(out var prioritizable))
+					//		prioritizable.SetMasterPriority(new(PriorityScreen.PriorityClass.basic, 5));
+					//}
 				}
 			}
 			MarkedForDrop.Clear();
@@ -78,11 +94,11 @@ namespace ItemDropPrevention.Content.Scripts
 		bool AboveSolidGround()
 		{
 			var cell = Grid.PosToCell(this);
-			if(!Grid.IsValidCell(cell))  //should never happen, but check anyway
+			if (!Grid.IsValidCell(cell))  //should never happen, but check anyway
 				return false;
 
 			var floorCell = Grid.CellBelow(cell);
-			if(!Grid.IsValidCell(floorCell))
+			if (!Grid.IsValidCell(floorCell))
 				return false;
 
 			bool isSolidGround = Grid.Solid[floorCell];
@@ -96,6 +112,8 @@ namespace ItemDropPrevention.Content.Scripts
 			{
 				//SgtLogger.l("Marking " + item + " as drop");
 				MarkedForDrop.Add(item.GetInstanceID());
+				if(item.TryGetComponent<Pickupable>(out var pickupable))
+					pickupable.prevent_absorb_until_stored = true;
 			}
 		}
 	}
