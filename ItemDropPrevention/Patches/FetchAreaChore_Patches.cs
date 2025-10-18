62f0da71d22731ee7979d272db761eede08c4913
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-10-18 13:53:57 +0200

    ItemDropPrevention: allow fetching marked for drop items

---
+++ ItemDropPrevention/Patches/FetchAreaChore_Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using ItemDropPrevention.Content.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ItemDropPrevention.Patches
+{
+	internal class FetchAreaChore_Patches
+	{
+
+        [HarmonyPatch(typeof(FetchAreaChore.StatesInstance), nameof(FetchAreaChore.StatesInstance.SetupDeliverables))]
+        public class FetchAreaChore_StatesInstance_SetupDeliverables_Patch
+		{
+            public static void Postfix(FetchAreaChore.StatesInstance __instance)
+            {
+                if(!DroppablesHolder.TryGet(__instance.sm.fetcher.Get(__instance),out var droppablesHolder))
+                    return;
+
+                __instance.fetchables.RemoveAll(item => droppablesHolder.IsItemMarkedForDrop(item.gameObject));
+            }
+        }
+	}
+}
