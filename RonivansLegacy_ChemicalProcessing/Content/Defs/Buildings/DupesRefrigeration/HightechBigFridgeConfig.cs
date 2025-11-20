1762e316e5229701f19c0d30bd856b7b801544a6
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-11-20 14:54:31 +0100

    RonivanAIO: insulate tech fridge storage

--- RonivansLegacy_ChemicalProcessing/Content/Defs/Buildings/DupesRefrigeration/HightechBigFridgeConfig.cs
+++ RonivansLegacy_ChemicalProcessing/Content/Defs/Buildings/DupesRefrigeration/HightechBigFridgeConfig.cs
@@ -109,7 +109,7 @@ namespace RonivansLegacy_ChemicalProcessing.Content.Defs.Buildings.DupesRefriger
 
 			///water storage
 			Storage liquidStorage = BuildingTemplates.CreateDefaultStorage(go, true);
-			liquidStorage.SetDefaultStoredItemModifiers(Storage.StandardSealedStorage);
+			liquidStorage.SetDefaultStoredItemModifiers(Storage.StandardInsulatedStorage);
 			//liquidStorage.showDescriptor = true;
 			liquidStorage.capacityKg = 500f;
 			liquidStorage.allowItemRemoval = true;
