927a6ebf42cb6858164f08901f9b0ea083d699a3
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2025-11-28 23:27:07 +0100

    CritterTraits: fix injection target for trait generation

--- CritterTraitsReborn/Source/Patches/EntityTemplates.cs
+++ CritterTraitsReborn/Source/Patches/EntityTemplates.cs
@@ -1,27 +1,35 @@
 ﻿using HarmonyLib;
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using static FactionManager;
 
 namespace CritterTraitsReborn.Patches
 {
-	[HarmonyPatch(typeof(EntityTemplates), nameof(EntityTemplates.ExtendEntityToBasicCreature), [
-	  typeof(bool),
-	  typeof(GameObject),
-	  typeof(FactionManager.FactionID),
-	  typeof(string) ,
-	  typeof(string) ,
-	  typeof(NavType) ,
-	  typeof(int) ,
-	  typeof(float ),
-	  typeof(string) ,
-	  typeof(int),
-	  typeof(bool ) ,
-	  typeof(bool) ,
-	  typeof(float ),
-	  typeof(float ),
-	  typeof(float ),
-	  typeof(float )
-])]
+	[HarmonyPatch(typeof(EntityTemplates), nameof(EntityTemplates.ExtendEntityToBasicCreature),
+		[
+		typeof(bool),
+		typeof(GameObject),
+		typeof(string),
+		typeof(string),
+		typeof(string),
+		typeof(FactionManager.FactionID),
+		typeof(string),
+		typeof(string),
+		typeof(NavType),
+		typeof(int),
+		typeof(float),
+		typeof(string),
+		typeof(float),
+		typeof(bool),
+		typeof(bool),
+		typeof(float),
+		typeof(float),
+		typeof(float),
+		typeof(float)
+		])]
+
+
 	class EntityTemplates_ExtendEntityToBasicCreature
 	{
 		static void Postfix(ref GameObject __result)
