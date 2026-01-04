bd1da694a5ab0b2161d7c168ef35a937a74ed839
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-04 22:36:40 +0100

    RE: improve cargobay logic handler

--- Rockets-TinyYetBig/Content/Scripts/Buildings/RocketModules/CargoBayStatusMonitor.cs
+++ Rockets-TinyYetBig/Content/Scripts/Buildings/RocketModules/CargoBayStatusMonitor.cs
@@ -19,6 +19,8 @@ namespace Rockets_TinyYetBig.Buildings.CargoBays
 		public CargoBayCluster cargoBay;
 		[MyCmpGet]
 		private LogicPorts ports;
+		[MyCmpGet]
+		public RocketModuleCluster rocketModule;
 
 		[MyCmpGet]
 		private Operational operational;
@@ -28,6 +30,9 @@ namespace Rockets_TinyYetBig.Buildings.CargoBays
 		private void UpdateLogicCircuitCB(object data) => this.UpdateLogicAndActiveState();
 		private void UpdateLogicAndActiveState()
 		{
+			if (rocketModule.CraftInterface.m_clustercraft.Status != Clustercraft.CraftStatus.Grounded)
+				return;
+
 			if (cargoBay?.storage == null)
 			{
 				SgtLogger.warning("cargo bay is null");
@@ -36,45 +41,27 @@ namespace Rockets_TinyYetBig.Buildings.CargoBays
 			float massStored = cargoBay.AmountStored;
 			float remainingCapacity = cargoBay.RemainingCapacity;
 			//SgtLogger.l("RemainingCapacity: " + remainingCapacity + ", massStored: " + massStored);
-			ports.SendSignal(EMPTY_PORT_ID, massStored < 0 || Mathf.Approximately(massStored, 0) ? 1 : 0);
-			ports.SendSignal(FULL_PORT_ID, Mathf.Approximately(remainingCapacity, 0) ? 1 : 0);
+			ports.SendSignal(EMPTY_PORT_ID, massStored <= 0 || Mathf.Approximately(massStored, 0) ? 1 : 0);
+			ports.SendSignal(FULL_PORT_ID, remainingCapacity <= 0 ||Mathf.Approximately(remainingCapacity, 0) ? 1 : 0);
 		}
 		private void UpdatePortsOnMoveCB(object data) => this.UpdateLogicAndActiveState();
-		//public void UpdateLogicAndActiveStateDelayed()
-		//{
-		//	SgtLogger.l("OnPortMove");
-		//	//ports.OnMove();
-		//	UpdateLogicAndActiveState();
-		//	StartCoroutine(DelayedUpdate());
-		//}
-		//private void UpdatePortsOnMoveDelayedCB(object data) => this.UpdateLogicAndActiveStateDelayed();
-		//IEnumerator DelayedUpdate()
-		//{
-		//	yield return null;
-		//	yield return null;
-		//	//ports.OnMove();
-		//	SgtLogger.l("OnPortMoveDelayed");
-		//	ports.OnMove();
-		//	UpdateLogicAndActiveState();
-		//}
 
+		List<int> handlers = [];
 		public override void OnSpawn()
 		{
 			base.OnSpawn();
-			Subscribe((int)GameHashes.OnStorageChange, UpdateLogicCircuitCBDelegate);
-			Subscribe((int)GameHashes.OperationalChanged, UpdateLogicCircuitCBDelegate);
-			Subscribe((int)GameHashes.StorageCapacityChanged, UpdateLogicCircuitCBDelegate);
-			Subscribe((int)GameHashes.ReorderableBuildingChanged, UpdateLogicCircuitCBDelegate);
+			handlers.Add(Subscribe((int)GameHashes.OnStorageChange, UpdateLogicCircuitCBDelegate));
+			handlers.Add(Subscribe((int)GameHashes.OperationalChanged, UpdateLogicCircuitCBDelegate));
+			handlers.Add(Subscribe((int)GameHashes.StorageCapacityChanged, UpdateLogicCircuitCBDelegate));
+			handlers.Add(Subscribe((int)GameHashes.ReorderableBuildingChanged, UpdateLogicCircuitCBDelegate));
+			handlers.Add(Subscribe((int)GameHashes.ClustercraftStateChanged, UpdateLogicCircuitCBDelegate));
 			UpdateLogicAndActiveState();
 
 		}
 		public override void OnCleanUp()
 		{
-			Unsubscribe((int)GameHashes.OnStorageChange, UpdateLogicCircuitCBDelegate);
-			Unsubscribe((int)GameHashes.OperationalChanged, UpdateLogicCircuitCBDelegate);
-			Unsubscribe((int)GameHashes.StorageCapacityChanged, UpdateLogicCircuitCBDelegate);
-			Unsubscribe((int)GameHashes.ReorderableBuildingChanged, UpdateLogicCircuitCBDelegate);
-
+			foreach(var ha in handlers)
+				Unsubscribe(ha);
 			base.OnCleanUp();
 		}
 	}
