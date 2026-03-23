2f4e6a50962360ceb17fe5b7fc430d8aa5e784fe
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-03-23 10:27:58 +0100

    RonivanAIO: add vanilla biodiesel as alt recipe for biodiesel generator, at vanilla petrol gen consumption rate

---
+++ RonivansLegacy_ChemicalProcessing/Content/Scripts/BiodieselEnergyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using RonivansLegacy_ChemicalProcessing.Content.ModDb;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace RonivansLegacy_ChemicalProcessing.Content.Scripts
+{
+	internal class BiodieselEnergyGenerator : EnergyGenerator
+	{
+		[SerializeField]
+		public Formula vanillaDieselFormula;
+		[SerializeField]
+		public Formula modDieselFormula;
+
+		public override void EnergySim200ms(float dt)
+		{
+			SetCurrentFormula();
+			base.EnergySim200ms(dt);
+		}
+		void SetCurrentFormula()
+		{
+			float modDieselAmount = storage.GetAmountAvailable(ModElements.BioDiesel_Liquid.Tag);
+			float vanillaDieselAmount = storage.GetAmountAvailable(SimHashes.RefinedLipid.CreateTag());
+			bool useModDiesel = modDieselAmount >= vanillaDieselAmount;
+			formula = useModDiesel ? modDieselFormula : vanillaDieselFormula;
+		}
+	}
+}
