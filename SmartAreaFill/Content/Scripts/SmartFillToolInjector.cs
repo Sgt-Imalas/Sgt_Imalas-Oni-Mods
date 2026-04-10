165e8b707a3041df5b5cfd676d8327b920458a6c
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-04-10 11:32:57 +0200

    FloodFill: change to incremental coroutine

--- SmartAreaFill/Content/Scripts/SmartFillToolInjector.cs
+++ SmartAreaFill/Content/Scripts/SmartFillToolInjector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,10 @@ namespace SmartAreaFill.Content.Scripts
 		List<GameObject> ActiveVis = new List<GameObject>();
 		HashSet<int> CachedClaimedCells = new();
 		List<int> CachedUnclaimedCells = new();
+
+		Queue<int> WalkableCells = new();
+		HashSet<int> VisitedCells = new();
+		byte StartCellWorldIdx;
 
 		enum ExpansionRules
 		{
@@ -47,6 +52,8 @@ namespace SmartAreaFill.Content.Scripts
 		HashSet<ObjectLayer> cachedLayers = [];
 		BuildingDef cachedDef;
 
+		Coroutine HoldDownCoroutine = null;
+
 		public void OnLeftClickDown()
 		{
 			startCell = Grid.PosToCell(PlayerController.Instance.GetCursorPos());
@@ -57,15 +64,28 @@ namespace SmartAreaFill.Content.Scripts
 			}
 			mouseHeldDown = true;
 			timeSinceMouseDown = 0;
-			CacheCells();
+			//CacheCells();
+			PreCacheWalk();
+			HoldDownCoroutine = StartCoroutine(FloodFillCoroutine());
 		}
 		public void OnLeftClickUp()
 		{
 			mouseHeldDown = false;
+			StopFloodfillCoroutine();
 			ConsumeCachedCells();
 		}
+		void StopFloodfillCoroutine()
+		{
+			if (HoldDownCoroutine != null)
+			{
+				StopCoroutine(HoldDownCoroutine);
+				HoldDownCoroutine = null;
+			}
+		}
+
 		public void OnDeactivateTool()
 		{
+			StopFloodfillCoroutine();
 			RecycleVisualizers();
 			CachedUnclaimedCells.Clear();
 			CachedClaimedCells.Clear();
@@ -132,24 +152,59 @@ namespace SmartAreaFill.Content.Scripts
 			ActiveVis.Add(vis);
 		}
 
-		void Update()
+		//void Update()
+		//{
+		//	if (PlayerController.Instance.activeTool != dragTool)
+		//		return;
+
+		//	if (!mouseHeldDown)
+		//		return;
+
+		//	timeSinceMouseDown += Time.unscaledDeltaTime;
+		//	if (timeSinceMouseDown < ExpansionDelay)
+		//		return;
+
+		//	if (CellChanged())
+		//	{
+		//		OnDeactivateTool();
+		//		return;
+		//	}
+		//	CacheNextCells();
+		//	CacheNextCells();
+		//	CacheNextCells();
+		//	CacheNextCells();
+		//	PickNextCellForVisualization();
+		//}
+
+		IEnumerator FloodFillCoroutine()
 		{
-			if (PlayerController.Instance.activeTool != dragTool)
-				return;
-
-			if (!mouseHeldDown)
-				return;
-
-			timeSinceMouseDown += Time.unscaledDeltaTime;
-			if (timeSinceMouseDown < ExpansionDelay)
-				return;
-
-			if (CellChanged())
+			if (PlayerController.Instance.activeTool != dragTool || !mouseHeldDown)
 			{
-				OnDeactivateTool();
-				return;
+				SgtLogger.l("Canceling coroutine: tool not active");
+				yield break;
 			}
-			PickNextCellForVisualization();
+
+
+			while (timeSinceMouseDown < ExpansionDelay)
+			{
+				timeSinceMouseDown += Time.unscaledDeltaTime;
+				yield return null;
+			}
+
+			do
+			{
+				if (CellChanged())
+				{
+					SgtLogger.l("Canceling coroutine: cell changed");
+					OnDeactivateTool();
+					yield break;
+				}
+				while(WalkableCells.Any() && !CachedUnclaimedCells.Any())
+					CacheNextCells();
+				PickNextCellForVisualization();
+				yield return null;
+			}
+			while (WalkableCells.Any() || CachedUnclaimedCells.Any());
 		}
 
 		bool CellChanged()
@@ -207,7 +262,7 @@ namespace SmartAreaFill.Content.Scripts
 			}
 		}
 
-		void CacheCells()
+		void PreCacheAllCells()
 		{
 			CachedUnclaimedCells.Clear();
 			var visited = new HashSet<int>();
@@ -240,6 +295,38 @@ namespace SmartAreaFill.Content.Scripts
 				CacheCells(toVisit, ref world, ref visited);
 				//CachedUnclaimedCells.Sort(CellSort);
 			}
+
+			if (followSource)
+				Rule = ExpansionRules.FollowSourceTileState;
+		}
+		void PreCacheWalk()
+		{
+			WalkableCells.Clear();
+			VisitedCells.Clear();
+			StartCellWorldIdx = Grid.WorldIdx[startCell];
+			if (filteredDragTool != null)
+			{
+				CacheFilterableLayers();
+			}
+			else if (buildTool != null)
+			{
+				SgtLogger.l("BuildTool caching, current build tool def: " + buildTool.def.name);
+				if (buildTool.def.WidthInCells == 1 && buildTool.def.HeightInCells == 1 && buildTool.def.ObjectLayer == ObjectLayer.Backwall)
+				{
+					cachedDef = buildTool.def;
+				}
+				else
+					cachedDef = null;
+
+				SgtLogger.l(cachedDef == null ? "invalid def for spread" : "valid def for spread");
+			}
+			bool followSource = (Rule == ExpansionRules.FollowSourceTileState);
+			if (followSource)
+				Rule = Grid.IsSolidCell(startCell) ? ExpansionRules.SolidTile : ExpansionRules.NonSolidTile;
+
+			elementIdx = Grid.ElementIdx[startCell];
+			if (IsValidCell(startCell))
+				WalkableCells.Enqueue(startCell);
 
 			if (followSource)
 				Rule = ExpansionRules.FollowSourceTileState;
@@ -305,6 +392,45 @@ namespace SmartAreaFill.Content.Scripts
 					toWalk.Enqueue(right);
 			}
 			while (toWalk.Any() && visitedCells.Count < 3000);
+		}
+
+
+		void CacheNextCells()
+		{
+
+			bool followSource = (Rule == ExpansionRules.FollowSourceTileState);
+			if (followSource)
+				Rule = Grid.IsSolidCell(startCell) ? ExpansionRules.SolidTile : ExpansionRules.NonSolidTile;
+
+			if (!WalkableCells.Any())
+				return;
+
+			int toCheck = WalkableCells.Dequeue();
+			if (VisitedCells.Contains(toCheck))
+				return;
+
+			VisitedCells.Add(toCheck);
+			if (!IsValidCell(toCheck))
+				return;
+
+			CachedUnclaimedCells.Add(toCheck);
+
+			int above = Grid.CellAbove(toCheck);
+			int below = Grid.CellBelow(toCheck);
+			int left = Grid.CellLeft(toCheck);
+			int right = Grid.CellRight(toCheck);
+
+			if (Grid.IsValidCellInWorld(above, StartCellWorldIdx))
+				WalkableCells.Enqueue(above);
+			if (Grid.IsValidCellInWorld(below, StartCellWorldIdx))
+				WalkableCells.Enqueue(below);
+			if (Grid.IsValidCellInWorld(left, StartCellWorldIdx))
+				WalkableCells.Enqueue(left);
+			if (Grid.IsValidCellInWorld(right, StartCellWorldIdx))
+				WalkableCells.Enqueue(right);
+
+			if (followSource)
+				Rule = ExpansionRules.FollowSourceTileState;
 		}
 
 		bool IsValidCell(int targetCell)
