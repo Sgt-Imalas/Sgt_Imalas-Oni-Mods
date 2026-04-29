4b838536749b9e609ec5597558d9b48748308a87
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-04-29 20:30:45 +0200

    dig up fossils mini mod

---
+++ DigUpFossils/Mod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+using System.Collections.Generic;
+
+namespace DigUpFossils
+{
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+		}
+
+	}
+}
---
+++ DigUpFossils/Patches.cs
@@ -0,0 +1,28 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DigUpFossils
+{
+	internal class Patches
+	{
+
+		[HarmonyPatch(typeof(DigTool), nameof(DigTool.OnDragTool))]
+		public class DigTool_OnDragTool_Patch
+		{
+			public static void Postfix(DigTool __instance, int cell)
+			{
+				var potentialFossil = Grid.Objects[cell, (int)ObjectLayer.Building];
+				if (potentialFossil != null && potentialFossil.TryGetComponent<FossilBits>(out var bits) && !bits.MarkedForDig)
+					bits.OnSidescreenButtonPressed();
+
+			}
+		}
+	}
+}
