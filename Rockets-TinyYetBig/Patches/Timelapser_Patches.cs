3d55cbb197f3be281d6f366fac26b854f164e65b
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-04-30 03:03:05 +0200

    RE: skip timelapser for stations again (otherwise the name cause invalid files)

---
+++ Rockets-TinyYetBig/Patches/Timelapser_Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Rockets_TinyYetBig.SpaceStations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rockets_TinyYetBig.Patches
+{
+	internal class Timelapser_Patches
+	{
+
+		[HarmonyPatch(typeof(Timelapser), nameof(Timelapser.OnNewDay))]
+		public class Timelapser_OnNewDay_Patch
+		{
+			public static void Postfix(Timelapser __instance)
+			{
+				__instance.worldsToScreenshot.RemoveAll(world_id => SpaceStationManager.WorldIsSpaceStationInterior(world_id));
+			}
+		}
+	}
+}
