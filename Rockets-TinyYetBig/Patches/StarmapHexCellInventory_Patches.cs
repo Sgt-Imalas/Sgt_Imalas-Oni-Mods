56bd46dd7b9568a9181025998d1524e2697250f3
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-05-08 03:29:04 +0200

    RE: starmap hex inventory: discover element on collect

---
+++ Rockets-TinyYetBig/Patches/StarmapHexCellInventory_Patches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rockets_TinyYetBig.Patches
+{
+	internal class StarmapHexCellInventory_Patches
+	{
+
+        [HarmonyPatch(typeof(StarmapHexCellInventory), nameof(StarmapHexCellInventory.ExtractAndSpawnItemMass))]
+        public class StarmapHexCellInventory_ExtractAndSpawnItemMass_Patch
+        {
+            public static void Postfix(Tag ID, float mass) => DiscoverItemIfNotFound(ID, mass);
+		}
+
+        [HarmonyPatch(typeof(StarmapHexCellInventory), nameof(StarmapHexCellInventory.ExtractAndStoreItemMass))]
+        public class StarmapHexCellInventory_ExtractAndStoreItemMass_Patch
+        {
+            public static void Postfix(Tag ID, float mass) => DiscoverItemIfNotFound(ID, mass);
+
+		}
+        static void DiscoverItemIfNotFound(Tag ID, float mass)
+        {
+            if (mass <= 0 || DiscoveredResources.Instance.IsDiscovered(ID))
+				return;
+			DiscoveredResources.Instance.Discover(ID);
+		} 
+	}
+}
