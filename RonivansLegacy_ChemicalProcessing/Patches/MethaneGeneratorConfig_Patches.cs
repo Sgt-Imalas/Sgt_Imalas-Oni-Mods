eef0a71323d21dc27c15e5bc109c267c0e0c14cc
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-05-13 23:58:32 +0200

    RonivanAIO: less destructive methane generator patches

--- RonivansLegacy_ChemicalProcessing/Patches/MethaneGeneratorConfig_Patches.cs
+++ RonivansLegacy_ChemicalProcessing/Patches/MethaneGeneratorConfig_Patches.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 
 namespace RonivansLegacy_ChemicalProcessing.Patches
 {
-    class MethaneGeneratorConfig_Patches
+	class MethaneGeneratorConfig_Patches
 	{
 		[HarmonyPatch(typeof(MethaneGeneratorConfig), nameof(MethaneGeneratorConfig.DoPostConfigureComplete))]
 		public class MethaneGeneratorConfig_DoPostConfigureComplete_Patch
 		{
 			public static void Postfix(GameObject go)
 			{
-				go.GetComponent<EnergyGenerator>().formula.inputs = [new EnergyGenerator.InputItem(GameTags.CombustibleGas, 0.09f, 0.9f)];
+				go.GetComponent<EnergyGenerator>().formula.inputs[0] = new EnergyGenerator.InputItem(GameTags.CombustibleGas, 0.09f, 0.9f);
 				var dispenser = go.AddOrGet<ConduitDispenser>();
 				dispenser.elementFilter = dispenser.elementFilter.Append(SimHashes.Propane);
 			}
