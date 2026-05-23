5cb96a4dfb81d158c1ea73723f45f23b26099f80
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-05-24 01:03:19 +0200

    funny mini mod that prevents mod update for prev branch users

---
+++ NotUpdateDate/Mod.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using KMod;
+using System;
+using System.Collections.Generic;
+
+namespace NotUpdateDate
+{
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+			Debug.Log(this.mod.title+" - "+mod.label.version);
+		}
+
+	}
+}
---
+++ NotUpdateDate/Patches.cs
@@ -0,0 +1,34 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using KMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NotUpdateDate
+{
+	internal class Patches
+	{
+
+        [HarmonyPatch(typeof(Manager), nameof(Manager.Subscribe))]
+        public class KMod_Manager_Subscribe_Patch
+        {
+            public static bool Prefix() => false;
+		}
+
+		[HarmonyPatch(typeof(Manager), nameof(Manager.Unsubscribe))]
+		public class KMod_Manager_Unsubscribe_Patch
+		{
+			public static bool Prefix() => false;
+		}
+		[HarmonyPatch(typeof(Manager), nameof(Manager.Update), [typeof(KMod.Mod),typeof(object)])]
+		public class KMod_Manager_Update_Patch
+		{
+			public static bool Prefix() => false;
+		}
+	}
+}
