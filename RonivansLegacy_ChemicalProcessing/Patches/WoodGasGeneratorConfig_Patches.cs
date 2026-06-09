8a2f848ac6ba2cf933fbb8656d9c456117794c58
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-06-10 00:30:59 +0200

    RonivanAIO: added optional material selection filters for ethanol distillery and wood burner

---
+++ RonivansLegacy_ChemicalProcessing/Patches/WoodGasGeneratorConfig_Patches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using RonivansLegacy_ChemicalProcessing.Content.ModDb;
+using RonivansLegacy_ChemicalProcessing.Content.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	internal class WoodGasGeneratorConfig_Patches
+	{
+		/// <summary>
+		/// Adds ui to the wood generator to select the type of acceptable fuels
+		/// </summary>
+        [HarmonyPatch(typeof(WoodGasGeneratorConfig), nameof(WoodGasGeneratorConfig.DoPostConfigureComplete))]
+        public class WoodGasGeneratorConfig_DoPostConfigureComplete_Patch
+        {
+            public static void Postfix(GameObject go)
+			{
+				var o = go.AddOrGet<SolidDeliverySelection>();
+				o.Options = [.. RefinementRecipeHelper.GetWoods().Select(sh => sh.CreateTag())];
+				o.AnyTag = GameTags.BuildingWood;
+			}
+        }
+	}
+}
