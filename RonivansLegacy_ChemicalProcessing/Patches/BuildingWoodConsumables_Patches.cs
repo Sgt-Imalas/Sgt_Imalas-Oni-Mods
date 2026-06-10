07ea1b9e993d29b47472592500479ea131ff1b94
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-06-10 02:52:39 +0200

    RonivanAIO: compact wood consumer patches, add wood heater

---
+++ RonivansLegacy_ChemicalProcessing/Patches/BuildingWoodConsumables_Patches.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RonivansLegacy_ChemicalProcessing.Content.ModDb;
+using RonivansLegacy_ChemicalProcessing.Content.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	internal class BuildingWoodConsumables_Patches
+	{
+		public class BuildingConfig_ConfigureBuildingTemplate_Patch
+		{
+			[HarmonyTargetMethods]
+			internal static IEnumerable<MethodBase> TargetMethods()
+			{
+				yield return typeof(EthanolDistilleryConfig).GetMethod(nameof(EthanolDistilleryConfig.ConfigureBuildingTemplate));
+				yield return typeof(CampfireConfig).GetMethod(nameof(CampfireConfig.ConfigureBuildingTemplate));
+				yield return typeof(WoodGasGeneratorConfig).GetMethod(nameof(WoodGasGeneratorConfig.DoPostConfigureComplete));
+			}
+			[HarmonyPostfix]
+			public static void Postfix(GameObject go)
+			{
+				var o = go.AddOrGet<SolidDeliverySelection>();
+				o.Options = [.. RefinementRecipeHelper.GetWoods().Select(sh => sh.CreateTag())];
+				o.AnyTag = GameTags.BuildingWood;
+			}
+		}
+	}
+}
