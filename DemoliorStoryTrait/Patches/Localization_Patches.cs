135a9dfd4ae1c75f6416a66ff819ba953c9f3646
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-08-11 19:57:49 +0200

    demolior story trait: fix name

--- DemoliorStoryTrait/Patches/Localization_Patches.cs
+++ DemoliorStoryTrait/Patches/Localization_Patches.cs
@@ -10,7 +10,7 @@ using UtilLibs;
 
 namespace DemoliorStoryTrait.Patches
 {
-    class Localization_Patches
+	class Localization_Patches
 	{
 		public const string SettingNameKey = "STRINGS.CGM_DEMOLIOR_CONFIG_NAME";
 
@@ -19,23 +19,28 @@ namespace DemoliorStoryTrait.Patches
 		{
 			public static void Postfix()
 			{
-				string pipmolior_name;
-				string pipmolior_uppercase_name;
-				SgtLogger.l("LOC KEY: " + Localization.GetCurrentLanguageCode());
-				switch (Localization.GetCurrentLanguageCode())
+				string pipmolior_name = global::STRINGS.UI.SPACEDESTINATIONS.CLUSTERMAPMETEORS.LARGEIMACTOR.NAME;
+				string pipmolior_uppercase_name = global::STRINGS.MISC.NOTIFICATIONS.INCOMINGPREHISTORICASTEROIDNOTIFICATION.NAME;
+
+				if (Config.Instance.PipReplaceDemoliorSprite)
 				{
-					///by Flandre:
-					case "zh":
-					case "zh_klei":
-						pipmolior_name = "鼠王星";
-						pipmolior_uppercase_name = pipmolior_name;
-						break;
-					default:
-						pipmolior_name = "Pipmolior";
-						pipmolior_uppercase_name = "PIPMOLIOR";
-						break;
+					switch (Localization.GetCurrentLanguageCode())
+					{
+						///by Flandre:
+						case "zh":
+						case "zh_klei":
+							pipmolior_name = "鼠王星";
+							pipmolior_uppercase_name = pipmolior_name;
+							break;
+						default:
+							pipmolior_name = "Pipmolior";
+							pipmolior_uppercase_name = "PIPMOLIOR";
+							break;
+					}
 				}
 
+
+				Strings.Add(SettingNameKey, global::STRINGS.CREATURES.SPECIES.SQUIRREL.NAME + "-" + pipmolior_name);
 
 				if (Config.Instance.PipReplaceDemoliorSprite)
 				{
@@ -43,10 +48,10 @@ namespace DemoliorStoryTrait.Patches
 					global::STRINGS.MISC.NOTIFICATIONS.LARGEIMPACTORREVEALSEQUENCE.RETICLE.LARGE_IMPACTOR_NAME = pipmolior_uppercase_name;
 					global::STRINGS.UI.SPACEDESTINATIONS.CLUSTERMAPMETEORS.LARGEIMACTOR.NAME = pipmolior_name;
 				}
+
 				Strings.Add("STRINGS.CODEX.STORY_TRAITS.CGM_IMPACTOR.NAME", pipmolior_name);
 				Strings.Add("STRINGS.CODEX.STORY_TRAITS.CGM_IMPACTOR.DESCRIPTION", global::STRINGS.UI.SPACEDESTINATIONS.CLUSTERMAPMETEORS.LARGEIMACTOR.DESCRIPTION);
 				Strings.Add("STRINGS.CODEX.STORY_TRAITS.CGM_IMPACTOR.DESCRIPTION_SHORT", global::STRINGS.UI.SPACEDESTINATIONS.CLUSTERMAPMETEORS.LARGEIMACTOR.DESCRIPTION);
-				Strings.Add(SettingNameKey, global::STRINGS.CREATURES.SPECIES.SQUIRREL.NAME+"-"+ pipmolior_name);
 
 			}
 		}
