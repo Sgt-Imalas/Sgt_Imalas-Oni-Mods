3142672c1e9bcabb0411f5a774b133e8d2aa04bd
wuguo13842 <106533733+wuguo13842@users.noreply.github.com>
2026-08-31 12:02:58 +0800

    Fix: Force enable IME after KInputTextField.LateUpdate to allow Chinese input when creating new blueprints
    - Add KInputTextFieldPatch via Harmony to set Input.imeCompositionMode = On after LateUpdate

---
+++ BlueprintsV2/BlueprintsV2/Patches/KInputTextFieldPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BlueprintsV2.Patches
+{
+    [HarmonyPatch(typeof(KInputTextField))]
+    [HarmonyPatch("LateUpdate")]
+    public static class KInputTextFieldPatch
+    {
+        public static void Postfix()
+        {
+            // 在 LateUpdate 执行完毕后强制全局 IME 开启
+            UnityEngine.Input.imeCompositionMode = UnityEngine.IMECompositionMode.On;
+        }
+    }
+}
