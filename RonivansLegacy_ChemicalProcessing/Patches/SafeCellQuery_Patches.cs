7ec9ee481c94651123f2c5be092eda3d2d65f975
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-09-11 12:32:59 +0200

    RonivanAIO: mark acid cells as unsafe for safecellquery

---
+++ RonivansLegacy_ChemicalProcessing/Patches/SafeCellQuery_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	internal class SafeCellQuery_Patches
+	{
+
+        [HarmonyPatch(typeof(SafeCellQuery), nameof(SafeCellQuery.GetFlags))]
+        public class SafeCellQuery_GetFlags_Patch
+        {
+            public static void Postfix(SafeCellQuery __instance, int cell, SafeCellQuery.SafeFlags __result)
+            {
+                //if its an acid, consider it irradiated; this makes dupes avoid acid areas
+                if (Grid.Element[cell].HasTag(ModAssets.Tags.AIO_Acid))
+					__result = __result & ~SafeCellQuery.SafeFlags.IsNotRadiated;
+			}
+        }
+	}
+}
