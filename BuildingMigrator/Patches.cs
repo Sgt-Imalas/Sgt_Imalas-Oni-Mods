3ca079a7670fe266779527a32cf36d8efc7d38da
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-09-11 12:34:27 +0200

    building migrator prototype, for that one dude whose airlocks have gone missing

---
+++ BuildingMigrator/Patches.cs
@@ -0,0 +1,46 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using PeterHan.PLib.Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+using static BuildingMigrator.ModAssets;
+
+namespace BuildingMigrator
+{
+	internal class Patches
+	{
+		/// <summary>
+		/// register legacy building ids for save migration for merged buildings
+		/// </summary>
+		[HarmonyPatch(typeof(SaveManager), nameof(SaveManager.OnPrefabInit))]
+		public class SaveManager_OnPrefabInit_Patch
+		{
+			public static void Postfix(SaveManager __instance)
+			{
+				RegisterAdditionalMigrations();
+			}
+		}
+		internal static void RegisterAdditionalMigrations()
+		{
+			var savemng = SaveLoader.Instance.saveManager;
+			if (savemng == null)
+				return;
+			TryMigrate("InsulatedSelfSealingAirLock", "FastInsulatedSelfSealingAirLock");
+		}
+		static void TryMigrate(string oldId, string newId)
+		{
+			var savemng = SaveLoader.Instance.saveManager;
+			if (savemng == null)
+				return;
+			var prefab = Assets.TryGetPrefab(newId);
+			if (prefab != null && !savemng.prefabMap.ContainsKey(oldId))
+				savemng.prefabMap.Add(oldId, prefab);
+		}
+	}
+}
