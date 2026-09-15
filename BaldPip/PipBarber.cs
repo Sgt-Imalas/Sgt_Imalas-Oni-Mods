274f7f1e31e71b4fe6b14c32a2f9e68b8bdc2e04
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-09-15 15:55:09 +0200

    shave tag too

--- BaldPip/PipBarber.cs
+++ BaldPip/PipBarber.cs
@@ -7,14 +7,26 @@ namespace BaldPip
 	internal class PipBarber : KMonoBehaviour
 	{
 		[MyCmpReq] KBatchedAnimController kbac;
+
+		int effectAdded, effectRemoved;
 		public override void OnSpawn()
 		{
 			base.OnSpawn();
 			Shave();
+			effectAdded = Subscribe((int)GameHashes.EffectAdded, Shave);
+			effectRemoved = Subscribe((int)GameHashes.EffectRemoved, Shave);
+
 		}
-		void Shave()
+		public override void OnCleanUp()
+		{
+			Unsubscribe(effectAdded);
+			Unsubscribe(effectRemoved);
+			base.OnCleanUp();
+		}
+		void Shave(object data = null)
 		{
 			kbac.SetSymbolVisiblity("sq_leaf", false);
+			kbac.SetSymbolVisiblity("snapto_tag", false);
 		}
 	}
 }
