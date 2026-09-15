10357d6cda93058cb21498473e2e1d205170e848
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-09-15 15:43:57 +0200

    BaldPip from code

---
+++ BaldPip/Patches.cs
@@ -0,0 +1,26 @@
+﻿using Database;
+using HarmonyLib;
+using Klei.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UtilLibs;
+
+namespace BaldPip
+{
+	internal class Patches
+	{
+
+        [HarmonyPatch(typeof(BaseSquirrelConfig), nameof(BaseSquirrelConfig.BaseSquirrel))]
+        public class BaseSquirrelConfig_BaseSquirrel_Patch
+        {
+            public static void Postfix(GameObject __result)
+            {
+                __result.AddOrGet<PipBarber>();
+            }
+        }
+	}
+}
---
+++ BaldPip/PipBarber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaldPip
+{
+	internal class PipBarber : KMonoBehaviour
+	{
+		[MyCmpReq] KBatchedAnimController kbac;
+		public override void OnSpawn()
+		{
+			base.OnSpawn();
+			Shave();
+		}
+		void Shave()
+		{
+			kbac.SetSymbolVisiblity("sq_leaf", false);
+		}
+	}
+}
