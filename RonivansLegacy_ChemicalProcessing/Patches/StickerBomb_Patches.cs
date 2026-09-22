796fa8213b859f748a5d9bb9a592832167ca2fdc
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-09-22 21:15:02 +0200

    RonivanAIO: fix stickers

---
+++ RonivansLegacy_ChemicalProcessing/Patches/StickerBomb_Patches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UtilLibs;
+
+namespace RonivansLegacy_ChemicalProcessing.Patches
+{
+	internal class StickerBomb_Patches
+	{
+
+        [HarmonyPatch(typeof(StickerBomb), nameof(StickerBomb.OnSpawn))]
+        public class StickerBomb_CreatePrefab_Patch
+		{
+			[HarmonyPrepare]
+			public static bool Prepare() => Config.Instance.Drywall_Hides_Pipes;
+			public static void Prefix(StickerBomb __instance)
+            {
+				if (__instance.TryGetComponent<KBatchedAnimController>(out var kbac))
+					kbac.SetSceneLayer(ModAssets.AboveDrywallLayer);
+				else
+					SgtLogger.l("No kbac on stickerbomb!");
+
+			}
+        }
+	}
+}
